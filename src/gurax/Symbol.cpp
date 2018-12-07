1abed2bfbab70091c5942303aa4d5690df341844
ypsitau <ypsitau@nifty.com>
2018-12-07 13:05:02 +0900

    implemented Symbol

--- src/gurax/Symbol.cpp
+++ src/gurax/Symbol.cpp
@@ -21,7 +21,7 @@ const Symbol* Symbol::Add(const char* name)
 	auto ppSymbol = _pSymbolPool->find(&symbol);
 	if (ppSymbol != _pSymbolPool->end()) return *ppSymbol;
 	size_t bytes = sizeof(Symbol) + ::strlen(name) + 1;
-	::printf("bytes = %zu, %zu\n", sizeof(Symbol), bytes);
+	//::printf("bytes = %zu, %zu\n", sizeof(Symbol), bytes);
 	Symbol* pSymbol = reinterpret_cast<Symbol*>(MemoryPool::Allocate(bytes));
 	pSymbol->_uniqId = _pSymbolPool->size();
 	pSymbol->_name = pSymbol->_nameBuff;
