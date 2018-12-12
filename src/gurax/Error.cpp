23b160ea0360c42c4b9a751ae004537edac75d07
ypsitau <ypsitau@nifty.com>
2018-12-12 12:03:23 +0900

    implementing Error

--- src/gurax/Error.cpp
+++ src/gurax/Error.cpp
@@ -8,59 +8,16 @@ namespace Gurax {
 //-----------------------------------------------------------------------------
 // Error
 //-----------------------------------------------------------------------------
-Error Error::_instance;
+bool Error::_errorFlag = false;
+ErrorOwner* Error::_pErrorOwnerGlobal = nullptr;
 
-void Error::AddError(const Expr* pExpr, const char* format, ...)
+void Error::Bootup()
 {
-	va_list ap;
-	va_start(ap, format);
-	_instance.AddErrorV(pExpr->GetPathNameSrc(), pExpr->GetLineNo(), format, ap);
+	_errorFlag = false;
+	_pErrorOwnerGlobal = new ErrorOwner();
 }
 
-void Error::AddError(const char* format, ...)
-{
-	va_list ap;
-	va_start(ap, format);
-	_instance.AddErrorV("", 0, format, ap);
-}
-
-void Error::AddError(const String& fileName, int lineNo, const char* format, ...)
-{
-	va_list ap;
-	va_start(ap, format);
-	_instance.AddErrorV(fileName, lineNo, format, ap);
-}
-
-void Error::AddErrorV(const String& fileName, int lineNo, const char* format, va_list ap)
-{
-	char message[512];
-	::vsprintf(message, format, ap);
-	if (!_instance.GetEntryOwner().DoesExist(fileName.c_str(), lineNo, message)) {
-		_instance.GetEntryOwner().push_back(new Entry(fileName, lineNo, message));
-	}
-}
-
-void Error::Print(FILE* fp)
-{
-	for (auto pEntry : _instance.GetEntryOwner()) {
-		::fprintf(fp, "%s\n", pEntry->GetString().c_str());
-	}
-}
-
-String Error::MakeResultText()
-{
-	String str;
-	for (auto pEntry : _instance.GetEntryOwner()) {
-		str += pEntry->GetString();
-		str += "\n";
-	}
-	return str;
-}
-
-//-----------------------------------------------------------------------------
-// Error::Entry
-//-----------------------------------------------------------------------------
-String Error::Entry::GetString() const
+String Error::GetString() const
 {
 	String rtn;
 	if (!_fileName.empty()) {
@@ -77,19 +34,72 @@ String Error::Entry::GetString() const
 	return rtn;
 }
 
-//-----------------------------------------------------------------------------
-// Error::EntryList
-//-----------------------------------------------------------------------------
-bool Error::EntryList::DoesExist(const char* fileName, int lineNo, const char* message)
+void Error::Clear()
 {
-	for (auto pEntry : *this) {
-		if (pEntry->DoesMatch(fileName, lineNo, message)) return true;
+	_errorFlag = false;
+	_pErrorOwnerGlobal->Clear();
+}
+
+void Error::AddError(const Expr* pExpr, const char* format, ...)
+{
+	va_list ap;
+	va_start(ap, format);
+	AddErrorV(pExpr->GetPathNameSrc(), pExpr->GetLineNo(), format, ap);
+}
+
+void Error::AddError(const char* format, ...)
+{
+	va_list ap;
+	va_start(ap, format);
+	AddErrorV("", 0, format, ap);
+}
+
+void Error::AddError(const String& fileName, int lineNo, const char* format, ...)
+{
+	va_list ap;
+	va_start(ap, format);
+	AddErrorV(fileName, lineNo, format, ap);
+}
+
+void Error::AddErrorV(const String& fileName, int lineNo, const char* format, va_list ap)
+{
+	char message[512];
+	::vsprintf(message, format, ap);
+	if (!_pErrorOwnerGlobal->DoesExist(fileName.c_str(), lineNo, message)) {
+		_pErrorOwnerGlobal->push_back(new Error(fileName, lineNo, message));
+	}
+}
+
+void Error::Print(FILE* fp)
+{
+	for (auto pError : *_pErrorOwnerGlobal) {
+		::fprintf(fp, "%s\n", pError->GetString().c_str());
+	}
+}
+
+String Error::MakeResultText()
+{
+	String str;
+	for (auto pError : *_pErrorOwnerGlobal) {
+		str += pError->GetString();
+		str += "\n";
+	}
+	return str;
+}
+
+//-----------------------------------------------------------------------------
+// ErrorList
+//-----------------------------------------------------------------------------
+bool ErrorList::DoesExist(const char* fileName, int lineNo, const char* message)
+{
+	for (auto pError : *this) {
+		if (pError->DoesMatch(fileName, lineNo, message)) return true;
 	}
 	return false;
 }
 
 //-----------------------------------------------------------------------------
-// Error::EntryOwner
+// ErrorOwner
 //-----------------------------------------------------------------------------
 
 }
--- src/gurax/Error.h
+++ src/gurax/Error.h
@@ -8,53 +8,70 @@
 namespace Gurax {
 
 class Expr;
+class ErrorOwner;
 
-//-----------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // Error
-//-----------------------------------------------------------------------------
-class Error {
-public:
-	class Entry {
-	private:
-		String _fileName;
-		int _lineNo;
-		String _message;
-	public:
-		Entry(const String& fileName, const String& message) :
-			_fileName(fileName), _lineNo(0), _message(message) {}
-		Entry(const String& fileName, int lineNo, const String& message) :
-			_fileName(fileName), _lineNo(lineNo), _message(message) {}
-		bool DoesMatch(const char* fileName, int lineNo, const char* message) {
-			return _fileName == fileName && _lineNo == lineNo && _message == message;
-		}
-		String GetString() const;
-	};
-	class EntryList : public std::vector<Entry*> {
-	public:
-		bool DoesExist(const char* fileName, int lineNo, const char* message);
-	};
-	class EntryOwner : public EntryList {
-	public:
-		~EntryOwner() { Clear(); }
-		void Clear() {
-			for (auto pEntry : *this) delete pEntry;
-		}
-	};
+//------------------------------------------------------------------------------
+class Error : public Referable {
 private:
-	EntryOwner _entryOwner;
-	static Error _instance;
+	String _fileName;
+	int _lineNo;
+	String _message;
+private:
+	static bool _errorFlag;
+	static ErrorOwner* _pErrorOwnerGlobal;
 public:
-	Error() {}
-	EntryOwner& GetEntryOwner() { return _entryOwner; }
-	static Error& GetInstance() { return _instance; }
-	static bool HasError() { return !_instance.GetEntryOwner().empty(); }
-	static void Clear() { _instance._entryOwner.Clear(); }
+	// Constructor
+	Error(const String& fileName, const String& message) :
+		_fileName(fileName), _lineNo(0), _message(message) {}
+	Error(const String& fileName, int lineNo, const String& message) :
+		_fileName(fileName), _lineNo(lineNo), _message(message) {}
+	// Copy constructor/operator
+	Error(const Error& src) = delete;
+	Error& operator=(const Error& src) = delete;
+	// Move constructor/operator
+	Error(Error&& src) = delete;
+	Error& operator=(Error&& src) noexcept = delete;
+protected:
+	// Destructor
+	~Error() = default;
+public:
+	// Referable accessor
+	Gurax_DeclareReferable(Error);
+public:
+	static void Bootup();
+	bool DoesMatch(const char* fileName, int lineNo, const char* message) {
+		return _fileName == fileName && _lineNo == lineNo && _message == message;
+	}
+	String GetString() const;
+	static bool HasError() { return _errorFlag; }
+	static void Clear();
 	static void AddError(const Expr* pExpr, const char* format, ...);
 	static void AddError(const char* format, ...);
 	static void AddError(const String& fileName, int lineNo, const char* format, ...);
 	static void AddErrorV(const String& fileName, int lineNo, const char* format, va_list ap);
 	static void Print(FILE* fp);
 	static String MakeResultText();
+};
+
+//------------------------------------------------------------------------------
+// ErrorList
+//------------------------------------------------------------------------------
+class ErrorList : public std::vector<Error*> {
+public:
+	bool DoesExist(const char* fileName, int lineNo, const char* message);
+};
+
+//------------------------------------------------------------------------------
+// ErrorOwner
+//------------------------------------------------------------------------------
+class ErrorOwner : public ErrorList {
+public:
+	~ErrorOwner() { Clear(); }
+	void Clear() {
+		for (auto pError : *this) Error::Delete(pError);
+	}
 };
 
 }
