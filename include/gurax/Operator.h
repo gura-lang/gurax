bd5ede439b3477c3c68c10ab5d7fb7c094b8eda7
ypsitau <ypsitau@nifty.com>
2019-01-08 12:15:00 +0900

    implementing Operator

--- include/gurax/Operator.h
+++ include/gurax/Operator.h
@@ -3,9 +3,15 @@
 //==============================================================================
 #ifndef GURAX_OPERATOR_H
 #define GURAX_OPERATOR_H
+#include "Common.h"
 
 namespace Gurax {
 
+class Object;
+	
+//------------------------------------------------------------------------------
+// OpType
+//------------------------------------------------------------------------------
 enum class OpType {
 	None,
 	// unary operators
@@ -56,38 +62,221 @@ enum class OpType {
 	Pair,
 	// mathematical functions
 	math,
-	Math_abs 		= math,
-	Math_acos,
-	Math_arg,
-	Math_asin,
-	Math_atan,
-	Math_atan2,
-	Math_ceil,
-	Math_conj,
-	Math_cos,
-	Math_cosh,
-	Math_covariance,
-	Math_cross,
-	Math_delta,
-	Math_dot,
-	Math_exp,
-	Math_floor,
-	Math_hypot,
-	Math_imag,
-	Math_log,
-	Math_log10,
-	Math_norm,
-	Math_real,
-	Math_relu,
-	Math_sigmoid,
-	Math_sin,
-	Math_sinh,
-	Math_sqrt,
-	Math_tan,
-	Math_tanh,
-	Math_unitstep,
+	math_abs 		= math,
+	math_acos,
+	math_arg,
+	math_asin,
+	math_atan,
+	math_atan2,
+	math_ceil,
+	math_conj,
+	math_cos,
+	math_cosh,
+	math_covariance,
+	math_cross,
+	math_delta,
+	math_dot,
+	math_exp,
+	math_floor,
+	math_hypot,
+	math_imag,
+	math_log,
+	math_log10,
+	math_norm,
+	math_real,
+	math_relu,
+	math_sigmoid,
+	math_sin,
+	math_sinh,
+	math_sqrt,
+	math_tan,
+	math_tanh,
+	math_unitstep,
 	max,
 };
+
+//------------------------------------------------------------------------------
+// Operator
+//------------------------------------------------------------------------------
+class GURAX_DLLDECLARE Operator {
+private:
+	OpType _opType;
+public:
+	// unary operators
+	static const Operator* Pos;
+	static const Operator* Neg;
+	static const Operator* Inv;
+	static const Operator* Not;
+	// suffixed unary operators
+	static const Operator* SeqInf;
+	static const Operator* Question;
+	static const Operator* Each;
+	static const Operator* PostPos;
+	static const Operator* PostMod;
+	// binary operators
+	static const Operator* Add;
+	static const Operator* Sub;
+	static const Operator* Mul;
+	static const Operator* Div;
+	static const Operator* Mod;
+	static const Operator* ModMod;
+	static const Operator* Dot;
+	static const Operator* Cross;
+	static const Operator* Gear;
+	static const Operator* Concat;
+	static const Operator* Difference;
+	static const Operator* Intersection;
+	static const Operator* Union;
+	static const Operator* Pow;
+	static const Operator* Eq;
+	static const Operator* Ne;
+	static const Operator* Gt;
+	static const Operator* Lt;
+	static const Operator* Ge;
+	static const Operator* Le;
+	static const Operator* Cmp;
+	static const Operator* Contains;
+	static const Operator* And;
+	static const Operator* Or;
+	static const Operator* Xor;
+	static const Operator* Shl;
+	static const Operator* Shr;
+	static const Operator* OrOr;
+	static const Operator* AndAnd;
+	static const Operator* Seq;
+	static const Operator* Pair;
+	// mathematical functions
+	static const Operator* math_abs;
+	static const Operator* math_acos;
+	static const Operator* math_arg;
+	static const Operator* math_asin;
+	static const Operator* math_atan;
+	static const Operator* math_atan2;
+	static const Operator* math_ceil;
+	static const Operator* math_conj;
+	static const Operator* math_cos;
+	static const Operator* math_cosh;
+	static const Operator* math_covariance;
+	static const Operator* math_cross;
+	static const Operator* math_delta;
+	static const Operator* math_dot;
+	static const Operator* math_exp;
+	static const Operator* math_floor;
+	static const Operator* math_hypot;
+	static const Operator* math_imag;
+	static const Operator* math_log;
+	static const Operator* math_log10;
+	static const Operator* math_norm;
+	static const Operator* math_real;
+	static const Operator* math_relu;
+	static const Operator* math_sigmoid;
+	static const Operator* math_sin;
+	static const Operator* math_sinh;
+	static const Operator* math_sqrt;
+	static const Operator* math_tan;
+	static const Operator* math_tanh;
+	static const Operator* math_unitstep;
+public:
+	// Constructor
+	Operator(OpType opType) : _opType(opType) {}
+	// Copy constructor/operator
+	Operator(const Operator& src) = delete;
+	Operator& operator=(const Operator& src) = delete;
+	// Move constructor/operator
+	Operator(Operator&& src) = delete;
+	Operator& operator=(Operator&& src) noexcept = delete;
+	// Destructor
+	virtual ~Operator() = default;
+public:
+	OpType GetOpType() const { return _opType; }
+public:
+	virtual Object* EvalUnary(const Object* pObject);
+	virtual Object* EvalBinary(const Object* pObjectL, const Object* pObjectR);
+};
+
+#define Gurax_DeclareOperator(name) \
+class GURAX_DLLDECLARE Operator_##name : public Operator {	\
+public: \
+	Operator_##name() : Operator(OpType::name) {} \
+};
+
+//------------------------------------------------------------------------------
+// Operator
+//------------------------------------------------------------------------------
+// unary operators
+Gurax_DeclareOperator(Pos);
+Gurax_DeclareOperator(Neg);
+Gurax_DeclareOperator(Inv);
+Gurax_DeclareOperator(Not);
+// suffixed unary operators
+Gurax_DeclareOperator(SeqInf);
+Gurax_DeclareOperator(Question);
+Gurax_DeclareOperator(Each);
+Gurax_DeclareOperator(PostPos);
+Gurax_DeclareOperator(PostMod);
+// binary operators
+Gurax_DeclareOperator(Add);
+Gurax_DeclareOperator(Sub);
+Gurax_DeclareOperator(Mul);
+Gurax_DeclareOperator(Div);
+Gurax_DeclareOperator(Mod);
+Gurax_DeclareOperator(ModMod);
+Gurax_DeclareOperator(Dot);
+Gurax_DeclareOperator(Cross);
+Gurax_DeclareOperator(Gear);
+Gurax_DeclareOperator(Concat);
+Gurax_DeclareOperator(Difference);
+Gurax_DeclareOperator(Intersection);
+Gurax_DeclareOperator(Union);
+Gurax_DeclareOperator(Pow);
+Gurax_DeclareOperator(Eq);
+Gurax_DeclareOperator(Ne);
+Gurax_DeclareOperator(Gt);
+Gurax_DeclareOperator(Lt);
+Gurax_DeclareOperator(Ge);
+Gurax_DeclareOperator(Le);
+Gurax_DeclareOperator(Cmp);
+Gurax_DeclareOperator(Contains);
+Gurax_DeclareOperator(And);
+Gurax_DeclareOperator(Or);
+Gurax_DeclareOperator(Xor);
+Gurax_DeclareOperator(Shl);
+Gurax_DeclareOperator(Shr);
+Gurax_DeclareOperator(OrOr);
+Gurax_DeclareOperator(AndAnd);
+Gurax_DeclareOperator(Seq);
+Gurax_DeclareOperator(Pair);
+// mathematical functions
+Gurax_DeclareOperator(math_abs);
+Gurax_DeclareOperator(math_acos);
+Gurax_DeclareOperator(math_arg);
+Gurax_DeclareOperator(math_asin);
+Gurax_DeclareOperator(math_atan);
+Gurax_DeclareOperator(math_atan2);
+Gurax_DeclareOperator(math_ceil);
+Gurax_DeclareOperator(math_conj);
+Gurax_DeclareOperator(math_cos);
+Gurax_DeclareOperator(math_cosh);
+Gurax_DeclareOperator(math_covariance);
+Gurax_DeclareOperator(math_cross);
+Gurax_DeclareOperator(math_delta);
+Gurax_DeclareOperator(math_dot);
+Gurax_DeclareOperator(math_exp);
+Gurax_DeclareOperator(math_floor);
+Gurax_DeclareOperator(math_hypot);
+Gurax_DeclareOperator(math_imag);
+Gurax_DeclareOperator(math_log);
+Gurax_DeclareOperator(math_log10);
+Gurax_DeclareOperator(math_norm);
+Gurax_DeclareOperator(math_real);
+Gurax_DeclareOperator(math_relu);
+Gurax_DeclareOperator(math_sigmoid);
+Gurax_DeclareOperator(math_sin);
+Gurax_DeclareOperator(math_sinh);
+Gurax_DeclareOperator(math_sqrt);
+Gurax_DeclareOperator(math_tan);
+Gurax_DeclareOperator(math_tanh);
+Gurax_DeclareOperator(math_unitstep);
 
 };
 
--- src/lib/Operator.cpp
+++ src/lib/Operator.cpp
@@ -8,5 +8,89 @@ namespace Gurax {
 //------------------------------------------------------------------------------
 // Operator
 //------------------------------------------------------------------------------
+// unary operators
+const Operator* Operator::Pos			= new Operator_Pos();
+const Operator* Operator::Neg			= new Operator_Neg();
+const Operator* Operator::Inv			= new Operator_Inv();
+const Operator* Operator::Not			= new Operator_Not();
+// suffixed unary operators
+const Operator* Operator::SeqInf		= new Operator_SeqInf();
+const Operator* Operator::Question		= new Operator_Question();
+const Operator* Operator::Each			= new Operator_Each();
+const Operator* Operator::PostPos		= new Operator_PostPos();
+const Operator* Operator::PostMod		= new Operator_PostMod();
+// binary operators
+const Operator* Operator::Add			= new Operator_Add();
+const Operator* Operator::Sub			= new Operator_Sub();
+const Operator* Operator::Mul			= new Operator_Mul();
+const Operator* Operator::Div			= new Operator_Div();
+const Operator* Operator::Mod			= new Operator_Mod();
+const Operator* Operator::ModMod		= new Operator_ModMod();
+const Operator* Operator::Dot			= new Operator_Dot();
+const Operator* Operator::Cross			= new Operator_Cross();
+const Operator* Operator::Gear			= new Operator_Gear();
+const Operator* Operator::Concat		= new Operator_Concat();
+const Operator* Operator::Difference	= new Operator_Difference();
+const Operator* Operator::Intersection	= new Operator_Intersection();
+const Operator* Operator::Union			= new Operator_Union();
+const Operator* Operator::Pow			= new Operator_Pow();
+const Operator* Operator::Eq			= new Operator_Eq();
+const Operator* Operator::Ne			= new Operator_Ne();
+const Operator* Operator::Gt			= new Operator_Gt();
+const Operator* Operator::Lt			= new Operator_Lt();
+const Operator* Operator::Ge			= new Operator_Ge();
+const Operator* Operator::Le			= new Operator_Le();
+const Operator* Operator::Cmp			= new Operator_Cmp();
+const Operator* Operator::Contains		= new Operator_Contains();
+const Operator* Operator::And			= new Operator_And();
+const Operator* Operator::Or			= new Operator_Or();
+const Operator* Operator::Xor			= new Operator_Xor();
+const Operator* Operator::Shl			= new Operator_Shl();
+const Operator* Operator::Shr			= new Operator_Shr();
+const Operator* Operator::OrOr			= new Operator_OrOr();
+const Operator* Operator::AndAnd		= new Operator_AndAnd();
+const Operator* Operator::Seq			= new Operator_Seq();
+const Operator* Operator::Pair			= new Operator_Pair();
+// mathematical functions
+const Operator* Operator::math_abs		= new Operator_math_abs();
+const Operator* Operator::math_acos		= new Operator_math_acos();
+const Operator* Operator::math_arg		= new Operator_math_arg();
+const Operator* Operator::math_asin		= new Operator_math_asin();
+const Operator* Operator::math_atan		= new Operator_math_atan();
+const Operator* Operator::math_atan2	= new Operator_math_atan2();
+const Operator* Operator::math_ceil		= new Operator_math_ceil();
+const Operator* Operator::math_conj		= new Operator_math_conj();
+const Operator* Operator::math_cos		= new Operator_math_cos();
+const Operator* Operator::math_cosh		= new Operator_math_cosh();
+const Operator* Operator::math_covariance = new Operator_math_covariance();
+const Operator* Operator::math_cross	= new Operator_math_cross();
+const Operator* Operator::math_delta	= new Operator_math_delta();
+const Operator* Operator::math_dot		= new Operator_math_dot();
+const Operator* Operator::math_exp		= new Operator_math_exp();
+const Operator* Operator::math_floor	= new Operator_math_floor();
+const Operator* Operator::math_hypot	= new Operator_math_hypot();
+const Operator* Operator::math_imag		= new Operator_math_imag();
+const Operator* Operator::math_log		= new Operator_math_log();
+const Operator* Operator::math_log10	= new Operator_math_log10();
+const Operator* Operator::math_norm		= new Operator_math_norm();
+const Operator* Operator::math_real		= new Operator_math_real();
+const Operator* Operator::math_relu		= new Operator_math_relu();
+const Operator* Operator::math_sigmoid	= new Operator_math_sigmoid();
+const Operator* Operator::math_sin		= new Operator_math_sin();
+const Operator* Operator::math_sinh		= new Operator_math_sinh();
+const Operator* Operator::math_sqrt		= new Operator_math_sqrt();
+const Operator* Operator::math_tan		= new Operator_math_tan();
+const Operator* Operator::math_tanh		= new Operator_math_tanh();
+const Operator* Operator::math_unitstep	= new Operator_math_unitstep();
+
+Object* Operator::EvalUnary(const Object* pObject)
+{
+	return Object::nil();
+}
+
+Object* Operator::EvalBinary(const Object* pObjectL, const Object* pObjectR)
+{
+	return Object::nil();
+}
 
 }
