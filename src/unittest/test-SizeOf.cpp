efad0b0467aea99666693c1dc9e086529305ced4
ypsitau <ypsitau@nifty.com>
2019-01-12 12:43:53 +0900

    updated test-Sizeof

--- src/unittest/test-SizeOf.cpp
+++ src/unittest/test-SizeOf.cpp
@@ -13,8 +13,15 @@ Gurax_TesterEntry(SizeOf)
 	Gurax_PrintSizeOf(Frame_Branch);
 	Gurax_PrintSizeOf(ObjectOwner);
 	Gurax_PrintSizeOf(ObjectStack);
+	Gurax_PrintSizeOf(Object_binary);
 	Gurax_PrintSizeOf(Object_bool);
+	Gurax_PrintSizeOf(Object_dict);
+	Gurax_PrintSizeOf(Object_expr);
 	Gurax_PrintSizeOf(Object_function);
+	Gurax_PrintSizeOf(Object_iterator);
+	Gurax_PrintSizeOf(Object_klass);
+	Gurax_PrintSizeOf(Object_list);
+	Gurax_PrintSizeOf(Object_module);
 	Gurax_PrintSizeOf(Object_nil);
 	Gurax_PrintSizeOf(Object_number);
 	Gurax_PrintSizeOf(Object_string);
