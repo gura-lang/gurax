f68ebb2663d9be99963529dfc2c846c747bea6e3
ypsitau <ypsitau@nifty.com>
2019-02-21 21:32:00 +0900

    implementing Processor

--- src/lib/Processor.cpp
+++ src/lib/Processor.cpp
@@ -9,7 +9,8 @@ namespace Gurax {
 // Processor
 //------------------------------------------------------------------------------
 Processor::Processor() :
-	_pPUnitCur(nullptr), _pValueStack(new ValueStack()), _pFrame(Context::GetFrame().Reference())
+	_pPUnitCur(nullptr), _pValueStack(new ValueStack()),
+	_pFrame(Context::GetFrame().Reference())
 {
 }
 
