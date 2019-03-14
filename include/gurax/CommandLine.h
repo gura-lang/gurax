6a384f93cc9a27bee0a4842958334ec3b55d2cdb
ypsitau <ypsitau@nifty.com>
2019-03-14 21:34:54 +0900

    implementing CommandLine

--- include/gurax/CommandLine.h
+++ include/gurax/CommandLine.h
@@ -3,6 +3,7 @@
 //==============================================================================
 #ifndef GURAX_COMMANDLINE_H
 #define GURAX_COMMANDLINE_H
+#include "Referable.h"
 #include "String.h"
 
 namespace Gurax {
@@ -13,29 +14,75 @@ namespace Gurax {
 class CommandLine {
 public:
 	enum class Type { Flag, Value };
-	struct Info {
-		const char* keyLong;
-		char keyShort;
-		Type type;
+	class Info : public Referable {
+	public:
+		// Referable declaration
+		Gurax_DeclareReferable(Info);
+	private:
+		String _keyLong;
+		char _keyShort;
+		Type _type;
+	public:
+		// Constructor
+		Info(String keyLong, char keyShort, Type type) :
+			_keyLong(std::move(keyLong)), _keyShort(keyShort), _type(type) {}
+		// Copy constructor/operator
+		Info(const Info& src) = delete;
+		Info& operator=(const Info& src) = delete;
+		// Move constructor/operator
+		Info(Info&& src) = delete;
+		Info& operator=(Info&& src) noexcept = delete;
+	protected:
+		// Destructor
+		~Info() = default;
+	public:
+		const char* GetKeyLong() const { return _keyLong.c_str(); }
+		char GetKeyShort() const { return _keyShort; }
+		Type GetType() const { return _type; }
 	};
-	typedef std::map<String, Info> InfoMapByKeyLong;
-	typedef std::map<char, Info> InfoMapByKeyShort;
-	typedef std::map<String, StringList*> Map;
+	class InfoList : public std::vector<Info*> {
+	};
+	class InfoOwner : public InfoList {
+	public:
+		~InfoOwner() { Clear(); }
+		void Clear() {
+			for (auto pInfo : *this) Info::Delete(pInfo);
+			clear();
+		}
+	};
+	using InfoMapByKeyLong = std::map<String, const Info*>;
+	using InfoMapByKeyShort = std::map<char, const Info*>;
+	using Map = std::map<String, StringList*>;
 private:
+	InfoOwner _infoOwner;
 	InfoMapByKeyLong _infoMapByKeyLong;
 	InfoMapByKeyShort _infoMapByKeyShort;
 	Map _map;
 public:
+	// Constructor
 	CommandLine() {}
-	~CommandLine();
-	CommandLine& AddInfo(const Info* infoTbl, int cntInfo);
+	// Copy constructor/operator
+	CommandLine(const CommandLine& src) = delete;
+	CommandLine& operator=(const CommandLine& src) = delete;
+	// Move constructor/operator
+	CommandLine(CommandLine&& src) = delete;
+	CommandLine& operator=(CommandLine&& src) noexcept = delete;
+	// Destructor
+	~CommandLine() {
+		for (auto iter : _map) delete iter.second;
+	}		
+public:
+	void AddInfo(Info* pInfo);
+	void AddInfo(String keyLong, char keyShort, Type type) {
+		AddInfo(new Info(std::move(keyLong), keyShort, type));
+	}
 	bool Parse(int& argc, const char* argv[], String& strErr);
-	bool IsSet(const char* key);
-	const char* GetString(const char* key, const char* defValue) const;
-	const StringList& GetStringList(const char* key) const;
-	Int GetInt(const char* key, Int defValue) const;
+	bool IsSet(const char* keyLong);
+	const char* GetString(const char* keyLong, const char* defValue) const;
+	const StringList& GetStringList(const char* keyLong) const;
+	Int GetInt(const char* keyLong, Int defValue) const;
 private:
-	const char* _GetString(const char* key) const;
+	const char* _GetString(const char* keyLong) const;
 };
 
 }
--- src/lib/CommandLine.cpp
+++ src/lib/CommandLine.cpp
@@ -8,21 +8,11 @@ namespace Gurax {
 //------------------------------------------------------------------------------
 // CommandLine
 //------------------------------------------------------------------------------
-CommandLine::~CommandLine()
+void CommandLine::AddInfo(Info* pInfo)
 {
-	for (auto iter : _map) {
-		delete iter.second;
-	}
-}
-
-CommandLine& CommandLine::AddInfo(const Info* infoTbl, int cntInfo)
-{
-	const Info* pInfo = infoTbl;
-	for (int i = 0; i < cntInfo; i++, pInfo++) {
-		_infoMapByKeyLong[pInfo->keyLong] = *pInfo;
-		_infoMapByKeyShort[pInfo->keyShort] = *pInfo;
-	}
-	return *this;
+	_infoOwner.push_back(pInfo);
+	_infoMapByKeyLong[pInfo->GetKeyLong()] = pInfo;
+	_infoMapByKeyShort[pInfo->GetKeyShort()] = pInfo;
 }
 
 bool CommandLine::Parse(int& argc, const char* argv[], String& strErr)
@@ -38,38 +28,38 @@ bool CommandLine::Parse(int& argc, const char* argv[], String& strErr)
 				continue;
 			} else if (arg[1] == '-') {
 				const char* keyLong = &arg[2];
-				InfoMapByKeyLong::iterator iter = _infoMapByKeyLong.find(keyLong);
-				pInfo = (iter == _infoMapByKeyLong.end())? nullptr : &iter->second;
+				auto iter = _infoMapByKeyLong.find(keyLong);
+				pInfo = (iter == _infoMapByKeyLong.end())? nullptr : iter->second;
 				if (pInfo == nullptr) {
 					strErr = "unknown option ";
 					strErr += arg;
 					return false;
 				}
-				if (pInfo->type == Type::Value) {
+				if (pInfo->GetType() == Type::Value) {
 					stat = STAT_Value;
 				} else {
-					_map[pInfo->keyLong] = nullptr;
+					_map[pInfo->GetKeyLong()] = nullptr;
 				}
 			} else {
 				char keyShort = arg[1];
 				if (keyShort == '\0') break;
-				InfoMapByKeyShort::iterator iter = _infoMapByKeyShort.find(keyShort);
-				pInfo = (iter == _infoMapByKeyShort.end())? nullptr : &iter->second;
+				auto iter = _infoMapByKeyShort.find(keyShort);
+				pInfo = (iter == _infoMapByKeyShort.end())? nullptr : iter->second;
 				if (pInfo == nullptr) {
 					strErr = "unknown option ";
 					strErr += arg;
 					return false;
 				}
 				const char* value = &arg[2];
-				if (pInfo->type == Type::Value) {
+				if (pInfo->GetType() == Type::Value) {
 					if (value[0] == '\0') {
 						stat = STAT_Value;
 					} else {
-						Map::iterator iter = _map.find(pInfo->keyLong);
+						auto iter = _map.find(pInfo->GetKeyLong());
 						StringList* pStrList = nullptr;
 						if (iter == _map.end()) {
 							pStrList = new StringList();
-							_map[pInfo->keyLong] = pStrList;
+							_map[pInfo->GetKeyLong()] = pStrList;
 						} else {
 							pStrList = iter->second;
 						}
@@ -81,7 +71,7 @@ bool CommandLine::Parse(int& argc, const char* argv[], String& strErr)
 						strErr += arg;
 						return false;
 					}
-					_map[pInfo->keyLong] = nullptr;
+					_map[pInfo->GetKeyLong()] = nullptr;
 				}
 			}
 			argc--;
@@ -90,11 +80,11 @@ bool CommandLine::Parse(int& argc, const char* argv[], String& strErr)
 			}
 		} else if (stat == STAT_Value) {
 			const char* value = arg;
-			Map::iterator iter = _map.find(pInfo->keyLong);
+			auto iter = _map.find(pInfo->GetKeyLong());
 			StringList* pStrList = nullptr;
 			if (iter == _map.end()) {
 				pStrList = new StringList();
-				_map[pInfo->keyLong] = pStrList;
+				_map[pInfo->GetKeyLong()] = pStrList;
 			} else {
 				pStrList = iter->second;
 			}
@@ -115,33 +105,33 @@ bool CommandLine::Parse(int& argc, const char* argv[], String& strErr)
 	return true;
 }
 
-bool CommandLine::IsSet(const char* key)
+bool CommandLine::IsSet(const char* keyLong)
 {
-	return _map.find(key) != _map.end();
+	return _map.find(keyLong) != _map.end();
 }
 
-const char* CommandLine::GetString(const char* key, const char* defValue) const
+const char* CommandLine::GetString(const char* keyLong, const char* defValue) const
 {
-	const char* valStr = _GetString(key);
+	const char* valStr = _GetString(keyLong);
 	return (valStr == nullptr)? defValue : valStr;
 }
 
-const StringList& CommandLine::GetStringList(const char* key) const
+const StringList& CommandLine::GetStringList(const char* keyLong) const
 {
-	Map::const_iterator iter = _map.find(key);
+	auto iter = _map.find(keyLong);
 	if (iter == _map.end()) return StringList::Empty;
 	return* iter->second;
 }
 
-Int CommandLine::GetInt(const char* key, Int defValue) const
+Int CommandLine::GetInt(const char* keyLong, Int defValue) const
 {
-	const char* valStr = _GetString(key);
+	const char* valStr = _GetString(keyLong);
 	return (valStr == nullptr)? defValue : static_cast<int>(::strtol(valStr, nullptr, 0));
 }
 
-const char* CommandLine::_GetString(const char* key) const
+const char* CommandLine::_GetString(const char* keyLong) const
 {
-	Map::const_iterator iter = _map.find(key);
+	auto iter = _map.find(keyLong);
 	return (iter == _map.end())? nullptr : iter->second->front().c_str();
 }
 
