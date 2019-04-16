756bc39d9a1b586ebae88d8f7232b9211e74bbbc
ypsitau <ypsitau@nifty.com>
2019-04-16 12:57:32 +0900

    refactoring ArgPicker

--- src/lib/ArgPicker.cpp
+++ src/lib/ArgPicker.cpp
@@ -8,22 +8,21 @@ namespace Gurax {
 //------------------------------------------------------------------------------
 // ArgPicker
 //------------------------------------------------------------------------------
-Value* ArgPicker::PickValue()
+Value& ArgPicker::PickValue()
 {
-	if (!_pArgSlot) return nullptr;
 	_pValuePicked.reset(_pArgSlot->PickValue());
 	_pArgSlot = _pArgSlot->GetNext();
-	return _pValuePicked.get();
+	return *_pValuePicked;
 }
 
 Stream& ArgPicker::PickStream()
 {
-	return dynamic_cast<Value_Stream*>(PickValue())->GetStream();
+	return dynamic_cast<Value_Stream&>(PickValue()).GetStream();
 }
 
 Iterator& ArgPicker::PickIterator()
 {
-	return dynamic_cast<Value_Iterator*>(PickValue())->GetIterator();
+	return dynamic_cast<Value_Iterator&>(PickValue()).GetIterator();
 }
 
 }
