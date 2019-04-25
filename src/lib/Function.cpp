b2da791293f7995f0ee0628c6b512b81e5eefec1
ypsitau <ypsitau@nifty.com>
2019-04-25 13:15:57 +0900

    implementing implicit mapping

--- src/lib/Function.cpp
+++ src/lib/Function.cpp
@@ -16,33 +16,33 @@ String Function::MakeFullName() const
 void Function::DoCall(Processor& processor, Argument& argument) const
 {
 	const PUnit* pPUnitOfCaller = processor.GetPUnitCur();
-	if (argument.IsMapMode(Argument::MapMode::ToList)) {
-		if (pPUnitOfCaller->GetDiscardValueFlag()) {
-			while (argument.ReadyToPickValue()) {
-				Value::Delete(DoEval(processor, argument));
-			}
-		} else if (GetDeclCallable().IsResultVoid()) {
-			while (argument.ReadyToPickValue()) {
-				Value::Delete(DoEval(processor, argument));
-			}
-			processor.PushValue(Value::nil());
-		} else if (GetDeclCallable().IsResultReduce()) {
-			RefPtr<Value> pValueRtn(Value::nil());
-			while (argument.ReadyToPickValue()) {
-				pValueRtn.reset(DoEval(processor, argument));
-			}
-			processor.PushValue(pValueRtn.release());
-		} else {
-			RefPtr<Value_List> pValueRtn(new Value_List());
-			ValueTypedOwner& valueTypedOwner = pValueRtn->GetValueTypedOwner();
-			while (argument.ReadyToPickValue()) {
-				valueTypedOwner.Add(DoEval(processor, argument));
-			}
-			processor.PushValue(pValueRtn.release());
-		}
-	} else {
+	if (argument.IsMapMode(Argument::MapMode::None)) {
 		RefPtr<Value> pValue(DoEval(processor, argument));
 		if (!pPUnitOfCaller->GetDiscardValueFlag()) processor.PushValue(pValue->Reference());
+	} else if (pPUnitOfCaller->GetDiscardValueFlag()) {
+		while (argument.ReadyToPickValue()) {
+			Value::Delete(DoEval(processor, argument));
+		}
+	} else if (GetDeclCallable().IsResultVoid()) {
+		while (argument.ReadyToPickValue()) {
+			Value::Delete(DoEval(processor, argument));
+		}
+		processor.PushValue(Value::nil());
+	} else if (GetDeclCallable().IsResultReduce()) {
+		RefPtr<Value> pValueRtn(Value::nil());
+		while (argument.ReadyToPickValue()) {
+			pValueRtn.reset(DoEval(processor, argument));
+		}
+		processor.PushValue(pValueRtn.release());
+	} else if (argument.IsMapMode(Argument::MapMode::ToList)) {
+		RefPtr<Value_List> pValueRtn(new Value_List());
+		ValueTypedOwner& valueTypedOwner = pValueRtn->GetValueTypedOwner();
+		while (argument.ReadyToPickValue()) {
+			valueTypedOwner.Add(DoEval(processor, argument));
+		}
+		processor.PushValue(pValueRtn.release());
+	} else { // argument.IsMapMode(Argument::MapMode::ToIter)
+		
 	}
 	processor.SetPUnitNext(pPUnitOfCaller->GetPUnitCont());
 }
