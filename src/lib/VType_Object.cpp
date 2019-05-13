eb9ffdfc47eb773f76dbc7f35edbc4c8252568e4
ypsitau <ypsitau@nifty.com>
2019-05-13 11:21:40 +0900

    implemented Object#ToString() and Object#IsInstanceOf()

--- src/lib/VType_Object.cpp
+++ src/lib/VType_Object.cpp
@@ -6,6 +6,46 @@
 namespace Gurax {
 
 //------------------------------------------------------------------------------
+// Implementation of method
+//------------------------------------------------------------------------------
+// Object#IsInstanceOf(vtype:VType)
+Gurax_DeclareMethod(Object, IsInstanceOf)
+{
+	Declare(VTYPE_Bool, Flag::None);
+	DeclareArg("vtype", VTYPE_VType, DeclArg::Occur::Once, DeclArg::Flag::None, nullptr);
+	AddHelp(
+		Gurax_Symbol(en), 
+		"Returns `true` if the object is an instance of the specified `vtype`.\n");
+}
+
+Gurax_ImplementMethod(Object, IsInstanceOf)
+{
+	// Target
+	auto& valueThis = GetValueThis(argument);
+	// Arguments
+	ArgPicker args(argument);
+	const VType& vtype = Value_VType::GetVTypeThis(args.PickValue());
+	// Function body
+	return new Value_Bool(valueThis.IsInstanceOf(vtype));
+}
+
+// Object#ToString()
+Gurax_DeclareMethod(Object, ToString)
+{
+	Declare(VTYPE_String, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en), 
+		"Converts the object to a string.\n");
+}
+
+Gurax_ImplementMethod(Object, ToString)
+{
+	// Target
+	auto& valueThis = GetValueThis(argument);
+	return new Value_String(valueThis.ToString());
+}
+
+//------------------------------------------------------------------------------
 // VType_Object
 //------------------------------------------------------------------------------
 VType_Object VTYPE_Object("Object");
@@ -15,6 +55,9 @@ void VType_Object::DoPrepare(Frame& frameOuter)
 	// VType settings
 	SetAttrs(VType::Empty, Flag::Immutable);
 	frameOuter.Assign(*this);
+	// Assignment of method
+	Assign(Gurax_CreateMethod(Object, IsInstanceOf));
+	Assign(Gurax_CreateMethod(Object, ToString));
 }
 
 //------------------------------------------------------------------------------
