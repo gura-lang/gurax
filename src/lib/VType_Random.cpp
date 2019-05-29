48b26653585d6e25d0e7e4a53b1026cde5d104de
ypsitau <ypsitau@nifty.com>
2019-05-29 12:01:21 +0900

    implementing Random class

---
+++ src/lib/VType_Random.cpp
@@ -0,0 +1,150 @@
+//==============================================================================
+// VType_Random.cpp
+//==============================================================================
+#include "stdafx.h"
+
+namespace Gurax {
+
+//------------------------------------------------------------------------------
+// Implementation of constructor
+//------------------------------------------------------------------------------
+// Random(seed?:Number):map
+Gurax_DeclareFunction(Random)
+{
+	Declare(VTYPE_Random, Flag::Map);
+	DeclareArg("seed", VTYPE_Number, DeclArg::Occur::ZeroOrOnce, DeclArg::Flag::None, nullptr);
+	AddHelp(
+		Gurax_Symbol(en),
+		"Creates a `Random` instance that has been initialized with the specified seed.\n"
+		"If the argument `seed` is omitted, it creates an instance that holds a reference\n"
+		"to a random generator globally shared in the interpreter's environment.\n");
+}
+
+Gurax_ImplementFunction(Random)
+{
+	// Arguments
+	ArgPicker args(argument);
+	bool seedFlag = false;
+	Int32 seed = (seedFlag = args.IsValid())? args.PickInt32() : 0;
+	// Function body
+	RefPtr<Random> pRandom(seedFlag? new Random(seed) : Random::Global().Reference());
+	return new Value_Random(pRandom.release());
+}
+
+//------------------------------------------------------------------------------
+// Implementation of method
+//------------------------------------------------------------------------------
+// Random#Normal(mean?:Number, stddev?:Number)
+Gurax_DeclareMethod(Random, Normal)
+{
+	Declare(VTYPE_Number, Flag::None);
+	DeclareArg("mean", VTYPE_Number, DeclArg::Occur::ZeroOrOnce, DeclArg::Flag::None, nullptr);
+	DeclareArg("stddev", VTYPE_Number, DeclArg::Occur::ZeroOrOnce, DeclArg::Flag::None, nullptr);
+	AddHelp(
+		Gurax_Symbol(en),
+		"Randomly generates a real number based on uniform distribution with specified mean and standard deviation values.\n"
+		"The default value of `mean` is `0` and `stddev` is `1`.");
+}
+
+Gurax_ImplementMethod(Random, Normal)
+{
+	// Target
+	auto& valueThis = GetValueThis(argument);
+	// Arguments
+	ArgPicker args(argument);
+	Double mean = args.IsValid()? args.PickDouble() : 0.;
+	Double stddev = args.IsValid()? args.PickDouble() : 1.;
+	// Function body
+	return new Value_Number(valueThis.GetRandom().Normal<Double>(mean, stddev));
+}
+
+// Random#Range(range:Number)
+Gurax_DeclareMethod(Random, Range)
+{
+	Declare(VTYPE_Number, Flag::None);
+	DeclareArg("range", VTYPE_Number, DeclArg::Occur::Once, DeclArg::Flag::None, nullptr);
+	AddHelp(
+		Gurax_Symbol(en),
+		"Randomly generates an integer number in the range of `[0, range)`");
+}
+
+Gurax_ImplementMethod(Random, Range)
+{
+	// Target
+	auto& valueThis = GetValueThis(argument);
+	// Arguments
+	ArgPicker args(argument);
+	Int range = args.PickInt();
+	// Function body
+	return new Value_Number(valueThis.GetRandom().Range<Int>(range));
+}
+
+// Random#Reset():void
+Gurax_DeclareMethod(Random, Reset)
+{
+	Declare(VTYPE_Nil, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"Reset the seed of the random generator.");
+}
+
+Gurax_ImplementMethod(Random, Reset)
+{
+	// Target
+	auto& valueThis = GetValueThis(argument);
+	// Function body
+	valueThis.GetRandom().Reset();
+	return Value::nil();
+}
+
+// Random#Uniform()
+Gurax_DeclareMethod(Random, Uniform)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"Randomly generates a real number in the range of `[0, 1)`");
+}
+
+Gurax_ImplementMethod(Random, Uniform)
+{
+	// Target
+	auto& valueThis = GetValueThis(argument);
+	// Function body
+	return new Value_Number(valueThis.GetRandom().Uniform<Double>());
+}
+
+//------------------------------------------------------------------------------
+// VType_Random
+//------------------------------------------------------------------------------
+VType_Random VTYPE_Random("Random");
+
+void VType_Random::DoPrepare(Frame& frameOuter)
+{
+	// VType settings
+	SetAttrs(VTYPE_Object, Flag::Immutable);
+	SetConstructor(Gurax_CreateFunction(Random));
+	// Assignment of method
+	Assign(Gurax_CreateMethod(Random, Normal));
+	Assign(Gurax_CreateMethod(Random, Range));
+	Assign(Gurax_CreateMethod(Random, Reset));
+	Assign(Gurax_CreateMethod(Random, Uniform));
+}
+
+//------------------------------------------------------------------------------
+// Value_Random
+//------------------------------------------------------------------------------
+String Value_Random::ToStringDigest(const StringStyle& ss) const
+{
+	String str;
+	_ToStringDigest(str, ss);
+	str.Printf(":seed=0x%08x>", GetRandom().GetSeed());
+	return str;
+}
+
+String Value_Random::ToStringDetail(const StringStyle& ss) const
+{
+	return ToStringDigest(ss);
+}
+
+}
