fa92d159ad3640a620e9c04a4228ac16854819ec
ypsitau <ypsitau@nifty.com>
2019-06-26 22:12:41 +0900

    implementing Stream

--- src/lib/VType_Stream.cpp
+++ src/lib/VType_Stream.cpp
@@ -82,6 +82,67 @@ Gurax_ImplementMethod(Stream, Println)
 	return Value::nil();
 }
 
+// Stream#Read(bytes?:Number)
+Gurax_DeclareMethod(Stream, Read)
+{
+	Declare(VTYPE_Binary, Flag::None);
+	DeclareArg("bytes", VTYPE_Number, DeclArg::Occur::ZeroOrMore, DeclArg::Flag::None, nullptr);
+	AddHelp(
+		Gurax_Symbol(en),
+		"Reads data from the `Stream` and returns it as a `Binary` instance.");
+}
+
+Gurax_ImplementMethod(Stream, Read)
+{
+	return Value::nil();
+}
+
+// Stream#ReadLine():[chop] {block?}
+Gurax_DeclareMethod(Stream, ReadLine)
+{
+	Declare(VTYPE_Binary, Flag::None);
+	DeclareAttrOpt(Gurax_Symbol(chop));
+	AddHelp(
+		Gurax_Symbol(en),
+		"Reads one line from the `Stream` and returns it as a `String` instance.");
+}
+
+Gurax_ImplementMethod(Stream, ReadLine)
+{
+	return Value::nil();
+}
+
+// Stream#ReadLines(nLines?:Number):[chop] {block?}
+Gurax_DeclareMethod(Stream, ReadLines)
+{
+	Declare(VTYPE_Binary, Flag::None);
+	DeclareArg("nLines", VTYPE_Number, DeclArg::Occur::ZeroOrMore, DeclArg::Flag::None, nullptr);
+	DeclareAttrOpt(Gurax_Symbol(chop));
+	AddHelp(
+		Gurax_Symbol(en),
+		"Creates an iterator that reads each line from the `Stream` and returns it as a `String` instance.");
+}
+
+Gurax_ImplementMethod(Stream, ReadLines)
+{
+	return Value::nil();
+}
+
+// Stream#Write(ptr:Pointer, bytes?:Number)
+Gurax_DeclareMethod(Stream, Write)
+{
+	Declare(VTYPE_Binary, Flag::None);
+	DeclareArg("bytes", VTYPE_Number, DeclArg::Occur::ZeroOrMore, DeclArg::Flag::None, nullptr);
+	AddHelp(
+		Gurax_Symbol(en),
+		"Writes data pointed by the specified `Pointer` instance to the `Stream`.");
+}
+
+Gurax_ImplementMethod(Stream, Write)
+{
+	return Value::nil();
+}
+
 //------------------------------------------------------------------------------
 // VType_Stream
 //------------------------------------------------------------------------------
@@ -95,6 +156,10 @@ void VType_Stream::DoPrepare(Frame& frameOuter)
 	Assign(Gurax_CreateMethod(Stream, Print));
 	Assign(Gurax_CreateMethod(Stream, Printf));
 	Assign(Gurax_CreateMethod(Stream, Println));
+	Assign(Gurax_CreateMethod(Stream, Read));
+	Assign(Gurax_CreateMethod(Stream, ReadLine));
+	Assign(Gurax_CreateMethod(Stream, ReadLines));
+	Assign(Gurax_CreateMethod(Stream, Write));
 }
 
 Value* VType_Stream::DoCastFrom(const Value& value, DeclArg::Flags flags) const
