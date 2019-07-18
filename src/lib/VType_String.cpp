367c67118455566a106575724e59f9b92a62bf7f
ypsitau <ypsitau@nifty.com>
2019-07-18 10:43:00 +0900

    implementing String

--- src/lib/VType_String.cpp
+++ src/lib/VType_String.cpp
@@ -8,6 +8,203 @@ namespace Gurax {
 //------------------------------------------------------------------------------
 // Implementation of method
 //------------------------------------------------------------------------------
+// String#Align(width:number, padding:string => ' '):map:[center,left,right] {block?}
+Gurax_DeclareMethod(String, Align)
+{
+#if 0
+	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_Map);
+	DeclareArg(env, "width", VTYPE_number);
+	DeclareArg(env, "padding", VTYPE_string, OCCUR_Once, FLAG_None, 0, new Expr_Value(Value(" ")));
+	DeclareAttr(Gurax_Symbol(center));
+	DeclareAttr(Gurax_Symbol(left));
+	DeclareAttr(Gurax_Symbol(right));
+	DeclareBlock(OCCUR_ZeroOrOnce);
+#endif
+	AddHelp(
+		Gurax_Symbol(en), 
+		"Align the string to the left, right or center within the specified `width`\n"
+		"and returns the result.\n"
+		"\n"
+		"The following attributes specify the alignment position:\n"
+		"\n"
+		"- `:center` .. Aligns to the center. This is the default.\n"
+		"- `:left` .. Aligns to the left\n"
+		"- `:right` .. Aligns to the right\n"
+		"\n"
+		"If the string width is narrower than the specified `width`, nothing would be done.\n"
+		"\n"
+		"It uses a string specified by the argument `padding` to fill lacking spaces.\n"
+		"If omitted, a white space is used for padding.\n"
+		"\n"
+		"This method takes into account the character width based on the specification\n"
+		"of East Asian Width. A kanji-character occupies two characters in width.\n");
+}
+
+Gurax_ImplementMethod(String, Align)
+{
+	return Value::nil();
+}
+
+// String#Capitalize() {block?}
+Gurax_DeclareMethod(String, Capitalize)
+{
+#if 0
+	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_None);
+	DeclareBlock(OCCUR_ZeroOrOnce);
+#endif
+	AddHelp(
+		Gurax_Symbol(en), 
+		"Returns a string that capitalizes the first character.\n");
+}
+
+Gurax_ImplementMethod(String, Capitalize)
+{
+	return Value::nil();
+}
+
+// String#Chop(suffix*:string):[eol,icase] {block?}
+Gurax_DeclareMethod(String, Chop)
+{
+#if 0
+	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_None);
+	DeclareArg(env, "suffix", VTYPE_string, OCCUR_ZeroOrMore);
+	DeclareAttr(Gurax_Symbol(eol));
+	DeclareAttr(Gurax_Symbol(icase));
+	DeclareBlock(OCCUR_ZeroOrOnce);
+#endif
+	AddHelp(
+		Gurax_Symbol(en), 
+		"Returns a string that removes a last character.\n"
+		"\n"
+		"If an attribute `:eol` is specified, only the end-of-line character shall be\n"
+		"removed. In this case, if the end-of-line has a sequence of CR-LF, CR code\n"
+		"shall be removed as well.");
+}
+
+Gurax_ImplementMethod(String, Chop)
+{
+	return Value::nil();
+}
+
+// String#DecodeURI() {block?}
+Gurax_DeclareMethod(String, DecodeURI)
+{
+#if 0
+	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_None);
+	DeclareBlock(OCCUR_ZeroOrOnce);
+#endif
+	AddHelp(
+		Gurax_Symbol(en), 
+		"Returns a string in which percent-encoded characters are decoded.");
+}
+
+Gurax_ImplementMethod(String, DecodeURI)
+{
+	return Value::nil();
+}
+
+// String#Each():[utf8,utf32] {block?}
+Gurax_DeclareMethod(String, Each)
+{
+#if 0
+	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_Map);
+	DeclareAttr(Gurax_Symbol(utf8));
+	DeclareAttr(Gurax_Symbol(utf32));
+	DeclareBlock(OCCUR_ZeroOrOnce);
+#endif
+	AddHelp(
+		Gurax_Symbol(en), 
+		"Creates an iterator generating strings of each character in the original one.\n");
+}
+
+Gurax_ImplementMethod(String, Each)
+{
+	return Value::nil();
+}
+
+// String#EachLine(nLines?:number):[chop] {block?}
+// conrresponding to file#readlines()
+Gurax_DeclareMethod(String, EachLine)
+{
+#if 0
+	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_None);
+	DeclareArg(env, "nLines", VTYPE_number, OCCUR_ZeroOrOnce);
+	DeclareAttr(Gurax_Symbol(chop));
+	DeclareBlock(OCCUR_ZeroOrOnce);
+#endif
+	AddHelp(
+		Gurax_Symbol(en), 
+		"Creates an iterator generating strings of each line in the original one.\n"
+		"\n"
+		"In default, end-of-line characters are involved in the result.\n"
+		"You can eliminates them by specifying `:chop` attribute.\n");
+}
+
+Gurax_ImplementMethod(String, EachLine)
+{
+	return Value::nil();
+}
+
+// String#Embed(dst?:stream:w):[noindent,lasteol]
+Gurax_DeclareMethod(String, Embed)
+{
+#if 0
+	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_None);
+	DeclareArg(env, "dst", VTYPE_stream, OCCUR_ZeroOrOnce, FLAG_Write);
+	DeclareAttr(Gurax_Symbol(noindent));
+	DeclareAttr(Gurax_Symbol(lasteol));
+#endif
+	AddHelp(
+		Gurax_Symbol(en),
+		"Evaluates a string that contains embedded scripts\n"
+		"and renders the result to the specified stream.\n"
+		"\n"
+		"If the stream is omitted, the function returns the rendered result as a string.\n"
+		"\n"
+		"Calling this method is equivalent to calling a method `String#template()` to\n"
+		"create a `template` instance on which a method `template#render()` is applied afterward.\n");
+}
+
+Gurax_ImplementMethod(String, Embed)
+{
+	return Value::nil();
+}
+
+// String#Encode(codec:Codec) {block?}
+Gurax_DeclareMethod(String, Encode)
+{
+#if 0
+	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_None);
+	DeclareArg(env, "codec", VTYPE_codec);
+	DeclareBlock(OCCUR_ZeroOrOnce);
+#endif
+	AddHelp(
+		Gurax_Symbol(en),
+		"Encodes the string with the given `codec` and return the result as a `binary`.\n");
+}
+
+Gurax_ImplementMethod(String, Encode)
+{
+	return Value::nil();
+}
+
+// String#EncodeURI() {block?}
+Gurax_DeclareMethod(String, EncodeURI)
+{
+#if 0
+	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_None);
+	DeclareBlock(OCCUR_ZeroOrOnce);
+#endif
+	AddHelp(
+		Gurax_Symbol(en), 
+		"Returns a string in which non-URIC characters are percent-encoded.\n");
+}
+
+Gurax_ImplementMethod(String, EncodeURI)
+{
+	return Value::nil();
+}
+
 // String#EndsWith(sub:String, posEnd?:Number):map:[rest,icase]
 Gurax_DeclareMethod(String, EndsWith)
 {
@@ -140,6 +337,23 @@ Gurax_ImplementMethod(String, StartsWith)
 		String::StartsWith<CharCase>(str, sub);
 	return !argument.IsSet(Gurax_Symbol(rest))? Value::Bool(rtn) :
 		rtn? new Value_String(rtn) : Value::nil();
+}
+
+// String#ToBinary() {block?}
+Gurax_DeclareMethod(String, ToBinary)
+{
+#if 0
+	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_None);
+	DeclareBlock(OCCUR_ZeroOrOnce);
+#endif
+	AddHelp(
+		Gurax_Symbol(en),
+		"Converts the string into `binary` instance.");
+}
+
+Gurax_ImplementMethod(String, ToBinary)
+{
+	return Value::nil();
 }
 
 // String#Upper()
