35f0839e975981248a0a6108fa2c7455fc294a00
ypsitau <ypsitau@nifty.com>
2019-07-27 20:46:43 +0900

    implementing Palette and SuffixMgr

--- include/gurax/SuffixMgr.h
+++ include/gurax/SuffixMgr.h
@@ -7,7 +7,47 @@
 
 namespace Gurax {
 
-class GURAX_DLLDECLARE SuffixMgr {
+//------------------------------------------------------------------------------
+// SuffixMgr
+//------------------------------------------------------------------------------
+class GURAX_DLLDECLARE SuffixMgr : public Referable {
+public:
+	// Referable declaration
+	Gurax_DeclareReferable(SuffixMgr);
+public:
+	// Constructor
+	SuffixMgr() {}
+	// Copy constructor/operator
+	SuffixMgr(const SuffixMgr& src) = delete;
+	SuffixMgr& operator=(const SuffixMgr& src) = delete;
+	// Move constructor/operator
+	SuffixMgr(SuffixMgr&& src) = delete;
+	SuffixMgr& operator=(SuffixMgr&& src) noexcept = delete;
+protected:
+	~SuffixMgr() = default;
+public:
+	size_t CalcHash() const { return reinterpret_cast<size_t>(this); }
+	bool IsIdentical(const SuffixMgr& suffixMgr) const { return this == &suffixMgr; }
+	bool IsEqualTo(const SuffixMgr& suffixMgr) const { return IsIdentical(suffixMgr); }
+	bool IsLessThan(const SuffixMgr& suffixMgr) const { return this < &suffixMgr; }
+	String ToString(const StringStyle& ss = StringStyle::Empty) const;
+};
+
+//------------------------------------------------------------------------------
+// SuffixMgrList
+//------------------------------------------------------------------------------
+class GURAX_DLLDECLARE SuffixMgrList : public std::vector<SuffixMgr*> {
+public:
+	using std::vector<SuffixMgr*>::vector;
+};
+
+//------------------------------------------------------------------------------
+// SuffixMgrOwner
+//------------------------------------------------------------------------------
+class GURAX_DLLDECLARE SuffixMgrOwner : public SuffixMgrList {
+public:
+	~SuffixMgrOwner() { Clear(); }
+	void Clear();
 };
 
 }
--- src/lib/VType_Palette.cpp
+++ src/lib/VType_Palette.cpp
@@ -6,6 +6,48 @@
 namespace Gurax {
 
 //------------------------------------------------------------------------------
+// Implementation of class method
+//------------------------------------------------------------------------------
+// Palette.Basic()
+Gurax_DeclareClassMethod(Palette, Basic)
+{
+	Declare(VTYPE_Palette, Flag::None);
+}
+
+Gurax_ImplementClassMethod(Palette, Basic)
+{
+	// Function body
+	RefPtr<Palette> pPalette(new Palette());
+	return new Value_Palette(pPalette.release());
+}
+
+// Palette.WebSafe()
+Gurax_DeclareClassMethod(Palette, WebSafe)
+{
+	Declare(VTYPE_Palette, Flag::None);
+}
+
+Gurax_ImplementClassMethod(Palette, WebSafe)
+{
+	// Function body
+	RefPtr<Palette> pPalette(new Palette());
+	return new Value_Palette(pPalette.release());
+}
+
+// Palette.Win256()
+Gurax_DeclareClassMethod(Palette, Win256)
+{
+	Declare(VTYPE_Palette, Flag::None);
+}
+
+Gurax_ImplementClassMethod(Palette, Win256)
+{
+	// Function body
+	RefPtr<Palette> pPalette(new Palette());
+	return new Value_Palette(pPalette.release());
+}
+
+//------------------------------------------------------------------------------
 // VType_Palette
 //------------------------------------------------------------------------------
 VType_Palette VTYPE_Palette("Palette");
@@ -14,6 +56,10 @@ void VType_Palette::DoPrepare(Frame& frameOuter)
 {
 	// VType settings
 	SetAttrs(VTYPE_Object, Flag::Immutable);
+	// Assignment of class method
+	Assign(Gurax_CreateClassMethod(Palette, Basic));
+	Assign(Gurax_CreateClassMethod(Palette, WebSafe));
+	Assign(Gurax_CreateClassMethod(Palette, Win256));
 }
 
 //------------------------------------------------------------------------------
