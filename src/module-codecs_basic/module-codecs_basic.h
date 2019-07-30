dcb1d4b9b695ca002398812a1c93cba68c8b737c
ypsitau <ypsitau@nifty.com>
2019-07-30 12:16:36 +0900

    added module-codecs_basic

---
+++ src/module-codecs_basic/module-codecs_basic.h
@@ -0,0 +1,10 @@
+//==============================================================================
+// module-codecs_basic.h
+//==============================================================================
+#ifndef GURAX_MODULE_CODECS_BASIC_H
+#define GURAX_MODULE_CODECS_BASIC_H
+
+Gurax_BeginModuleHeader(codecs_basic)
+Gurax_EndModuleHeader(codecs_basic)
+
+#endif
