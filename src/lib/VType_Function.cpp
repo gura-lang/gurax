e07294aa3f97c361c6df180cae46091e59df3aaa
ypsitau <ypsitau@nifty.com>
2019-09-03 13:51:00 +0900

    implemented Expr::GatherArgSymbols()

--- src/lib/VType_Function.cpp
+++ src/lib/VType_Function.cpp
@@ -8,13 +8,64 @@ namespace Gurax {
 //------------------------------------------------------------------------------
 // Implementation of constructor
 //------------------------------------------------------------------------------
+// &{block}
+Gurax_DeclareFunctionAlias(_function_, "&")
+{
+	Declare(VTYPE_Dict, Flag::None);
+	DeclareBlock(DeclBlock::Occur::Once, DeclBlock::Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"Creates a `Function` instance.\n");
+}
+
+Gurax_ImplementFunction(_function_)
+{
+#if 0
+	Expr* pExpr = exprCaller.GetExprOfBlock()->GetExprElemFirst();
+	composer.Add_CreateDict(&exprCaller);						// [Dict]
+	for ( ; pExpr; pExpr = pExpr->GetExprNext()) {
+		if (pExpr->IsType<Expr_BinaryOp>() &&
+			dynamic_cast<Expr_BinaryOp*>(pExpr)->GetOperator()->IsType(OpType::Pair)) {
+			// %{ .. key => value .. }
+			Expr_BinaryOp* pExprEx = dynamic_cast<Expr_BinaryOp*>(pExpr);
+			pExprEx->GetExprLeft().ComposeOrNil(composer);		// [Dict Key]
+			pExprEx->GetExprRight().ComposeOrNil(composer);		// [Dict Key Elem]
+		} else if (pExpr->IsType<Expr_Block>()) {
+			// %{ .. {key, value} .. }
+			Expr_Block* pExprEx = dynamic_cast<Expr_Block*>(pExpr);
+			if (pExprEx->CountExprElem() != 2) {
+				Error::IssueWith(ErrorType::SyntaxError, exprCaller,
+								 "block is expected to have a format of {key, value}");
+				return;
+			}
+			Expr* pExprElem = pExprEx->GetExprElemFirst();
+			pExprElem->ComposeOrNil(composer);					// [Dict Key]
+			pExprElem = pExprElem->GetExprNext();
+			pExprElem->ComposeOrNil(composer);					// [Dict Key Elem]
+		} else {
+			// %{ .. key, value .. }
+			pExpr->ComposeOrNil(composer);						// [Dict Key]
+			pExpr = pExpr->GetExprNext();
+			if (!pExpr) {
+				Error::IssueWith(ErrorType::SyntaxError, exprCaller,
+								 "value is missing in the initialization list for dictionary");
+				return;
+			}
+			pExpr->ComposeOrNil(composer);						// [Dict Key Elem]
+		}
+		composer.Add_DictElem(0, &exprCaller);					// [Dict]
+	}
+#endif
+	return Value::nil();
+}
+
 // Function(`exprs*) {block}
 Gurax_DeclareFunction(Function)
 {
 	Declare(VTYPE_Random, Flag::Map);
 	DeclareArg("argDecls", VTYPE_Quote, ArgOccur::ZeroOrMore, ArgFlag::None);
 	DeclareBlock(DeclBlock::Occur::Once);
-		AddHelp(
+	AddHelp(
 		Gurax_Symbol(en),
 		"");
 }
@@ -105,6 +156,8 @@ void VType_Function::DoPrepare(Frame& frameOuter)
 	// VType settings
 	SetAttrs(VTYPE_Object, Flag::Immutable);
 	SetConstructor(Gurax_CreateFunction(Function));
+	// Assignment of function
+	Assign(Gurax_CreateFunction(_function_));
 	// Assignment of property
 	Assign(Gurax_CreateProperty(Function, expr));
 	Assign(Gurax_CreateProperty(Function, name));
