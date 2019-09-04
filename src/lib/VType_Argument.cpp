816e5c707627279bddd51bcdea4412dc1fb5e819
ypsitau <ypsitau@nifty.com>
2019-09-04 12:11:46 +0900

    implemented Argument::IsSet()

--- src/lib/VType_Argument.cpp
+++ src/lib/VType_Argument.cpp
@@ -6,6 +6,30 @@
 namespace Gurax {
 
 //------------------------------------------------------------------------------
+// Implementation of method
+//------------------------------------------------------------------------------
+// Argument#IsSet(symbol:Symbol):Bool
+Gurax_DeclareMethod(Argument, IsSet)
+{
+	Declare(VTYPE_Bool, Flag::None);
+	DeclareArg("symbol", VTYPE_Symbol, ArgOccur::Once, ArgFlag::None);
+	AddHelp(
+		Gurax_Symbol(en), 
+		"Returns `true` if the argument has te specified symbol in its attribute list.");
+}
+
+Gurax_ImplementMethod(Argument, IsSet)
+{
+	// Target
+	auto& valueThis = GetValueThis(argument);
+	// Arguments
+	ArgPicker args(argument);
+	const Symbol* pSymbol = args.PickSymbol();
+	// Function body
+	return new Value_Bool(valueThis.GetArgument().IsSet(pSymbol));
+}
+
+//------------------------------------------------------------------------------
 // VType_Argument
 //------------------------------------------------------------------------------
 VType_Argument VTYPE_Argument("Argument");
@@ -14,6 +38,8 @@ void VType_Argument::DoPrepare(Frame& frameOuter)
 {
 	// VType settings
 	SetAttrs(VTYPE_Object, Flag::Immutable);
+	// Assignment of method
+	Assign(Gurax_CreateMethod(Argument, IsSet));
 }
 
 //------------------------------------------------------------------------------
