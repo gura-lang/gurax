1fd1f217720bfe708ccd72831c0591dca636d0ad
ypsitau <ypsitau@nifty.com>
2019-11-15 10:05:33 +0900

    implementing conio module

--- src/module-conio/Util.cpp
+++ src/module-conio/Util.cpp
@@ -155,6 +155,7 @@ Value* SetColor(Processor& processor, const Symbol* pSymbolFg, const Symbol* pSy
 	::SetConsoleTextAttribute(hConsole, fg + (bg << 4));
 	if (pExprOfBlock) {
 		RefPtr<Value> pValue(pExprOfBlock->DoEval(processor));
+		if (Error::IsIssued()) return Value::nil();
 		::SetConsoleTextAttribute(hConsole, csbi.wAttributes);
 		return pValue.release();
 	}
@@ -170,6 +171,7 @@ Value* MoveTo(Processor& processor, int x, int y, const Expr_Block* pExprOfBlock
 	::SetConsoleCursorPosition(hConsole, pos);
 	if (pExprOfBlock) {
 		RefPtr<Value> pValue(pExprOfBlock->DoEval(processor));
+		if (Error::IsIssued()) return Value::nil();
 		::SetConsoleCursorPosition(hConsole, csbi.dwCursorPosition);
 		return pValue.release();
 	}
@@ -313,6 +315,7 @@ Value* SetColor(Processor& processor, const Symbol* pSymbolFg, const Symbol* pSy
 	if (pExprOfBlock) {
 		g_attrStack.push_back(str);
 		RefPtr<Value> pValue(pExprOfBlock->DoEval(processor));
+		if (Error::IsIssued()) return Value::nil();
 		if (!g_attrStack.empty()) g_attrStack.pop_back();
 		if (g_attrStack.empty()) {
 			::printf("\033[0m");
@@ -332,6 +335,7 @@ Value* MoveTo(Processor& processor, int x, int y, const Expr_Block* pExprOfBlock
 		::printf("\033[s");
 		::printf("\033[%d;%dH", y + 1, x + 1);
 		RefPtr<Value> pValue(pExprOfBlock->DoEval(processor));
+		if (Error::IsIssued()) return Value::nil();
 		::printf("\033[u");
 		return pValue.release();
 	}
