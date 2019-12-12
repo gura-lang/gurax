7ea8670be96d0a330a9816d3082bd7eb49cc140e
ypsitau <ypsitau@nifty.com>
2019-12-12 20:35:25 +0900

    implementing Pointer

--- include/gurax/Pointer.h
+++ include/gurax/Pointer.h
@@ -36,30 +36,18 @@ public:
 		return (_offset < bytesEntire)? bytesEntire - _offset : 0;
 	}
 	bool Advance(int distance);
-	bool PackStay(const char *format, const ValueList& valListArg);
-	Value UnpackStay(const char *format, const ValueList& valListArg, bool exeedErrorFlag);
-	template<typename T> bool PutStay(Environment &env, T num, bool bigEndianFlag) {
-		size_t offset = _offset;
-		bool rtn = Put<T>(num, bigEndianFlag);
-		_offset = offset;
-		return rtn;
-	}
-	template<typename T> bool GetStay(Environment &env, T *pNum, bool bigEndianFlag, bool exceedErrorFlag) {
-		size_t offset = _offset;
-		bool rtn = Get<T>(pNum, bigEndianFlag, exceedErrorFlag);
-		_offset = offset;
-		return rtn;
-	}
 #endif
+	bool PackStay(const char* format, const ValueList& valListArg);
+	Value* UnpackStay(const char* format, const ValueList& valListArg, bool exceedErrorFlag);
+	template<typename T, bool bigEndianFlag> bool PutStay(T num);
+	template<typename T, bool bigEndianFlag> bool GetStay(T* pNum);
 public:
-#if 0
 	virtual Pointer* Clone() const = 0;
-	//virtual Object* GetTarget() const = 0;
+	//virtual Value& GetTarget() const = 0;
 	virtual const UInt8* GetPointerC() const = 0;
 	virtual UInt8* GetWritablePointerC() const = 0;
 	virtual size_t GetEntireSize() const = 0;
 	virtual bool IsWritable() const = 0;
-#endif
 public:
 	size_t CalcHash() const { return reinterpret_cast<size_t>(this); }
 	bool IsIdentical(const Pointer& other) const { return this == &other; }
@@ -67,6 +55,22 @@ public:
 	bool IsLessThan(const Pointer& other) const { return this < &other; }
 	String ToString(const StringStyle& ss = StringStyle::Empty) const;
 };
+
+template<typename T, bool bigEndianFlag> bool Pointer::PutStay(T num)
+{
+	size_t offset = _offset;
+	bool rtn = Put<T, bigEndianFlag>(num);
+	_offset = offset;
+	return rtn;
+}
+
+template<typename T, bool bigEndianFlag> bool Pointer::GetStay(T* pNum)
+{
+	size_t offset = _offset;
+	bool rtn = Get<T>(pNum);
+	_offset = offset;
+	return rtn;
+}
 
 }
 
--- src/lib/Pointer.cpp
+++ src/lib/Pointer.cpp
@@ -9,6 +9,22 @@ namespace Gurax {
 //------------------------------------------------------------------------------
 // Pointer
 //------------------------------------------------------------------------------
+bool Pointer::PackStay(const char* format, const ValueList& valListArg)
+{
+	size_t offset = _offset;
+	if (!Pack(format, valListArg)) return false;
+	_offset = offset;
+	return true;
+}
+
+Value* Pointer::UnpackStay(const char* format, const ValueList& valListArg, bool exceedErrorFlag)
+{
+	size_t offset = _offset;
+	RefPtr<Value> pValue(Unpack(format, valListArg, exceedErrorFlag));
+	_offset = offset;
+	return pValue.release();
+}
+
 String Pointer::ToString(const StringStyle& ss) const
 {
 	return "Pointer";
