a9430619dbb4919db6d8da1d6fcb638fe0403853
ypsitau <ypsitau@nifty.com>
2019-12-24 11:48:16 +0900

    implementing BZLibHelper

--- src/module-bzip2/BZLibHelper.h
+++ src/module-bzip2/BZLibHelper.h
@@ -49,16 +49,14 @@ public:
 		return true;
 	}
 	virtual const char* GetName() const override {
-		return (_pStream.IsNull())? "(invalid)" : _pStream->GetName();
+		return _pStream? _pStream->GetName() : "(invalid)";
 	}
 	virtual const char* GetIdentifier() const override {
-		return (_pStream.IsNull())? nullptr : _pStream->GetIdentifier();
+		return _pStream? _pStream->GetIdentifier() : nullptr;
 	}
 	virtual bool DoClose() override { return true; }
-	virtual size_t Write(const void* buff, size_t len) override {
-		return 0;
-	}
-	virtual size_t Read(void* buff, size_t bytes) override {
+	virtual size_t DoWrite(const void* buff, size_t len) override { return 0; }
+	virtual size_t DoRead(void* buff, size_t bytes) override {
 		size_t bytesRead = 0;
 		char* buffp = reinterpret_cast<char*>(buff);
 		bool continueFlag = true;
@@ -78,7 +76,7 @@ public:
 							Error::Issue(ErrorType::IOError, "unexpected end of bzip stream");
 							return 0;
 						}
-						if (_bytesSrc != InvalidSize) {
+						if (_bytesSrc != -1) {
 							_bytesSrc -= bytesRead;
 						}
 						_bzstrm.avail_in = static_cast<unsigned int>(bytesRead);
@@ -112,24 +110,16 @@ public:
 		}
 		return bytesRead;
 	}
-	virtual bool Seek(long offset, size_t offsetPrev, SeekMode seekMode) override {
-		if (seekMode == SeekSet) {
-			if (static_cast<size_t>(offset) >= offsetPrev) {
-				size_t bytesToRead = static_cast<size_t>(offset) - offsetPrev;
-				if (bytesToRead == 0) return true;
-				return DoRead(nullptr, bytesToRead) == static_cast<size_t>(bytesToRead);
-			}
-		} else if (seekMode == SeekCur) {
-			if (offset == 0) {
-				return true;
-			} else if (offset > 0) {
-				return DoRead(nullptr, offset) == static_cast<size_t>(offset);
-			}
+	virtual bool DoSeek(size_t offset, size_t offsetPrev) override {
+		if (offset >= offsetPrev) {
+			size_t bytesToRead = offset - offsetPrev;
+			if (bytesToRead == 0) return true;
+			return DoRead(nullptr, bytesToRead) == bytesToRead;
 		}
 		Error::Issue(ErrorType::UnimplementedError, "backward seeking is not supported");
 		return false;
 	}
-	virtual bool Flush() override { return true; }
+	virtual bool DoFlush() override { return true; }
 };
 
 //------------------------------------------------------------------------------
@@ -140,19 +130,16 @@ private:
 	RefPtr<Stream> _pStream;
 	RefPtr<Memory> _pMemory;
 	size_t _bytesBuff;
-	size_t _offsetOut;
 	bz_stream _bzstrm;
 	char* _buffOut;
 	char* _buffIn;
 public:
 	Stream_Compressor(Stream* pStream, size_t bytesBuff = 32768) :
 			Stream(Flag::Writable), _pStream(pStream),
-			_bytesBuff(bytesBuff), _offsetOut(0), _buffOut(nullptr), _buffIn(nullptr) {
+			_bytesBuff(bytesBuff), _buffOut(nullptr), _buffIn(nullptr) {
 		SetCodec(pStream->GetCodec().Duplicate());
 	}
-	~Stream_Compressor() {
-		Close();
-	}
+	~Stream_Compressor() { Close(); }
 	bool Initialize(int blockSize100k, int verbosity, int workFactor) {
 		::memset(&_bzstrm, 0x00, sizeof(_bzstrm));
 		_bzstrm.bzalloc = nullptr;
@@ -170,13 +157,13 @@ public:
 		return true;
 	}
 	virtual const char* GetName() const override {
-		return (_pStream.IsNull())? "(invalid)" : _pStream->GetName();
+		return _pStream? _pStream->GetName() : "(invalid)";
 	}
 	virtual const char* GetIdentifier() const override {
-		return (_pStream.IsNull())? nullptr : _pStream->GetIdentifier();
+		return _pStream? _pStream->GetIdentifier() : nullptr;
 	}
-	virtual size_t Write(const void* buff, size_t len) override {
-		if (_pStream.IsNull()) return 0;
+	virtual size_t DoWrite(const void* buff, size_t len) override {
+		if (!_pStream) return 0;
 		_bzstrm.next_in = reinterpret_cast<char*>(const_cast<void*>(buff));
 		_bzstrm.avail_in = static_cast<unsigned int>(len);
 		while (_bzstrm.avail_in > 0) {
@@ -194,9 +181,9 @@ public:
 		}
 		return len;
 	}
-	virtual bool Flush() override { return true; }
-	virtual bool Close() override {
-		if (_pStream.IsNull()) return true;
+	virtual bool DoFlush() override { return true; }
+	virtual bool DoClose() override {
+		if (!_pStream) return true;
 		for (;;) {
 			if (_bzstrm.avail_out == 0) {
 				_pStream->Write(_buffOut, _bytesBuff);
@@ -217,16 +204,13 @@ public:
 		}
 		::BZ2_bzCompressEnd(&_bzstrm);
 		if (Error::IsIssued()) return false;
-		bool rtn = _pStream->Flush();
 		_pStream.reset(nullptr);
-		return rtn;
+		return true;
 	}
-	virtual size_t Read(void* buff, size_t bytes) override {
+	virtual size_t DoRead(void* buff, size_t bytes) override {
 		return 0;
 	}
-	virtual bool Seek(long offset, size_t offsetPrev, SeekMode seekMode) override {
-		return false;
-	}
+	virtual bool DoSeek(size_t offset, size_t offsetPrev) override { return false; }
 };
 
 }
--- src/module-bzip2/Util.cpp
+++ src/module-bzip2/Util.cpp
@@ -2,6 +2,7 @@
 // Util.cpp
 //==============================================================================
 #include "stdafx.h"
+#include "BZLibHelper.h"
 
 Gurax_BeginModuleScope(bzip2)
 Gurax_EndModuleScope(bzip2)
