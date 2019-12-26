ac52cf9f6a16a95cc2087116856212ce0f3f8ba7
ypsitau <ypsitau@nifty.com>
2019-12-26 20:38:40 +0900

    added skeleton for hash module

---
+++ src/module-hash/Util.h
@@ -0,0 +1,11 @@
+//==============================================================================
+// Util.h
+//==============================================================================
+#ifndef GURAX_MODULE_HASH_UTIL_H
+#define GURAX_MODULE_HASH_UTIL_H
+#include <gurax.h>
+
+Gurax_BeginModuleScope(hash)
+Gurax_EndModuleScope(hash)
+
+#endif
---
+++ src/module-hash/module-hash.h
@@ -0,0 +1,12 @@
+//==============================================================================
+// module-hash.h
+//==============================================================================
+#ifndef GURAX_MODULE_HASH_H
+#define GURAX_MODULE_HASH_H
+#include <gurax.h>
+#include "Util.h"
+
+Gurax_BeginModuleHeader(hash)
+Gurax_EndModuleHeader(hash)
+
+#endif
