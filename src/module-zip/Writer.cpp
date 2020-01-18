35d253b518e2d6cf8ffd9f96e1e654e2fddcd493
ypsitau <ypsitau@nifty.com>
2020-01-18 20:47:01 +0900

    implementing zip module

--- src/module-zip/Writer.cpp
+++ src/module-zip/Writer.cpp
@@ -19,46 +19,39 @@ bool Writer::Add(Stream& streamSrc, const char* fileName, UInt16 compressionMeth
 	if (pStat) {
 		pDateTime.reset(pStat->GetDateTimeM().Reference());
 	} else {
-		pDateTime.reset();
+		pDateTime.reset(OAL::CreateDateTimeCur(false));
 	}
-#if 0
-	Stream::Attribute attr;
-	if (streamSrc.GetExtribute(attr)) {
-		dt = attr.mtime;
-	} else {
-		dt = OAL::GetCurDateTime(false);
-	}
-	UInt16 lastModFileTime = GetDosTime(dt);
-	UInt16 lastModFileDate = GetDosDate(dt);
+	UInt16 lastModFileTime = GetDosTime(*pDateTime);
+	UInt16 lastModFileDate = GetDosDate(*pDateTime);
 	UInt32 compressedSize = 0;
 	UInt32 uncompressedSize = 0;
 	UInt32 externalFileAttributes = (1 << 5);
-	UInt32 relativeOffsetOfLocalHeader =
-							static_cast<UInt32>(_pStreamDst->Tell());
+	UInt32 relativeOffsetOfLocalHeader = static_cast<UInt32>(_pStreamDst->GetOffset());
+	std::unique_ptr<CentralFileHeader> pCentralFileHeader(new CentralFileHeader());
 	do {
-		CentralFileHeader::Fields &fields = pHdr->GetFields();
-		Gura_PackUInt16(fields.VersionMadeBy,				version);
-		Gura_PackUInt16(fields.VersionNeededToExtract,		version);
-		Gura_PackUInt16(fields.GeneralPurposeBitFlag,		generalPurposeBitFlag);
-		Gura_PackUInt16(fields.CompressionMethod,			compressionMethod);
-		Gura_PackUInt16(fields.LastModFileTime,				lastModFileTime);
-		Gura_PackUInt16(fields.LastModFileDate,				lastModFileDate);
-		Gura_PackUInt32(fields.Crc32,						0x00000000);
-		Gura_PackUInt32(fields.CompressedSize,				compressedSize);
-		Gura_PackUInt32(fields.UncompressedSize,			uncompressedSize);
-		Gura_PackUInt16(fields.FileNameLength,				0x0000);
-		Gura_PackUInt16(fields.ExtraFieldLength,			0x0000);
-		Gura_PackUInt16(fields.FileCommentLength,			0x0000);
-		Gura_PackUInt16(fields.DiskNumberStart,				0x0000);
-		Gura_PackUInt16(fields.InternalFileAttributes,		0x0000);
-		Gura_PackUInt32(fields.ExternalFileAttributes,		externalFileAttributes);
-		Gura_PackUInt32(fields.RelativeOffsetOfLocalHeader,	relativeOffsetOfLocalHeader);
-		pHdr->SetFileName(fileName);
-		if (!pHdr->WriteAsLocalFileHeader(sig, *_pStreamDst)) return false;
+		CentralFileHeader::Fields& fields = pCentralFileHeader->GetFields();
+		Gurax_PackUInt16(fields.VersionMadeBy,					version);
+		Gurax_PackUInt16(fields.VersionNeededToExtract,			version);
+		Gurax_PackUInt16(fields.GeneralPurposeBitFlag,			generalPurposeBitFlag);
+		Gurax_PackUInt16(fields.CompressionMethod,				compressionMethod);
+		Gurax_PackUInt16(fields.LastModFileTime,				lastModFileTime);
+		Gurax_PackUInt16(fields.LastModFileDate,				lastModFileDate);
+		Gurax_PackUInt32(fields.Crc32,							0x00000000);
+		Gurax_PackUInt32(fields.CompressedSize,					compressedSize);
+		Gurax_PackUInt32(fields.UncompressedSize,				uncompressedSize);
+		Gurax_PackUInt16(fields.FileNameLength,					0x0000);
+		Gurax_PackUInt16(fields.ExtraFieldLength,				0x0000);
+		Gurax_PackUInt16(fields.FileCommentLength,				0x0000);
+		Gurax_PackUInt16(fields.DiskNumberStart,				0x0000);
+		Gurax_PackUInt16(fields.InternalFileAttributes,			0x0000);
+		Gurax_PackUInt32(fields.ExternalFileAttributes,			externalFileAttributes);
+		Gurax_PackUInt32(fields.RelativeOffsetOfLocalHeader,	relativeOffsetOfLocalHeader);
+		pCentralFileHeader->SetFileName(fileName);
+		if (!pCentralFileHeader->WriteAsLocalFileHeader(*_pStreamDst)) return false;
 	} while (0);
-	AutoPtr<Stream> pStreamOut;
-	size_t offsetDst = _pStreamDst->Tell();
-	size_t offsetSrc = streamSrc.Tell();
+	RefPtr<Stream> pStreamOut;
+	size_t offsetDst = _pStreamDst->GetOffset();
+	size_t offsetSrc = streamSrc.GetOffset();
 	if (compressionMethod == CompressionMethod::Store) {
 		pStreamOut.reset(Stream::Reference(_pStreamDst.get()));
 	} else if (compressionMethod == CompressionMethod::Shrink) {
@@ -76,22 +69,19 @@ bool Writer::Add(Stream& streamSrc, const char* fileName, UInt16 compressionMeth
 	} else if (compressionMethod == CompressionMethod::Factor1) {
 		// unsupported
 	} else if (compressionMethod == CompressionMethod::Deflate) {
-		AutoPtr<ZLib::Stream_Deflater> pStreamDeflater(
-			new ZLib::Stream_Deflater(env, Stream::Reference(_pStreamDst.get())));
-		if (!pStreamDeflater->Initialize(sig, Z_DEFAULT_COMPRESSION,
-					-MAX_WBITS, memLevel, Z_DEFAULT_STRATEGY)) return false;
-		pStreamOut.reset(pStreamDeflater.release());
+		RefPtr<ZLib::Stream_Writer> pStreamWriter(new ZLib::Stream_Writer(_pStreamDst->Reference()));
+		if (!pStreamWriter->Initialize(Z_DEFAULT_COMPRESSION,
+									   -MAX_WBITS, memLevel, Z_DEFAULT_STRATEGY)) return false;
+		pStreamOut.reset(pStreamWriter.release());
 	} else if (compressionMethod == CompressionMethod::Deflate64) {
 		// unsupported
 	} else if (compressionMethod == CompressionMethod::PKWARE) {
 		// unsupported
 	} else if (compressionMethod == CompressionMethod::BZIP2) {
-		AutoPtr<BZLib::Stream_Compressor> pStreamCompressor(
-			new BZLib::Stream_Compressor(env, Stream::Reference(_pStreamDst.get())));
+		RefPtr<BZLib::Stream_Writer> pStreamWriter(new BZLib::Stream_Writer(_pStreamDst->Reference()));
 		int blockSize100k = 9, verbosity = 0, workFactor = 0;
-		if (!pStreamCompressor->Initialize(sig,
-							blockSize100k, verbosity, workFactor)) return false;
-		pStreamOut.reset(pStreamCompressor.release());
+		if (!pStreamWriter->Initialize(blockSize100k, verbosity, workFactor)) return false;
+		pStreamOut.reset(pStreamWriter.release());
 	} else if (compressionMethod == CompressionMethod::LZMA) {
 		// unsupported
 	} else if (compressionMethod == CompressionMethod::TERSA) {
@@ -103,42 +93,40 @@ bool Writer::Add(Stream& streamSrc, const char* fileName, UInt16 compressionMeth
 	} else if (compressionMethod == CompressionMethod::PPMd) {
 		// unsupported
 	}
-	if (pStreamOut.IsNull()) {
-		sig.SetError(ERR_IOError,
-				"unsupported compression method %d", compressionMethod);
+	if (!pStreamOut) {
+		Error::Issue(ErrorType::FormatError, "unsupported compression method %d", compressionMethod);
 		return false;
 	}
 	CRC32 crc32;
-	AutoPtr<Memory> pMemory(new MemoryHeap(32768));
-	void *buff = pMemory->GetPointer();
+	RefPtr<Memory> pMemory(new MemoryHeap(32768));
+	void* buff = pMemory->GetPointer();
 	for (;;) {
-		size_t bytesRead = streamSrc.Read(sig, buff, pMemory->GetSize());
-		if (sig.IsSignalled()) break;
+		size_t bytesRead = streamSrc.Read(buff, pMemory->GetSize());
+		if (Error::IsIssued()) break;
 		if (bytesRead == 0) break;
 		crc32.Update(buff, bytesRead);
-		pStreamOut->Write(sig, buff, bytesRead);
-		if (sig.IsSignalled()) break;
+		pStreamOut->Write(buff, bytesRead);
+		if (Error::IsIssued()) break;
 	}
-	pStreamOut->Flush(sig);
-	if (sig.IsSignalled()) return false;
+	pStreamOut->Flush();
+	if (Error::IsIssued()) return false;
 	UInt32 crc32num = crc32.GetResult();
-	compressedSize = static_cast<UInt32>(_pStreamDst->Tell() - offsetDst);
-	uncompressedSize = static_cast<UInt32>(streamSrc.Tell() - offsetSrc);
+	compressedSize = static_cast<UInt32>(_pStreamDst->GetOffset() - offsetDst);
+	uncompressedSize = static_cast<UInt32>(streamSrc.GetOffset() - offsetSrc);
 	do {
 		DataDescriptor desc;
 		DataDescriptor::Fields &fields = desc.GetFields();
-		Gura_PackUInt32(fields.Crc32,			crc32num);
-		Gura_PackUInt32(fields.CompressedSize,	compressedSize);
-		Gura_PackUInt32(fields.UncompressedSize,	uncompressedSize);
-		if (!desc.Write(sig, *_pStreamDst)) return false;
+		Gurax_PackUInt32(fields.Crc32,			crc32num);
+		Gurax_PackUInt32(fields.CompressedSize,	compressedSize);
+		Gurax_PackUInt32(fields.UncompressedSize,	uncompressedSize);
+		if (!desc.Write(*_pStreamDst)) return false;
 	} while (0);
 	do {
-		CentralFileHeader::Fields &fields = pHdr->GetFields();
-		Gura_PackUInt32(fields.Crc32,			crc32num);
-		Gura_PackUInt32(fields.CompressedSize,	compressedSize);
-		Gura_PackUInt32(fields.UncompressedSize,	uncompressedSize);
+		CentralFileHeader::Fields &fields = pCentralFileHeader->GetFields();
+		Gurax_PackUInt32(fields.Crc32,			crc32num);
+		Gurax_PackUInt32(fields.CompressedSize,	compressedSize);
+		Gurax_PackUInt32(fields.UncompressedSize,	uncompressedSize);
 	} while (0);
-#endif
 	return true;
 }
 
@@ -159,14 +147,14 @@ bool Writer::Finish()
 	do {
 		EndOfCentralDirectoryRecord rec;
 		EndOfCentralDirectoryRecord::Fields &fields = rec.GetFields();
-		Gura_PackUInt16(fields.NumberOfThisDisk,									0x0000);
-		Gura_PackUInt16(fields.NumberOfTheDiskWithTheStartOfTheCentralDirectory,	0x0000);
-		Gura_PackUInt16(fields.TotalNumberOfEntriesInTheCentralDirectoryOnThisDisk,	nCentralFileHeaders);
-		Gura_PackUInt16(fields.TotalNumberOfEntriesInTheCentralDirectory,			nCentralFileHeaders);
-		Gura_PackUInt32(fields.SizeOfTheCentralDirectory,							sizeOfTheCentralDirectory);
-		Gura_PackUInt32(fields.OffsetOfStartOfCentralDirectoryWithRespectToTheStartingDiskNumber,
+		Gurax_PackUInt16(fields.NumberOfThisDisk,									0x0000);
+		Gurax_PackUInt16(fields.NumberOfTheDiskWithTheStartOfTheCentralDirectory,	0x0000);
+		Gurax_PackUInt16(fields.TotalNumberOfEntriesInTheCentralDirectoryOnThisDisk,	nCentralFileHeaders);
+		Gurax_PackUInt16(fields.TotalNumberOfEntriesInTheCentralDirectory,			nCentralFileHeaders);
+		Gurax_PackUInt32(fields.SizeOfTheCentralDirectory,							sizeOfTheCentralDirectory);
+		Gurax_PackUInt32(fields.OffsetOfStartOfCentralDirectoryWithRespectToTheStartingDiskNumber,
 															offsetOfCentralDirectory);
-		Gura_PackUInt16(fields.ZIPFileCommentLength,								0x0000);
+		Gurax_PackUInt16(fields.ZIPFileCommentLength,								0x0000);
 		if (!rec.Write(_sig, *_pStreamDst)) return false;
 	} while (0);
 	_pStreamDst->Close();
