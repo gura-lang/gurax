76369a3a9e37bf08d6a8ad5db020316d11b9beb5
ypsitau <ypsitau@nifty.com>
2020-01-28 10:37:29 +0900

    implementing Directory

--- src/unittest/test-Directory.cpp
+++ src/unittest/test-Directory.cpp
@@ -7,6 +7,111 @@
 
 namespace Gurax {
 
+//------------------------------------------------------------------------------
+// Directory_CustomContainer
+//------------------------------------------------------------------------------
+class GURAX_DLLDECLARE Directory_CustomContainer : public Directory {
+public:
+	class Factory : public Referable {
+	public:
+		Gurax_DeclareReferable(Factory);
+	protected:
+		String _name;
+	public:
+		Factory(String name) : _name(name) {}
+	protected:
+		virtual ~Factory() = default;
+	public:
+		virtual Directory* CreateDirectory() const = 0;
+	};
+	class FactoryList : public std::vector<Factory*> {};
+	class FactoryOwner : public FactoryList {
+	public:
+		~FactoryOwner() { Clear(); }
+		void Clear() {
+			for (Factory* pFactory : *this) Factory::Delete(pFactory);
+			clear();
+		}
+	};
+public:
+	FactoryOwner _factoryOwner;
+	size_t _idxChild;
+public:
+	Directory_CustomContainer(Type type, char sep, bool caseFlag) :
+		Directory(type, sep, caseFlag), _idxChild(0) {}
+	Directory_CustomContainer(String name, Type type, char sep, bool caseFlag) :
+		Directory(name, type, sep, caseFlag), _idxChild(0) {}
+public:
+	bool AddChildInTree(const char* pathName, RefPtr<Factory> pFactoryChild);
+protected:
+	virtual void DoRewindChild() override;
+	virtual Directory* DoNextChild() override;
+};
+
+//------------------------------------------------------------------------------
+// Directory_CustomContainer
+//------------------------------------------------------------------------------
+bool Directory_CustomContainer::AddChildInTree(const char* pathName, RefPtr<Factory> pDirectoryChild)
+{
+#if 0
+	String driveLetter;
+	String prefix;
+	StringList fields;
+	PathName(pathName).SplitIntoFields(&driveLetter, &prefix, fields);
+	Directory_CustomContainer* pDirectoryParent = this;
+	auto pField = fields.begin();
+	if (pField == fields.end()) {
+		Error::Issue(ErrorType::PathError, "invalid path name");
+		return false;
+	}
+	for ( ; pField + 1 != fields.end() && !(pField + 1)->empty(); pField++) {
+		const String& field = *pField;
+		DirectoryOwner& directoryOwner = pDirectoryParent->GetDirectoryOwner();
+		Directory* pDirectory = directoryOwner.FindByName(field.c_str());
+		if (!pDirectory) {
+			auto pDirectoryNew = new Directory_CustomContainer(
+				field, Type::Container, pDirectoryParent->GetSep(), pDirectoryParent->IsCaseSensitive());
+			pDirectoryNew->SetDirectoryParent(*pDirectoryParent);
+			directoryOwner.push_back(pDirectoryNew);
+			pDirectoryParent = pDirectoryNew;
+		} else if (!pDirectory->IsCustomContainer()) {
+			Error::Issue(ErrorType::PathError, "invalid path name");
+			return false;
+		} else {
+			pDirectoryParent = dynamic_cast<Directory_CustomContainer*>(pDirectory);
+		}
+	}
+	const String& field = *pField;
+	DirectoryOwner& directoryOwner = pDirectoryParent->GetDirectoryOwner();
+	pDirectoryChild->SetDirectoryParent(*pDirectoryParent);
+	pDirectoryChild->SetName(field);
+	auto ppDirectoryFound = directoryOwner.FindIteratorByName(field.c_str());
+	if (ppDirectoryFound == directoryOwner.end()) {
+		directoryOwner.push_back(pDirectoryChild.release());
+	} else {
+		if (pDirectoryChild->IsCustomContainer() && (*ppDirectoryFound)->IsCustomContainer()) {
+			dynamic_cast<Directory_CustomContainer&>(*pDirectoryChild).SetDirectoryOwner(
+				dynamic_cast<Directory_CustomContainer*>(*ppDirectoryFound)->GetDirectoryOwner().Reference());
+		}
+		Directory::Delete(*ppDirectoryFound);
+		*ppDirectoryFound = pDirectoryChild.release();
+	}
+#endif
+	return true;
+}
+
+void Directory_CustomContainer::DoRewindChild()
+{
+	_idxChild = 0;
+}
+
+Directory* Directory_CustomContainer::DoNextChild()
+{
+	//if (_idxChild >= _pDirectoryOwner->size()) return nullptr;
+	//return (*_pDirectoryOwner)[_idxChild++]->Reference();
+	return nullptr;
+}
+
 void PrintDirectory(Directory& directory, int indentLevel)
 {
 	::printf("%*s%s\n", indentLevel * 2, "", directory.GetName());
