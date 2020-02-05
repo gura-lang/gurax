02399efd2310ab459d5eab588632447173cde653
ypsitau <ypsitau@nifty.com>
2020-02-05 12:58:13 +0900

    implementing Image

--- include/gurax/Image.h
+++ include/gurax/Image.h
@@ -15,12 +15,121 @@ public:
 	// Referable declaration
 	Gurax_DeclareReferable(Image);
 public:
-	
+	enum class Format { None, BGR, BGRA };
+	enum ScanDir {
+		None, LeftTopHorz, LeftTopVert, RightTopHorz, RightTopVert,
+		LeftBottomHorz, LeftBottomVert, RightBottomHorz, RightBottomVert,
+	};
+	class PixelBGR {
+	protected:
+		UInt8* _p;
+	public:
+		static const size_t bytesPerPixel = 3;
+	public:
+		// Constructor
+		PixelBGR(UInt8* p) : _p(p) {}
+		// Copy constructor/operator
+		PixelBGR(const PixelBGR& src) : _p(src._p) {}
+		PixelBGR& operator=(const PixelBGR& src) { _p = src._p; return *this; }
+		// Move constructor/operator
+		PixelBGR(const PixelBGR&& src) : _p(src._p) {}
+		PixelBGR& operator=(const PixelBGR&& src) noexcept { _p = src._p; return *this; }
+	public:
+		UInt8* GetPointer() { return _p; }
+		const UInt8* GetPointer() const { return _p; }
+		void Forward() { _p += bytesPerPixel; }
+		void Forward(size_t n) { _p += bytesPerPixel * n; }
+		template<typename T_Pixel> void Inject(const T_Pixel& pixel) {
+			*(_p + 0) = pixel.GetB(), *(_p + 1) = pixel.GetG(), *(_p + 2) = pixel.GetR();
+		}
+		template<typename T_Pixel> void InjectN(const T_Pixel& pixel, size_t n) {}
+		void SetColor(const Color &color) {
+			*(_p + 0) = color.GetB(), *(_p + 1) = color.GetG(), *(_p + 2) = color.GetR();
+		}
+		void SetColorN(const Color &color, size_t n) {
+			UInt8* pDst = _p;
+			for (size_t i = 0; i < n; i++) {
+				*pDst++ = color.GetB(), *pDst++ = color.GetG(), *pDst++ = color.GetR();
+			}
+		}
+		Color GetColor() const { return Color(*(_p + 2), *(_p + 1), *(_p + 0), 0xff); }
+		void SetR(UInt8 r) { *(_p + 2) = r; }
+		void SetG(UInt8 g) { *(_p + 1) = g; }
+		void SetB(UInt8 b) { *(_p + 0) = b; }
+		void SetA(UInt8 a) {}
+		static void SetR(UInt8* p, UInt8 r) { *(p + 2) = r; }
+		static void SetG(UInt8* p, UInt8 g) { *(p + 1) = g; }
+		static void SetB(UInt8* p, UInt8 b) { *(p + 0) = b; }
+		static void SetA(UInt8* p, UInt8 a) {}
+		UInt8 GetR() const { return *(_p + 2); }
+		UInt8 GetG() const { return *(_p + 1); }
+		UInt8 GetB() const { return *(_p + 0); }
+		UInt8 GetA() const { return 0xff; }
+		static UInt8 GetR(const UInt8* p) { return *(p + 2); }
+		static UInt8 GetG(const UInt8* p) { return *(p + 1); }
+		static UInt8 GetB(const UInt8* p) { return *(p + 0); }
+		static UInt8 GetA(const UInt8* p) { return 0xff; }
+	};
+	class PixelBGRA {
+	protected:
+		UInt8* _p;
+	public:
+		static const size_t bytesPerPixel = 4;
+	public:
+		// Constructor
+		PixelBGRA(UInt8* p) : _p(p) {}
+		// Copy constructor/operator
+		PixelBGRA(const PixelBGRA& src) : _p(src._p) {}
+		PixelBGRA& operator=(const PixelBGRA& src) { _p = src._p; return *this; }
+		// Move constructor/operator
+		PixelBGRA(const PixelBGRA&& src) : _p(src._p) {}
+		PixelBGRA& operator=(const PixelBGRA&& src) noexcept { _p = src._p; return *this; }
+	public:
+		UInt8* GetPointer() { return _p; }
+		const UInt8* GetPointer() const { return _p; }
+		void Forward() { _p += bytesPerPixel; }
+		void Forward(size_t n) { _p += bytesPerPixel * n; }
+		template<typename T_Pixel> void Inject(const T_Pixel& pixel) {
+			*(_p + 0) = pixel.GetB(), *(_p + 1) = pixel.GetG(), *(_p + 2) = pixel.GetR(), *(_p + 3) = pixel.GetA();
+		}
+		template<typename T_Pixel> void InjectN(const T_Pixel& pixel, size_t n) {}
+		void SetColor(const Color &color) {
+			*(_p + 0) = color.GetB(), *(_p + 1) = color.GetG(), *(_p + 2) = color.GetR();
+			*(_p + 3) = color.GetA();
+		}
+		void SetColorN(const Color &color, size_t n) {
+			UInt8* pDst = _p;
+			for (size_t i = 0; i < n; i++) {
+				*pDst++ = color.GetB(), *pDst++ = color.GetG(), *pDst++ = color.GetR(), *pDst++ = color.GetA();
+			}
+		}
+		Color GetColor() const { return Color(*(_p + 2), *(_p + 1), *(_p + 0), *(_p + 3)); }
+		void SetR(UInt8 r) { *(_p + 2) = r; }
+		void SetG(UInt8 g) { *(_p + 1) = g; }
+		void SetB(UInt8 b) { *(_p + 0) = b; }
+		void SetA(UInt8 a) { *(_p + 3) = a; }
+		static void SetR(UInt8* p, UInt8 r) { *(p + 2) = r; }
+		static void SetG(UInt8* p, UInt8 g) { *(p + 1) = g; }
+		static void SetB(UInt8* p, UInt8 b) { *(p + 0) = b; }
+		static void SetA(UInt8* p, UInt8 a) { *(p + 3) = a; }
+		UInt8 GetR() const { return *(_p + 2); }
+		UInt8 GetG() const { return *(_p + 1); }
+		UInt8 GetB() const { return *(_p + 0); }
+		UInt8 GetA() const { return *(_p + 3); }
+		static UInt8 GetR(const UInt8* p) { return *(p + 2); }
+		static UInt8 GetG(const UInt8* p) { return *(p + 1); }
+		static UInt8 GetB(const UInt8* p) { return *(p + 0); }
+		static UInt8 GetA(const UInt8* p) { return *(p + 3); }
+	};
 protected:
 	RefPtr<Memory> _pMemory;
+	RefPtr<Palette> _pPalette;	// may be nullptr
+	Format _format;
+	size_t _width, _height;
 public:
 	// Constructor
-	Image() {}
+	Image(Format format, Memory* pMemory, size_t width, size_t height) :
+		_pMemory(pMemory), _format(format), _width(width), _height(height) {}
 	// Copy constructor/operator
 	Image(const Image& src) = delete;
 	Image& operator=(const Image& src) = delete;
@@ -29,6 +138,10 @@ public:
 	Image& operator=(Image&& src) noexcept = delete;
 protected:
 	~Image() = default;
+public:
+	UInt8* GetPointer() { return reinterpret_cast<UInt8*>(_pMemory->GetPointer()); }
+	template<typename T_Pixel> void FillTmpl(const Color& color);
+	void Fill(const Color& color);
 public:
 	size_t CalcHash() const { return reinterpret_cast<size_t>(this); }
 	bool IsIdentical(const Image& image) const { return this == &image; }
--- src/lib/Image.cpp
+++ src/lib/Image.cpp
@@ -6,69 +6,68 @@
 
 namespace Gurax {
 
-class PixelBGR {
-protected:
-	UInt8* _p;
-public:
-	static const size_t bytes = 3;
-public:
-	PixelBGR(UInt8* p) : _p(p) {}
-public:
-	void InjectBGR(const UInt8* p) {
-		*(_p + 2) = *(p + 2); *(_p + 1) = *(p + 1); *(_p + 0) = *(p + 0);
-	}
-	void InjectBGRA(const UInt8* p) {
-		*(_p + 2) = *(p + 2); *(_p + 1) = *(p + 1); *(_p + 0) = *(p + 0);
-	}
-	void SetColor(const Color &color) {
-		*(_p + 2) = color.GetR(); *(_p + 1) = color.GetG(); *(_p + 0) = color.GetB();
-	}
-	Color GetColor() const { return Color(*(_p + 2), *(_p + 1), *(_p + 0), 0xff); }
-	void SetR(UInt8 r) { *(_p + 2) = r; }
-	void SetG(UInt8 g) { *(_p + 1) = g; }
-	void SetB(UInt8 b) { *(_p + 0) = b; }
-	void SetA(UInt8 a) {}
-	UInt8 GetR() const { return *(_p + 2); }
-	UInt8 GetG() const { return *(_p + 1); }
-	UInt8 GetB() const { return *(_p + 0); }
-	UInt8 GetA() const { return 0xff; }
-};
-
-class PixelBGRA {
-protected:
-	UInt8* _p;
-public:
-	static const size_t bytes = 4;
-public:
-	PixelBGRA(UInt8* p) : _p(p) {}
-public:
-	void InjectBGR(const UInt8* p) {
-		*(_p + 2) = *(p + 2); *(_p + 1) = *(p + 1); *(_p + 0) = *(p + 0); *(_p + 3) = 0xff;
-	}
-	void InjectBGRA(const UInt8* p) {
-		*(_p + 2) = *(p + 2); *(_p + 1) = *(p + 1); *(_p + 0) = *(p + 0); *(_p + 3) = *(p + 3);
-	}
-	void SetColor(const Color &color) {
-		*(_p + 2) = color.GetR(); *(_p + 1) = color.GetG(); *(_p + 0) = color.GetB();
-		*(_p + 3) = color.GetA();
-	}
-	Color GetColor() const { return Color(*(_p + 2), *(_p + 1), *(_p + 0), *(_p + 3)); }
-	void SetR(UInt8 r) { *(_p + 2) = r; }
-	void SetG(UInt8 g) { *(_p + 1) = g; }
-	void SetB(UInt8 b) { *(_p + 0) = b; }
-	void SetA(UInt8 a) { *(_p + 3) = a; }
-	UInt8 GetR() const { return *(_p + 2); }
-	UInt8 GetG() const { return *(_p + 1); }
-	UInt8 GetB() const { return *(_p + 0); }
-	UInt8 GetA() const { return *(_p + 3); }
-};
-
 //------------------------------------------------------------------------------
 // Image
 //------------------------------------------------------------------------------
+template<typename T_Pixel> void Image::FillTmpl(const Color& color)
+{
+	if (_width == 0 || _height == 0) return;
+	size_t bytesPerLine = T_Pixel::bytesPerPixel * _width;
+	T_Pixel pixel(GetPointer());
+	pixel.SetColorN(color, _width);
+	const UInt8* pSrc = GetPointer();
+	UInt8* pDst = GetPointer() + bytesPerLine;
+	for (size_t i = 1; i < _height; i++, pDst += bytesPerLine, pSrc += bytesPerLine) {
+		::memcpy(pDst, pSrc, bytesPerLine);
+	}
+}
+
+void Image::Fill(const Color& color)
+{
+	switch (_format) {
+	case Format::BGR: FillTmpl<PixelBGR>(color); break;
+	case Format::BGRA: FillTmpl<PixelBGRA>(color); break;
+	default: break;
+	}
+}
+
 String Image::ToString(const StringStyle& ss) const
 {
 	return "";
+}
+
+//------------------------------------------------------------------------------
+// Image::PixelBGR
+//------------------------------------------------------------------------------
+template<> void Image::PixelBGR::InjectN<Image::PixelBGR>(const PixelBGR& pixel, size_t n)
+{
+	::memcpy(GetPointer(), pixel.GetPointer(), bytesPerPixel * n);
+}
+
+template<> void Image::PixelBGR::InjectN<Image::PixelBGRA>(const PixelBGRA& pixel, size_t n)
+{
+	UInt8* pDst = GetPointer();
+	const UInt8* pSrc = pixel.GetPointer();
+	for (size_t i = 0; i < n; i++) {
+		*pDst++ = *pSrc++, *pDst++ = *pSrc++, *pDst++ = *pSrc++, pSrc++;
+	}
+}
+
+//------------------------------------------------------------------------------
+// Image::PixelBGRA
+//------------------------------------------------------------------------------
+template<> void Image::PixelBGRA::InjectN<Image::PixelBGR>(const PixelBGR& pixel, size_t n)
+{
+	UInt8* pDst = GetPointer();
+	const UInt8* pSrc = pixel.GetPointer();
+	for (size_t i = 0; i < n; i++) {
+		*pDst++ = *pSrc++, *pDst++ = *pSrc++, *pDst++ = *pSrc++, *pDst++ = 0xff;
+	}
+}
+
+template<> void Image::PixelBGRA::InjectN<Image::PixelBGRA>(const PixelBGRA& pixel, size_t n)
+{
+	::memcpy(GetPointer(), pixel.GetPointer(), bytesPerPixel * n);
 }
 
 //------------------------------------------------------------------------------
