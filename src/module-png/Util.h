56fc8b9177c5fc5d0d04f120daa63074a383d00b
ypsitau <ypsitau@nifty.com>
2020-04-03 15:30:32 +0900

    updated util.modbuild

---
+++ src/module-png/Util.h
@@ -0,0 +1,11 @@
+//==============================================================================
+// Util.h
+//==============================================================================
+#ifndef GURAX_MODULE_PNG_UTIL_H
+#define GURAX_MODULE_PNG_UTIL_H
+#include <gurax.h>
+
+Gurax_BeginModuleScope(png)
+Gurax_EndModuleScope(png)
+
+#endif
---
+++ src/module-png/module-png.h
@@ -0,0 +1,12 @@
+//==============================================================================
+// module-png.h
+//==============================================================================
+#ifndef GURAX_MODULE_PNG_H
+#define GURAX_MODULE_PNG_H
+#include <gurax.h>
+#include "Util.h"
+
+Gurax_BeginModuleHeader(png)
+Gurax_EndModuleHeader(png)
+
+#endif
