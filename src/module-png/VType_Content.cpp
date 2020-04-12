7ea344852e582f1d3cb1ccf48409c068aa1de9fa
ypsitau <ypsitau@nifty.com>
2020-04-12 22:01:47 +0900

    implementing png module

--- src/module-png/VType_Content.cpp
+++ src/module-png/VType_Content.cpp
@@ -78,6 +78,21 @@ Gurax_ImplementMethod(Content, MethodSkeleton)
 //-----------------------------------------------------------------------------
 // Implementation of property
 //-----------------------------------------------------------------------------
+// png.Content#rowbytes
+Gurax_DeclareProperty_R(Content, rowbytes)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(Content, rowbytes)
+{
+	Content& c = GetValueThis(valueTarget).GetContent();
+	return new Value_Number(::png_get_rowbytes(c.png_ptr, c.info_ptr));
+}
+
 // png.Content#image_width
 Gurax_DeclareProperty_R(Content, image_width)
 {
@@ -108,6 +123,141 @@ Gurax_ImplementPropertyGetter(Content, image_height)
 	return new Value_Number(::png_get_image_height(c.png_ptr, c.info_ptr));
 }
 
+// png.Content#bit_depth
+Gurax_DeclareProperty_R(Content, bit_depth)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(Content, bit_depth)
+{
+	Content& c = GetValueThis(valueTarget).GetContent();
+	return new Value_Number(::png_get_bit_depth(c.png_ptr, c.info_ptr));
+}
+
+// png.Content#color_type
+Gurax_DeclareProperty_R(Content, color_type)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(Content, color_type)
+{
+	Content& c = GetValueThis(valueTarget).GetContent();
+	return new Value_Number(::png_get_color_type(c.png_ptr, c.info_ptr));
+}
+
+// png.Content#filter_type
+Gurax_DeclareProperty_R(Content, filter_type)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(Content, filter_type)
+{
+	Content& c = GetValueThis(valueTarget).GetContent();
+	return new Value_Number(::png_get_filter_type(c.png_ptr, c.info_ptr));
+}
+
+// png.Content#interlace_type
+Gurax_DeclareProperty_R(Content, interlace_type)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(Content, interlace_type)
+{
+	Content& c = GetValueThis(valueTarget).GetContent();
+	return new Value_Number(::png_get_interlace_type(c.png_ptr, c.info_ptr));
+}
+
+// png.Content#compression_type
+Gurax_DeclareProperty_R(Content, compression_type)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(Content, compression_type)
+{
+	Content& c = GetValueThis(valueTarget).GetContent();
+	return new Value_Number(::png_get_compression_type(c.png_ptr, c.info_ptr));
+}
+
+// png.Content#compression_x_pixels_per_meter
+Gurax_DeclareProperty_R(Content, x_pixels_per_meter)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(Content, x_pixels_per_meter)
+{
+	Content& c = GetValueThis(valueTarget).GetContent();
+	return new Value_Number(::png_get_x_pixels_per_meter(c.png_ptr, c.info_ptr));
+}
+
+// png.Content#y_pixels_per_meter
+Gurax_DeclareProperty_R(Content, y_pixels_per_meter)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(Content, y_pixels_per_meter)
+{
+	Content& c = GetValueThis(valueTarget).GetContent();
+	return new Value_Number(::png_get_y_pixels_per_meter(c.png_ptr, c.info_ptr));
+}
+
+// png.Content#pixels_per_meter
+Gurax_DeclareProperty_R(Content, pixels_per_meter)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(Content, pixels_per_meter)
+{
+	Content& c = GetValueThis(valueTarget).GetContent();
+	return new Value_Number(::png_get_pixels_per_meter(c.png_ptr, c.info_ptr));
+}
+
+// png.Content#pixel_aspect_ratio
+Gurax_DeclareProperty_R(Content, pixel_aspect_ratio)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(Content, pixel_aspect_ratio)
+{
+	Content& c = GetValueThis(valueTarget).GetContent();
+	return new Value_Number(::png_get_pixel_aspect_ratio(c.png_ptr, c.info_ptr));
+}
+
 //------------------------------------------------------------------------------
 // VType_Content
 //------------------------------------------------------------------------------
@@ -122,8 +272,18 @@ void VType_Content::DoPrepare(Frame& frameOuter)
 	// Assignment of method
 	Assign(Gurax_CreateMethod(Content, MethodSkeleton));
 	// Assignment of property
+	Assign(Gurax_CreateProperty(Content, rowbytes));
 	Assign(Gurax_CreateProperty(Content, image_width));
 	Assign(Gurax_CreateProperty(Content, image_height));
+	Assign(Gurax_CreateProperty(Content, bit_depth));
+	Assign(Gurax_CreateProperty(Content, color_type));
+	Assign(Gurax_CreateProperty(Content, filter_type));
+	Assign(Gurax_CreateProperty(Content, interlace_type));
+	Assign(Gurax_CreateProperty(Content, compression_type));
+	Assign(Gurax_CreateProperty(Content, x_pixels_per_meter));
+	Assign(Gurax_CreateProperty(Content, y_pixels_per_meter));
+	Assign(Gurax_CreateProperty(Content, pixels_per_meter));
+	Assign(Gurax_CreateProperty(Content, pixel_aspect_ratio));
 }
 
 //------------------------------------------------------------------------------
