351f6b2b866ed77d2bc1ef3014257d6385eee265
ypsitau <ypsitau@nifty.com>
2020-04-13 20:23:27 +0900

    added diff module

---
+++ src/module-diff/module-diff.h
@@ -0,0 +1,12 @@
+//==============================================================================
+// module-diff.h
+//==============================================================================
+#ifndef GURAX_MODULE_DIFF_H
+#define GURAX_MODULE_DIFF_H
+#include <gurax.h>
+#include "Util.h"
+
+Gurax_BeginModuleHeader(diff)
+Gurax_EndModuleHeader(diff)
+
+#endif
