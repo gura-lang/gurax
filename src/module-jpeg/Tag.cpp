7c56d48d5a450150c77f0bc81d47afa471c27d93
ypsitau <ypsitau@nifty.com>
2020-04-24 11:41:53 +0900

    implementing jpeg module

--- src/module-jpeg/Tag.cpp
+++ src/module-jpeg/Tag.cpp
@@ -8,52 +8,34 @@ Gurax_BeginModuleScope(jpeg)
 //------------------------------------------------------------------------------
 // Tag
 //------------------------------------------------------------------------------
-Tag::Tag(UInt16 tagId, UInt16 typeId, const Symbol* pSymbol, Value* pValue, Value* pValueCooked) :
-	_tagId(tagId), _typeId(typeId), _pSymbol(pSymbol), _pValue(pValue), _pValueCooked(pValueCooked)
+Tag::Tag(UInt16 typeId, UInt16 tagId, const Symbol* pSymbol, Value* pValue, Value* pValueCooked) :
+	_typeId(typeId), _tagId(tagId), _pSymbol(pSymbol), _pValue(pValue), _pValueCooked(pValueCooked)
 {
 }
 
 Tag* Tag::Create(UInt typeId, UInt16 tagId, const Symbol* pSymbol, Value* pValue, Value* pValueCooked)
 {
-	return new Tag(typeId, tagId, pSymbol, pValue, pValueCooked);
-}
-
-bool Tag::CheckAcceptableValue(Value& value) const
-{
-	switch (_typeId) {
-	case TypeId::BYTE: {
-		return value.IsType(VTYPE_Binary);
-	}
-	case TypeId::ASCII: {
-		return value.IsType(VTYPE_String);
-	}
-	case TypeId::SHORT: {
-		return CheckRangedNumber(value, 0x0000, 0xffff);
-	}
-	case TypeId::LONG: {
-		return CheckRangedNumber(value, 0x00000000, 0xffffffff);
-	}
-	case TypeId::RATIONAL: {
-		return value.IsType(VTYPE_Rational);
-			(value.IsType(VTYPE_List) &&
-			 Value_List::GetValueTypedOwner(value).RefreshVTypeOfElems().IsIdentical(VTYPE_Rational));
-	}
-	case TypeId::UNDEFINED: {
-		return value.IsType(VTYPE_Binary);
-	}
-	case TypeId::SLONG: {
-		return CheckRangedNumber(value, -0x80000000, 0x7fffffff);
-	}
-	case TypeId::SRATIONAL: {
-		return value.IsType(VTYPE_Rational);
-			(value.IsType(VTYPE_List) &&
-			 Value_List::GetValueTypedOwner(value).RefreshVTypeOfElems().IsIdentical(VTYPE_Rational));
-	}
+	switch (typeId) {
+	case TypeId::BYTE:
+		return new Tag_BYTE(tagId, pSymbol, pValue, pValueCooked);
+	case TypeId::ASCII:
+		return new Tag_ASCII(tagId, pSymbol, pValue, pValueCooked);
+	case TypeId::SHORT:
+		return new Tag_SHORT(tagId, pSymbol, pValue, pValueCooked);
+	case TypeId::LONG:
+		return new Tag_LONG(tagId, pSymbol, pValue, pValueCooked);
+	case TypeId::RATIONAL:
+		return new Tag_RATIONAL(tagId, pSymbol, pValue, pValueCooked);
+	case TypeId::UNDEFINED:
+		return new Tag_UNDEFINED(tagId, pSymbol, pValue, pValueCooked);
+	case TypeId::SLONG:
+		return new Tag_SLONG(tagId, pSymbol, pValue, pValueCooked);
+	case TypeId::SRATIONAL:
+		return new Tag_SRATIONAL(tagId, pSymbol, pValue, pValueCooked);
 	default:
 		break;
 	}
-	IssueError_InvalidFormat();
-	return false;
+	return nullptr;
 }
 
 bool Tag::CheckRangedNumber(const Value& value, Double numMin, Double numMax)
@@ -82,6 +64,114 @@ String Tag::ToString(const StringStyle& ss) const
 	str += ":";
 	str += _pValue->ToString(ss);
 	return str;
+}
+
+//------------------------------------------------------------------------------
+// Tag_BYTE
+//------------------------------------------------------------------------------
+bool Tag_BYTE::CheckAcceptableValue(Value& value) const
+{
+	return value.IsType(VTYPE_Binary);
+}
+
+bool Tag_BYTE::WriteToStream(Stream& stream) const
+{
+	return true;
+}
+
+//------------------------------------------------------------------------------
+// Tag_ASCII
+//------------------------------------------------------------------------------
+bool Tag_ASCII::CheckAcceptableValue(Value& value) const
+{
+	return value.IsType(VTYPE_String);
+}
+
+bool Tag_ASCII::WriteToStream(Stream& stream) const
+{
+	return true;
+}
+
+//------------------------------------------------------------------------------
+// Tag_SHORT
+//------------------------------------------------------------------------------
+bool Tag_SHORT::CheckAcceptableValue(Value& value) const
+{
+	return CheckRangedNumber(value, 0x0000, 0xffff);
+}
+
+bool Tag_SHORT::WriteToStream(Stream& stream) const
+{
+	return true;
+}
+
+//------------------------------------------------------------------------------
+// Tag_LONG
+//------------------------------------------------------------------------------
+bool Tag_LONG::CheckAcceptableValue(Value& value) const
+{
+	return CheckRangedNumber(value, 0x00000000, 0xffffffff);
+}
+
+bool Tag_LONG::WriteToStream(Stream& stream) const
+{
+	return true;
+}
+
+//------------------------------------------------------------------------------
+// Tag_RATIONAL
+//------------------------------------------------------------------------------
+bool Tag_RATIONAL::CheckAcceptableValue(Value& value) const
+{
+	return value.IsType(VTYPE_Rational);
+		(value.IsType(VTYPE_List) &&
+			Value_List::GetValueTypedOwner(value).RefreshVTypeOfElems().IsIdentical(VTYPE_Rational));
+}
+
+bool Tag_RATIONAL::WriteToStream(Stream& stream) const
+{
+	return true;
+}
+
+//------------------------------------------------------------------------------
+// Tag_UNDEFINED
+//------------------------------------------------------------------------------
+bool Tag_UNDEFINED::CheckAcceptableValue(Value& value) const
+{
+	return value.IsType(VTYPE_Binary);
+}
+
+bool Tag_UNDEFINED::WriteToStream(Stream& stream) const
+{
+	return true;
+}
+
+//------------------------------------------------------------------------------
+// Tag_SLONG
+//------------------------------------------------------------------------------
+bool Tag_SLONG::CheckAcceptableValue(Value& value) const
+{
+	return CheckRangedNumber(value, -0x80000000, 0x7fffffff);
+}
+
+bool Tag_SLONG::WriteToStream(Stream& stream) const
+{
+	return true;
+}
+
+//------------------------------------------------------------------------------
+// Tag_SRATIONAL
+//------------------------------------------------------------------------------
+bool Tag_SRATIONAL::CheckAcceptableValue(Value& value) const
+{
+	return value.IsType(VTYPE_Rational);
+		(value.IsType(VTYPE_List) &&
+			Value_List::GetValueTypedOwner(value).RefreshVTypeOfElems().IsIdentical(VTYPE_Rational));
+}
+
+bool Tag_SRATIONAL::WriteToStream(Stream& stream) const
+{
+	return true;
 }
 
 //------------------------------------------------------------------------------
--- src/module-jpeg/Tag.h
+++ src/module-jpeg/Tag.h
@@ -43,8 +43,10 @@ public:
 	const Symbol* GetSymbol() const { return _pSymbol; }
 	const Value& GetValue() const { return *_pValue; }
 	const Value& GetValueCooked() const { return *_pValueCooked; }
-	bool CheckAcceptableValue(Value& value) const;
 	void SetValue(Value* pValue) { _pValue.reset(pValue); }
+public:
+	virtual bool CheckAcceptableValue(Value& value) const = 0;
+	virtual bool WriteToStream(Stream& stream) const = 0;
 protected:
 	static bool CheckRangedNumber(const Value& value, Double numMin, Double numMax);
 public:
@@ -53,6 +55,78 @@ public:
 	bool IsEqualTo(const Tag& other) const { return IsIdentical(other); }
 	bool IsLessThan(const Tag& other) const { return this < &other; }
 	String ToString(const StringStyle& ss = StringStyle::Empty) const;
+};
+
+class GURAX_DLLDECLARE Tag_BYTE : public Tag {
+public:
+	Tag_BYTE(UInt16 tagId, const Symbol* pSymbol, Value* pValue, Value* pValueCooked) :
+		Tag(TypeId::BYTE, tagId, pSymbol, pValue, pValueCooked) {}
+public:
+	virtual bool CheckAcceptableValue(Value& value) const override;
+	virtual bool WriteToStream(Stream& stream) const override;
+};
+
+class GURAX_DLLDECLARE Tag_ASCII : public Tag {
+public:
+	Tag_ASCII(UInt16 tagId, const Symbol* pSymbol, Value* pValue, Value* pValueCooked) :
+		Tag(TypeId::ASCII, tagId, pSymbol, pValue, pValueCooked) {}
+public:
+	virtual bool CheckAcceptableValue(Value& value) const override;
+	virtual bool WriteToStream(Stream& stream) const override;
+};
+
+class GURAX_DLLDECLARE Tag_SHORT : public Tag {
+public:
+	Tag_SHORT(UInt16 tagId, const Symbol* pSymbol, Value* pValue, Value* pValueCooked) :
+		Tag(TypeId::SHORT, tagId, pSymbol, pValue, pValueCooked) {}
+public:
+	virtual bool CheckAcceptableValue(Value& value) const override;
+	virtual bool WriteToStream(Stream& stream) const override;
+};
+
+class GURAX_DLLDECLARE Tag_LONG : public Tag {
+public:
+	Tag_LONG(UInt16 tagId, const Symbol* pSymbol, Value* pValue, Value* pValueCooked) :
+		Tag(TypeId::LONG, tagId, pSymbol, pValue, pValueCooked) {}
+public:
+	virtual bool CheckAcceptableValue(Value& value) const override;
+	virtual bool WriteToStream(Stream& stream) const override;
+};
+
+class GURAX_DLLDECLARE Tag_RATIONAL : public Tag {
+public:
+	Tag_RATIONAL(UInt16 tagId, const Symbol* pSymbol, Value* pValue, Value* pValueCooked) :
+		Tag(TypeId::RATIONAL, tagId, pSymbol, pValue, pValueCooked) {}
+public:
+	virtual bool CheckAcceptableValue(Value& value) const override;
+	virtual bool WriteToStream(Stream& stream) const override;
+};
+
+class GURAX_DLLDECLARE Tag_UNDEFINED : public Tag {
+public:
+	Tag_UNDEFINED(UInt16 tagId, const Symbol* pSymbol, Value* pValue, Value* pValueCooked) :
+		Tag(TypeId::UNDEFINED, tagId, pSymbol, pValue, pValueCooked) {}
+public:
+	virtual bool CheckAcceptableValue(Value& value) const override;
+	virtual bool WriteToStream(Stream& stream) const override;
+};
+
+class GURAX_DLLDECLARE Tag_SLONG : public Tag {
+public:
+	Tag_SLONG(UInt16 tagId, const Symbol* pSymbol, Value* pValue, Value* pValueCooked) :
+		Tag(TypeId::SLONG, tagId, pSymbol, pValue, pValueCooked) {}
+public:
+	virtual bool CheckAcceptableValue(Value& value) const override;
+	virtual bool WriteToStream(Stream& stream) const override;
+};
+
+class GURAX_DLLDECLARE Tag_SRATIONAL : public Tag {
+public:
+	Tag_SRATIONAL(UInt16 tagId, const Symbol* pSymbol, Value* pValue, Value* pValueCooked) :
+		Tag(TypeId::SRATIONAL, tagId, pSymbol, pValue, pValueCooked) {}
+public:
+	virtual bool CheckAcceptableValue(Value& value) const override;
+	virtual bool WriteToStream(Stream& stream) const override;
 };
 
 //------------------------------------------------------------------------------
