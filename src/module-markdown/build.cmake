2f4aa4bc72c790c0939acf09569a539f4cb52df8
ypsitau <ypsitau@nifty.com>
2020-05-10 16:12:23 +0900

    refactoring CMakeLists.txt

--- src/module-markdown/build.cmake
+++
@@ -1,8 +0,0 @@
-list(APPEND files-module
-	${GURAX_CURDIR}/Document.cpp
-	${GURAX_CURDIR}/Item.cpp
-	${GURAX_CURDIR}/Util.cpp
-	${GURAX_CURDIR}/VType_Document.cpp
-	${GURAX_CURDIR}/VType_Item.cpp
-	${GURAX_CURDIR}/module-markdown.cpp
-)
