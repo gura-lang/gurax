b3fb316e9b8f49589ccb49c7e1354089e99e89d9
ypsitau <ypsitau@nifty.com>
2020-05-13 11:33:21 +0900

    removed unnecessay code

--- src/module-diff/Util.cpp
+++ src/module-diff/Util.cpp
@@ -4,22 +4,4 @@
 #include "stdafx.h"
 
 Gurax_BeginModuleScope(diff)
-
-const char* GetEditMark_Normal(dtl::edit_t editType)
-{
-	return (editType == dtl::SES_ADD)? ">" : (editType == dtl::SES_DELETE)? "<" : " ";
-}
-
-const char* GetEditMark_Context(dtl::edit_t editType)
-{
-	//return (editType == EDITTYPE_Add)? "+" : (editType == EDITTYPE_Delete)? "-" :
-	//	(editType == EDITTYPE_Change)? "!" : " ";
-    return "";
-}
-
-const char* GetEditMark_Unified(dtl::edit_t editType)
-{
-	return (editType == dtl::SES_ADD)? "+" : (editType == dtl::SES_DELETE)? "-" : " ";
-}
-
 Gurax_EndModuleScope(diff)
