1392f1b0ac6e6d6b47cfe0bf4b456ad56710728c
ypsitau <ypsitau@nifty.com>
2020-06-29 11:46:17 +0900

    removed vector inheritance from StringList

--- include/gurax/String.h
+++ include/gurax/String.h
@@ -346,7 +346,35 @@ inline String operator+(const String& v1, const String& v2) {
 //------------------------------------------------------------------------------
 // StringList
 //------------------------------------------------------------------------------
-class GURAX_DLLDECLARE StringList : public std::vector<String> {
+class GURAX_DLLDECLARE StringList {
+public:
+	using V = std::vector<String>;
+	V v;
+	using iterator = V::iterator;
+	using const_iterator = V::const_iterator;
+	using reverse_iterator = V::reverse_iterator;
+	using const_reverse_iterator = V::const_reverse_iterator;
+	V::reference operator[](size_t pos) { return v[pos]; }
+	bool empty() const { return v.empty(); }
+	size_t size() const { return v.size(); }
+	void clear() { v.clear(); }
+	void reserve(size_t size) { v.reserve(size); }
+	V::const_reference front() const { return v.front(); }
+	V::const_reference back() const { return v.back(); }
+	V::reference front() { return v.front(); }
+	V::reference back() { return v.back(); }
+	iterator begin() { return v.begin(); }
+	iterator end() { return v.end(); }
+	const_iterator begin() const { return v.begin(); }
+	const_iterator end() const { return v.end(); }
+	reverse_iterator rbegin() { return v.rbegin(); }
+	reverse_iterator rend() { return v.rend(); }
+	const_reverse_iterator rbegin() const { return v.rbegin(); }
+	const_reverse_iterator rend() const { return v.rend(); }
+	void push_back(V::const_reference elem) { v.push_back(elem); }
+	void pop_back() { v.pop_back(); }
+	void erase(const_iterator where) { v.erase(where); }
+	void erase(const_iterator first, const_iterator last) { v.erase(first, last); }
 public:
 	static const StringList Empty;
 public:
