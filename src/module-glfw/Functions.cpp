44f09b1432d140f2b9406bfcc4302b9fd54d97a5
ypsitau <ypsitau@nifty.com>
2020-06-30 15:20:55 +0900

    implementing cbridge

--- src/module-glfw/Functions.cpp
+++ src/module-glfw/Functions.cpp
@@ -8,7 +8,7 @@ Gurax_BeginModuleScope(glfw)
 // glfwInit()
 Gurax_DeclareFunction(glfwInit)
 {
-	Declare(VTYPE_Any, Flag::None);
+	Declare(VTYPE_Nil, Flag::None);
 	AddHelp(
 		Gurax_Symbol(en),
 		"");
@@ -35,6 +35,78 @@ Gurax_ImplementFunction(glfwTerminate)
 	// Function body
 	glfwTerminate();
 	return Value::nil();
+}
+
+// glfwInitHint()
+Gurax_DeclareFunction(glfwInitHint)
+{
+	Declare(VTYPE_Nil, Flag::None);
+	DeclareArg("hint", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
+	DeclareArg("value", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementFunction(glfwInitHint)
+{
+	// Arguments
+	ArgPicker args(argument);
+	int hint = args.PickNumber<int>();
+	int value = args.PickNumber<int>();
+	// Function body
+	glfwInitHint(hint, value);
+	return Value::nil();
+}
+
+// glfwGetVersion()
+Gurax_DeclareFunction(glfwGetVersion)
+{
+	Declare(VTYPE_Any, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementFunction(glfwGetVersion)
+{
+	// Function body
+	int major, minor, rev;
+	glfwGetVersion(&major, &minor, &rev);
+	return Value_List::Create(new Value_Number(major), new Value_Number(minor), new Value_Number(rev));
+}
+
+// glfwGetVersionString()
+Gurax_DeclareFunction(glfwGetVersionString)
+{
+	Declare(VTYPE_String, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementFunction(glfwGetVersionString)
+{
+	// Function body
+	const char* rtn = glfwGetVersionString();
+	return new Value_String(rtn);
+}
+
+// glfwGetError()
+Gurax_DeclareFunction(glfwGetError)
+{
+	Declare(VTYPE_String, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementFunction(glfwGetError)
+{
+	// Function body
+	const char* description;
+	glfwGetError(&description);
+	return description? new Value_String(description) : Value::nil();
 }
 
 // glfwDefaultWindowHints()
@@ -100,7 +172,7 @@ Gurax_ImplementFunction(glfwWindowHintString)
 // glfwCreateWindow()
 Gurax_DeclareFunction(glfwCreateWindow)
 {
-	Declare(VTYPE_Any, Flag::None);
+	Declare(VTYPE_GLFWwindow, Flag::None);
 	DeclareArg("width", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
 	DeclareArg("height", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
 	DeclareArg("title", VTYPE_String, ArgOccur::Once, ArgFlag::None);
@@ -151,7 +223,7 @@ Gurax_ImplementFunction(glfwDestroyWindow)
 // glfwWindowShouldClose()
 Gurax_DeclareFunction(glfwWindowShouldClose)
 {
-	Declare(VTYPE_Any, Flag::None);
+	Declare(VTYPE_Bool, Flag::None);
 	DeclareArg("window", VTYPE_GLFWwindow, ArgOccur::Once, ArgFlag::None);
 	AddHelp(
 		Gurax_Symbol(en),
@@ -237,7 +309,7 @@ Gurax_ImplementFunction(glfwSetWindowIcon)
 // glfwGetWindowPos()
 Gurax_DeclareFunction(glfwGetWindowPos)
 {
-	Declare(VTYPE_Any, Flag::None);
+	Declare(VTYPE_Bool, Flag::None);
 	DeclareArg("window", VTYPE_GLFWwindow, ArgOccur::Once, ArgFlag::None);
 	AddHelp(
 		Gurax_Symbol(en),
@@ -255,10 +327,209 @@ Gurax_ImplementFunction(glfwGetWindowPos)
 	return Value_List::Create(new Value_Number(xpos), new Value_Number(ypos));
 }
 
+// glfwGetInputMode()
+Gurax_DeclareFunction(glfwGetInputMode)
+{
+	Declare(VTYPE_Number, Flag::None);
+	DeclareArg("window", VTYPE_GLFWwindow, ArgOccur::Once, ArgFlag::None);
+	DeclareArg("mode", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementFunction(glfwGetInputMode)
+{
+	// Arguments
+	ArgPicker args(argument);
+	GLFWwindow* window = args.Pick<Value_GLFWwindow>().GetEntity();
+	int mode = args.PickNumber<int>();
+	// Function body
+	int rtn = glfwGetInputMode(window, mode);
+	return new Value_Number(rtn);
+}
+
+// glfwSetInputMode()
+Gurax_DeclareFunction(glfwSetInputMode)
+{
+	Declare(VTYPE_Nil, Flag::None);
+	DeclareArg("window", VTYPE_GLFWwindow, ArgOccur::Once, ArgFlag::None);
+	DeclareArg("mode", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
+	DeclareArg("value", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementFunction(glfwSetInputMode)
+{
+	// Arguments
+	ArgPicker args(argument);
+	GLFWwindow* window = args.Pick<Value_GLFWwindow>().GetEntity();
+	int mode = args.PickNumber<int>();
+	int value = args.PickNumber<int>();
+	// Function body
+	glfwSetInputMode(window, mode, value);
+	return Value::nil();
+}
+
+// glfwRawMouseMotionSupported()
+Gurax_DeclareFunction(glfwRawMouseMotionSupported)
+{
+	Declare(VTYPE_Bool, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementFunction(glfwRawMouseMotionSupported)
+{
+	// Function body
+	bool rtn = (glfwRawMouseMotionSupported() == GLFW_TRUE);
+	return new Value_Bool(rtn);
+}
+
+// glfwGetKeyName()
+Gurax_DeclareFunction(glfwGetKeyName)
+{
+	Declare(VTYPE_String, Flag::None);
+	DeclareArg("key", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
+	DeclareArg("scancode", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementFunction(glfwGetKeyName)
+{
+	// Arguments
+	ArgPicker args(argument);
+	int key = args.PickNumber<int>();
+	int scancode = args.PickNumber<int>();
+	// Function body
+	if (const char* rtn = glfwGetKeyName(key, scancode)) {
+		return new Value_String(rtn);
+	}
+	return Value::nil();
+}
+
+// glfwGetKeyScancode()
+Gurax_DeclareFunction(glfwGetKeyScancode)
+{
+	Declare(VTYPE_Number, Flag::None);
+	DeclareArg("key", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementFunction(glfwGetKeyScancode)
+{
+	// Arguments
+	ArgPicker args(argument);
+	int key = args.PickNumber<int>();
+	// Function body
+	int rtn = glfwGetKeyScancode(key);
+	return new Value_Number(rtn);
+}
+
+// glfwGetKey()
+Gurax_DeclareFunction(glfwGetKey)
+{
+	Declare(VTYPE_Number, Flag::None);
+	DeclareArg("window", VTYPE_GLFWwindow, ArgOccur::Once, ArgFlag::None);
+	DeclareArg("key", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementFunction(glfwGetKey)
+{
+	// Arguments
+	ArgPicker args(argument);
+	GLFWwindow* window = args.Pick<Value_GLFWwindow>().GetEntity();
+	int key = args.PickNumber<int>();
+	// Function body
+	int rtn = glfwGetKey(window, key);
+	return new Value_Number(rtn);
+}
+
+// glfwGetMouseButton()
+Gurax_DeclareFunction(glfwGetMouseButton)
+{
+	Declare(VTYPE_Number, Flag::None);
+	DeclareArg("window", VTYPE_GLFWwindow, ArgOccur::Once, ArgFlag::None);
+	DeclareArg("button", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementFunction(glfwGetMouseButton)
+{
+	// Arguments
+	ArgPicker args(argument);
+	GLFWwindow* window = args.Pick<Value_GLFWwindow>().GetEntity();
+	int button = args.PickNumber<int>();
+	// Function body
+	int rtn = glfwGetMouseButton(window, button);
+	return new Value_Number(rtn);
+}
+
+// glfwGetCursorPos()
+Gurax_DeclareFunction(glfwGetCursorPos)
+{
+	Declare(VTYPE_Any, Flag::None);
+	DeclareArg("window", VTYPE_GLFWwindow, ArgOccur::Once, ArgFlag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementFunction(glfwGetCursorPos)
+{
+	// Arguments
+	ArgPicker args(argument);
+	GLFWwindow* window = args.Pick<Value_GLFWwindow>().GetEntity();
+	// Function body
+	double xpos, ypos;
+	glfwGetCursorPos(window, &xpos, &ypos);
+	return Value_List::Create(new Value_Number(xpos), new Value_Number(ypos));
+}
+
+// glfwSetCursorPos()
+Gurax_DeclareFunction(glfwSetCursorPos)
+{
+	Declare(VTYPE_Nil, Flag::None);
+	DeclareArg("window", VTYPE_GLFWwindow, ArgOccur::Once, ArgFlag::None);
+	DeclareArg("xpos", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
+	DeclareArg("ypos", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementFunction(glfwSetCursorPos)
+{
+	// Arguments
+	ArgPicker args(argument);
+	GLFWwindow* window = args.Pick<Value_GLFWwindow>().GetEntity();
+	double xpos = args.PickNumber<double>();
+	double ypos = args.PickNumber<double>();
+	// Function body
+	glfwSetCursorPos(window, xpos, ypos);
+	return Value::nil();
+}
+
 void AssignFunctions(Frame& frame)
 {
 	frame.Assign(Gurax_CreateFunction(glfwInit));
 	frame.Assign(Gurax_CreateFunction(glfwTerminate));
+	frame.Assign(Gurax_CreateFunction(glfwInitHint));
+	frame.Assign(Gurax_CreateFunction(glfwGetVersion));
+	frame.Assign(Gurax_CreateFunction(glfwGetVersionString));
+	frame.Assign(Gurax_CreateFunction(glfwGetError));
 	frame.Assign(Gurax_CreateFunction(glfwDefaultWindowHints));
 	frame.Assign(Gurax_CreateFunction(glfwWindowHint));
 	frame.Assign(Gurax_CreateFunction(glfwWindowHintString));
@@ -269,6 +540,15 @@ void AssignFunctions(Frame& frame)
 	frame.Assign(Gurax_CreateFunction(glfwSetWindowTitle));
 	frame.Assign(Gurax_CreateFunction(glfwSetWindowIcon));
 	frame.Assign(Gurax_CreateFunction(glfwGetWindowPos));
+	frame.Assign(Gurax_CreateFunction(glfwGetInputMode));
+	frame.Assign(Gurax_CreateFunction(glfwSetInputMode));
+	frame.Assign(Gurax_CreateFunction(glfwRawMouseMotionSupported));
+	frame.Assign(Gurax_CreateFunction(glfwGetKeyName));
+	frame.Assign(Gurax_CreateFunction(glfwGetKeyScancode));
+	frame.Assign(Gurax_CreateFunction(glfwGetKey));
+	frame.Assign(Gurax_CreateFunction(glfwGetMouseButton));
+	frame.Assign(Gurax_CreateFunction(glfwGetCursorPos));
+	frame.Assign(Gurax_CreateFunction(glfwSetCursorPos));
 }
 
 Gurax_EndModuleScope(glfw)
