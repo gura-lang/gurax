b3ec256197b54ef9bfda2727817adf57a154f0aa
ypsitau <ypsitau@nifty.com>
2020-07-08 12:37:03 +0900

    implementing opengl module

--- src/module-opengl/Functions.cpp
+++ src/module-opengl/Functions.cpp
@@ -101,6 +101,50 @@ Gurax_ImplementFunction(glewGetString)
 	return new Value_String(reinterpret_cast<const char*>(rtn));
 }
 
+// opengl.glAccum(op:Number, value:Number)
+Gurax_DeclareFunction(glAccum)
+{
+	Declare(VTYPE_Nil, Flag::None);
+	DeclareArg("op", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
+	DeclareArg("value", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementFunction(glAccum)
+{
+	// Arguments
+	ArgPicker args(argument);
+	GLenum op = args.PickNumber<GLenum>();
+	GLfloat value = args.PickNumber<GLfloat>();
+	// Function body
+	glAccum(op, value);
+	return Value::nil();
+}
+
+// opengl.glAlphaFunc(func:Number, ref:Number)
+Gurax_DeclareFunction(glAlphaFunc)
+{
+	Declare(VTYPE_Nil, Flag::None);
+	DeclareArg("func", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
+	DeclareArg("ref", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementFunction(glAlphaFunc)
+{
+	// Arguments
+	ArgPicker args(argument);
+	GLenum func = args.PickNumber<GLenum>();
+	GLclampf ref = args.PickNumber<GLclampf>();
+	// Function body
+	glAlphaFunc(func, ref);
+	return Value::nil();
+}
+
 void AssignFunctions(Frame& frame)
 {
 	frame.Assign(Gurax_CreateFunction(glewInit));
@@ -108,6 +152,8 @@ void AssignFunctions(Frame& frame)
 	frame.Assign(Gurax_CreateFunction(glewGetExtension));
 	frame.Assign(Gurax_CreateFunction(glewGetErrorString));
 	frame.Assign(Gurax_CreateFunction(glewGetString));
+	frame.Assign(Gurax_CreateFunction(glAccum));
+	frame.Assign(Gurax_CreateFunction(glAlphaFunc));
 }
 
 Gurax_EndModuleScope(opengl)
