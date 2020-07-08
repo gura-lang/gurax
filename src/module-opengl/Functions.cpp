5beebc50a208f66ae2f3f5320168d685a7b0bfbc
ypsitau <ypsitau@nifty.com>
2020-07-08 12:10:30 +0900

    implementing opengl module

--- src/module-opengl/Functions.cpp
+++ src/module-opengl/Functions.cpp
@@ -21,9 +21,30 @@ Gurax_ImplementFunction(glewInit)
 	return new Value_Number(rtn);
 }
 
+// opengl.glewIsSupported(name:String)
+Gurax_DeclareFunction(glewIsSupported)
+{
+	Declare(VTYPE_Bool, Flag::None);
+	DeclareArg("name", VTYPE_String, ArgOccur::Once, ArgFlag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementFunction(glewIsSupported)
+{
+	// Arguments
+	ArgPicker args(argument);
+	const char* name = args.PickString();
+	// Function body
+	GLboolean rtn = glewIsSupported(name);
+	return new Value_Bool(!!rtn);
+}
+
 void AssignFunctions(Frame& frame)
 {
 	frame.Assign(Gurax_CreateFunction(glewInit));
+	frame.Assign(Gurax_CreateFunction(glewIsSupported));
 }
 
 Gurax_EndModuleScope(opengl)
