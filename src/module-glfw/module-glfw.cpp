4338acfb60545ed8ff30eba4903750aa285ca303
ypsitau <ypsitau@nifty.com>
2020-07-21 12:19:06 +0900

    removed unnecessary functions

--- src/module-glfw/module-glfw.cpp
+++ src/module-glfw/module-glfw.cpp
@@ -6,57 +6,6 @@
 Gurax_BeginModule(glfw)
 
 //------------------------------------------------------------------------------
-// Implementation of function
-//------------------------------------------------------------------------------
-// glfw.Test()
-Gurax_DeclareFunction(Test)
-{
-	Declare(VTYPE_Nil, Flag::None);
-	AddHelp(
-		Gurax_Symbol(en),
-		"");
-}
-
-Gurax_ImplementFunction(Test)
-{
-	// Arguments
-	//ArgPicker args(argument);
-	// Function body
-	GLFWwindow* window;
-
-	/* Initialize the library */
-	if (!glfwInit())
-		return Value::nil();
-
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window)
-	{
-		glfwTerminate();
-		return Value::nil();
-	}
-
-	/* Make the window's context current */
-	glfwMakeContextCurrent(window);
-
-	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
-	{
-		/* Render here */
-		//glClear(GL_COLOR_BUFFER_BIT);
-
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
-		glfwPollEvents();
-	}
-
-	glfwTerminate();
-	return Value::nil();
-}
-
-//------------------------------------------------------------------------------
 // Callback function
 //------------------------------------------------------------------------------
 RefPtr<Processor> g_pProcessor_ErrorCallback;
