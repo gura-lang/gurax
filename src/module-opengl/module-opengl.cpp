5997619ef319b21c5386b4d8bde1ec42d7381344
ypsitau <ypsitau@nifty.com>
2020-08-19 15:00:12 +0900

    implemented glReadAsImage()

--- src/module-opengl/module-opengl.cpp
+++ src/module-opengl/module-opengl.cpp
@@ -8,6 +8,50 @@
 
 Gurax_BeginModule(opengl)
 
+//------------------------------------------------------------------------------
+// Function
+//------------------------------------------------------------------------------
+// opengl.glReadAsImage():[rgb,rgba]
+Gurax_DeclareFunction(glReadAsImage)
+{
+	Declare(VTYPE_Number, Flag::None);
+	DeclareArg("x", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
+	DeclareArg("y", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
+	DeclareArg("width", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
+	DeclareArg("height", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
+	DeclareAttrOpt(Gurax_Symbol(rgb));
+	DeclareAttrOpt(Gurax_Symbol(rgba));
+	DeclareBlock(BlkOccur::ZeroOrOnce);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementFunction(glReadAsImage)
+{
+	// Arguments
+	ArgPicker args(argument);
+	GLint x = args.PickNumber<GLint>();
+	GLint y = args.PickNumber<GLint>();
+	GLsizei width = args.PickNumberPos<GLsizei>();
+	GLsizei height = args.PickNumberPos<GLsizei>();
+	const Image::Format& format =
+		argument.IsSet(Gurax_Symbol(rgb))? Image::Format::RGB : Image::Format::RGBA;
+	if (Error::IsIssued()) return Value::nil();
+	// Function body
+	RefPtr<Image> pImage(new Image(format));
+	if (!pImage->Allocate(width, height)) return Value::nil();
+	GLint param_PACK_ALIGNMENT;
+	glGetIntegerv(GL_PACK_ALIGNMENT, &param_PACK_ALIGNMENT);
+	glPixelStorei(GL_PACK_ALIGNMENT, 1);
+	//glReadBuffer(GL_FRONT);
+	glReadPixels(x, y, width, height,
+		format.IsIdentical(Image::Format::RGB)? GL_BGR : GL_BGRA, GL_UNSIGNED_BYTE,
+		pImage->GetPointerC());
+	glPixelStorei(GL_PACK_ALIGNMENT, param_PACK_ALIGNMENT);
+	return argument.ReturnValue(processor, new Value_Image(pImage->Flip(false, true)));
+}
+
 // opengl.Test()
 Gurax_DeclareFunction(Test)
 {
@@ -45,7 +89,8 @@ Gurax_ModulePrepare()
 	// Assignment of value
 	AssignConsts(GetFrame());
 	// Assignment of function
-	Assign(Gurax_CreateFunction(Test));
+	Assign(Gurax_CreateFunction(glReadAsImage));
+	//Assign(Gurax_CreateFunction(Test));
 	AssignFunctions(GetFrame());
 	return true;
 }
