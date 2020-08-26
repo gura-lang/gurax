1f8aef65a080f15c27bb40f078e16e6d951fff2a
ypsitau <ypsitau@nifty.com>
2020-08-26 15:36:24 +0900

    implementing Extract script

---
+++ src/module-sdl/Functions.cpp
@@ -0,0 +1,15 @@
+//==============================================================================
+// Functions.cpp
+//==============================================================================
+#include "stdafx.h"
+
+Gurax_BeginModuleScope(sdl)
+
+
+
+void AssignFunctions(Frame& frame)
+{
+	
+}
+
+Gurax_EndModuleScope(sdl)
