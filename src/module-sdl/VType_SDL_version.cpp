a35728a4957a89802721a73c03565b7ec2572e6a
ypsitau <ypsitau@nifty.com>
2020-09-20 12:42:35 +0900

    implementing sdl module

--- src/module-sdl/VType_SDL_version.cpp
+++ src/module-sdl/VType_SDL_version.cpp
@@ -27,8 +27,8 @@ static const char* g_docHelp_en = u8R"**(
 //-----------------------------------------------------------------------------
 // Implementation of property
 //-----------------------------------------------------------------------------
-// sdl.SDL_version#propSkeleton
-Gurax_DeclareProperty_R(SDL_version, propSkeleton)
+// sdl.SDL_version#major
+Gurax_DeclareProperty_R(SDL_version, major)
 {
 	Declare(VTYPE_Number, Flag::None);
 	AddHelp(
@@ -36,10 +36,40 @@ Gurax_DeclareProperty_R(SDL_version, propSkeleton)
 		"");
 }
 
-Gurax_ImplementPropertyGetter(SDL_version, propSkeleton)
+Gurax_ImplementPropertyGetter(SDL_version, major)
 {
-	//auto& valueThis = GetValueThis(valueTarget);
-	return new Value_Number(3);
+	auto& valueThis = GetValueThis(valueTarget);
+	return new Value_Number(valueThis.GetEntity().major);
+}
+
+// sdl.SDL_version#minor
+Gurax_DeclareProperty_R(SDL_version, minor)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(SDL_version, minor)
+{
+	auto& valueThis = GetValueThis(valueTarget);
+	return new Value_Number(valueThis.GetEntity().minor);
+}
+
+// sdl.SDL_version#patch
+Gurax_DeclareProperty_R(SDL_version, patch)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(SDL_version, patch)
+{
+	auto& valueThis = GetValueThis(valueTarget);
+	return new Value_Number(valueThis.GetEntity().patch);
 }
 
 //------------------------------------------------------------------------------
@@ -54,7 +84,9 @@ void VType_SDL_version::DoPrepare(Frame& frameOuter)
 	// Declaration of VType
 	Declare(VTYPE_Object, Flag::Mutable);
 	// Assignment of property
-	Assign(Gurax_CreateProperty(SDL_version, propSkeleton));
+	Assign(Gurax_CreateProperty(SDL_version, major));
+	Assign(Gurax_CreateProperty(SDL_version, minor));
+	Assign(Gurax_CreateProperty(SDL_version, patch));
 }
 
 //------------------------------------------------------------------------------
