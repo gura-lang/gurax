1bc16a333eb3a5e14666bb8755a636888c91045b
ypsitau <ypsitau@nifty.com>
2020-10-11 17:16:38 +0900

    implementing sdl module

--- src/module-sdl/VType_SDL_AudioSpec.cpp
+++ src/module-sdl/VType_SDL_AudioSpec.cpp
@@ -24,11 +24,34 @@ static const char* g_docHelp_en = u8R"**(
 # Method
 )**";
 
+//------------------------------------------------------------------------------
+// Implementation of constructor
+//------------------------------------------------------------------------------
+// SDL_AudioSpec() {block?}
+Gurax_DeclareConstructor(SDL_AudioSpec)
+{
+	Declare(VTYPE_Color, Flag::None);
+	DeclareBlock(BlkOccur::ZeroOrOnce);
+	AddHelp(
+		Gurax_Symbol(en),
+		"Creates a `SDL_AudioSpec` instance.\n");
+}
+
+Gurax_ImplementConstructor(SDL_AudioSpec)
+{
+	// Arguments
+	ArgPicker args(argument);
+	SDL_AudioSpec audioSpec = {};
+	// Function body
+	return argument.ReturnValue(processor,
+				new Value_SDL_AudioSpec(audioSpec, processor.Reference()));
+}
+
 //-----------------------------------------------------------------------------
 // Implementation of property
 //-----------------------------------------------------------------------------
-// sdl.SDL_AudioSpec#propSkeleton
-Gurax_DeclareProperty_R(SDL_AudioSpec, propSkeleton)
+// sdl.SDL_AudioSpec#freq
+Gurax_DeclareProperty_RW(SDL_AudioSpec, freq)
 {
 	Declare(VTYPE_Number, Flag::None);
 	AddHelp(
@@ -36,10 +59,166 @@ Gurax_DeclareProperty_R(SDL_AudioSpec, propSkeleton)
 		"");
 }
 
-Gurax_ImplementPropertyGetter(SDL_AudioSpec, propSkeleton)
+Gurax_ImplementPropertyGetter(SDL_AudioSpec, freq)
 {
-	//auto& valueThis = GetValueThis(valueTarget);
-	return new Value_Number(3);
+	auto& valueThis = GetValueThis(valueTarget);
+	return new Value_Number(valueThis.GetEntity().freq);
+}
+
+Gurax_ImplementPropertySetter(SDL_AudioSpec, freq)
+{
+	auto& valueThis = GetValueThis(valueTarget);
+	valueThis.GetEntity().freq = Value_Number::GetNumber<int>(value);
+}
+
+// sdl.SDL_AudioSpec#format
+Gurax_DeclareProperty_RW(SDL_AudioSpec, format)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(SDL_AudioSpec, format)
+{
+	auto& valueThis = GetValueThis(valueTarget);
+	return new Value_Number(valueThis.GetEntity().format);
+}
+
+Gurax_ImplementPropertySetter(SDL_AudioSpec, format)
+{
+	auto& valueThis = GetValueThis(valueTarget);
+	valueThis.GetEntity().format = Value_Number::GetNumber<SDL_AudioFormat>(value);
+}
+
+// sdl.SDL_AudioSpec#channels
+Gurax_DeclareProperty_RW(SDL_AudioSpec, channels)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(SDL_AudioSpec, channels)
+{
+	auto& valueThis = GetValueThis(valueTarget);
+	return new Value_Number(valueThis.GetEntity().channels);
+}
+
+Gurax_ImplementPropertySetter(SDL_AudioSpec, channels)
+{
+	auto& valueThis = GetValueThis(valueTarget);
+	valueThis.GetEntity().channels = Value_Number::GetNumber<Uint8>(value);
+}
+
+// sdl.SDL_AudioSpec#silence
+Gurax_DeclareProperty_RW(SDL_AudioSpec, silence)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(SDL_AudioSpec, silence)
+{
+	auto& valueThis = GetValueThis(valueTarget);
+	return new Value_Number(valueThis.GetEntity().silence);
+}
+
+Gurax_ImplementPropertySetter(SDL_AudioSpec, silence)
+{
+	auto& valueThis = GetValueThis(valueTarget);
+	valueThis.GetEntity().silence = Value_Number::GetNumber<Uint8>(value);
+}
+
+// sdl.SDL_AudioSpec#samples
+Gurax_DeclareProperty_RW(SDL_AudioSpec, samples)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(SDL_AudioSpec, samples)
+{
+	auto& valueThis = GetValueThis(valueTarget);
+	return new Value_Number(valueThis.GetEntity().samples);
+}
+
+Gurax_ImplementPropertySetter(SDL_AudioSpec, samples)
+{
+	auto& valueThis = GetValueThis(valueTarget);
+	valueThis.GetEntity().samples = Value_Number::GetNumber<Uint16>(value);
+}
+
+// sdl.SDL_AudioSpec#size
+Gurax_DeclareProperty_RW(SDL_AudioSpec, size)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(SDL_AudioSpec, size)
+{
+	auto& valueThis = GetValueThis(valueTarget);
+	return new Value_Number(valueThis.GetEntity().size);
+}
+
+Gurax_ImplementPropertySetter(SDL_AudioSpec, size)
+{
+	auto& valueThis = GetValueThis(valueTarget);
+	valueThis.GetEntity().size = Value_Number::GetNumber<Uint32>(value);
+}
+
+// sdl.SDL_AudioSpec#callback
+Gurax_DeclareProperty_RW(SDL_AudioSpec, callback)
+{
+	Declare(VTYPE_Function, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(SDL_AudioSpec, callback)
+{
+	auto& valueThis = GetValueThis(valueTarget);
+	valueThis.GetEntity().callback = Value_SDL_AudioSpec::AudioCallback;
+	Function* pFunc = valueThis.GetFuncAudioCallback();
+	if (!pFunc) return Value::nil();
+	return new Value_Function(pFunc->Reference());
+}
+
+Gurax_ImplementPropertySetter(SDL_AudioSpec, callback)
+{
+	auto& valueThis = GetValueThis(valueTarget);
+	valueThis.SetFuncAudioCallback(Value_Function::GetFunction(value).Reference());
+}
+
+// sdl.SDL_AudioSpec#userdata
+Gurax_DeclareProperty_RW(SDL_AudioSpec, userdata)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(SDL_AudioSpec, userdata)
+{
+	auto& valueThis = GetValueThis(valueTarget);
+	return valueThis.GetValueUserdata().Reference();
+}
+
+Gurax_ImplementPropertySetter(SDL_AudioSpec, userdata)
+{
+	auto& valueThis = GetValueThis(valueTarget);
+	valueThis.SetValueUserdata(value.Reference());
 }
 
 //------------------------------------------------------------------------------
@@ -52,15 +231,47 @@ void VType_SDL_AudioSpec::DoPrepare(Frame& frameOuter)
 	// Add help
 	AddHelpTmpl(Gurax_Symbol(en), g_docHelp_en);
 	// Declaration of VType
-	Declare(VTYPE_Object, Flag::Mutable);
+	Declare(VTYPE_Object, Flag::Mutable, Gurax_CreateConstructor(SDL_AudioSpec));
 	// Assignment of property
-	Assign(Gurax_CreateProperty(SDL_AudioSpec, propSkeleton));
+	Assign(Gurax_CreateProperty(SDL_AudioSpec, freq));
+	Assign(Gurax_CreateProperty(SDL_AudioSpec, format));
+	Assign(Gurax_CreateProperty(SDL_AudioSpec, channels));
+	Assign(Gurax_CreateProperty(SDL_AudioSpec, silence));
+	Assign(Gurax_CreateProperty(SDL_AudioSpec, samples));
+	Assign(Gurax_CreateProperty(SDL_AudioSpec, size));
+	Assign(Gurax_CreateProperty(SDL_AudioSpec, callback));
+	Assign(Gurax_CreateProperty(SDL_AudioSpec, userdata));
 }
 
 //------------------------------------------------------------------------------
 // Value_SDL_AudioSpec
 //------------------------------------------------------------------------------
 VType& Value_SDL_AudioSpec::vtype = VTYPE_SDL_AudioSpec;
+
+Value_SDL_AudioSpec::Value_SDL_AudioSpec(Processor* pProcessor, VType& vtype) :
+	Value_Object(vtype), _pProcessor(pProcessor), _pValueUserdata(Value::nil())
+{
+	_entity.callback = AudioCallback;
+	_entity.userdata = this;
+}
+
+Value_SDL_AudioSpec::Value_SDL_AudioSpec(const SDL_AudioSpec& entity, Processor* pProcessor, VType& vtype) :
+	Value_Object(vtype), _entity(entity), _pProcessor(pProcessor), _pValueUserdata(Value::nil())
+{
+	_entity.callback = AudioCallback;
+	_entity.userdata = this;
+}
+
+void Value_SDL_AudioSpec::AudioCallback(void* userdata, Uint8* stream, int len)
+{
+	auto& valueThis = *reinterpret_cast<Value_SDL_AudioSpec*>(userdata);
+	Function* pFunc = valueThis.GetFuncAudioCallback();
+	if (!pFunc) return;
+	RefPtr<Memory> pMemory(new MemoryHeap(len));
+	::memcpy(pMemory->GetPointerC<void>(), stream, len);
+	pFunc->EvalEasy(valueThis.GetProcessor(), valueThis.GetValueUserdata().Reference(),
+					new Value_Pointer(new Pointer_Memory(pMemory.release())));
+}
 
 String Value_SDL_AudioSpec::ToString(const StringStyle& ss) const
 {
--- src/module-sdl/VType_SDL_AudioSpec.h
+++ src/module-sdl/VType_SDL_AudioSpec.h
@@ -28,14 +28,16 @@ public:
 	// Uses MemoryPool allocator
 	Gurax_MemoryPoolAllocator("Value_SDL_AudioSpec");
 protected:
-	SDL_AudioSpec* _entity;
+	SDL_AudioSpec _entity;
+	RefPtr<Processor> _pProcessor;
+	RefPtr<Function> _pFuncAudioCallback;
+	RefPtr<Value> _pValueUserdata;
 public:
 	static VType& vtype;
 public:
 	// Constructor
-	explicit Value_SDL_AudioSpec(VType& vtype = VTYPE_SDL_AudioSpec) : Value_Object(vtype) {}
-	explicit Value_SDL_AudioSpec(SDL_AudioSpec* entity, VType& vtype = VTYPE_SDL_AudioSpec) :
-		Value_Object(vtype), _entity(entity) {}
+	explicit Value_SDL_AudioSpec(Processor* pProcessor, VType& vtype = VTYPE_SDL_AudioSpec);
+	explicit Value_SDL_AudioSpec(const SDL_AudioSpec& entity, Processor* pProcessor, VType& vtype = VTYPE_SDL_AudioSpec);
 	// Copy constructor/operator
 	Value_SDL_AudioSpec(const Value_SDL_AudioSpec& src) = delete;
 	Value_SDL_AudioSpec& operator=(const Value_SDL_AudioSpec& src) = delete;
@@ -46,10 +48,22 @@ protected:
 	// Destructor
 	~Value_SDL_AudioSpec() = default;
 public:
-	SDL_AudioSpec& GetEntity() { return *_entity; }
-	const SDL_AudioSpec& GetEntity() const { return *_entity; }
-	SDL_AudioSpec* GetEntityPtr() { return _entity; }
-	const SDL_AudioSpec* GetEntityPtr() const { return _entity; }
+	SDL_AudioSpec& GetEntity() { return _entity; }
+	const SDL_AudioSpec& GetEntity() const { return _entity; }
+	SDL_AudioSpec* GetEntityPtr() { return &_entity; }
+	const SDL_AudioSpec* GetEntityPtr() const { return &_entity; }
+public:
+	Processor& GetProcessor() { return *_pProcessor; }
+	void SetFuncAudioCallback(Function* pFuncAudioCallback) {
+		_pFuncAudioCallback.reset(pFuncAudioCallback);
+	}
+	void SetValueUserdata(Value* pValueUserdata) {
+		_pValueUserdata.reset(pValueUserdata);
+	}
+	Function* GetFuncAudioCallback() { return _pFuncAudioCallback.get(); }
+	const Value& GetValueUserdata() { return *_pValueUserdata; }
+public:
+	static void AudioCallback(void* userdata, Uint8* stream, int len);
 public:
 	static SDL_AudioSpec& GetEntity(Value& value) {
 		return dynamic_cast<Value_SDL_AudioSpec&>(value).GetEntity();
