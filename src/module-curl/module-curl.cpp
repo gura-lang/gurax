d27c5c35fb12ed30df973007118a6690cb87f769
ypsitau <ypsitau@nifty.com>
2020-10-25 12:39:58 +0900

    added module-curl

---
+++ src/module-curl/module-curl.cpp
@@ -0,0 +1,63 @@
+//==============================================================================
+// module-curl.cpp
+//==============================================================================
+#include "stdafx.h"
+
+Gurax_BeginModule(curl)
+
+//------------------------------------------------------------------------------
+// Implementation of function
+//------------------------------------------------------------------------------
+// curl.Test()
+Gurax_DeclareFunction(Test)
+{
+	Declare(VTYPE_Nil, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"Adds up the given two numbers and returns the result.");
+}
+
+Gurax_ImplementFunction(Test)
+{
+	CURL *curl;
+	CURLcode res;
+ 
+	curl = curl_easy_init();
+	if(curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, "https://example.com");
+		/* example.com is redirected, so we tell libcurl to follow redirection */ 
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+ 
+		/* Perform the request, res will get the return code */ 
+		res = curl_easy_perform(curl);
+		/* Check for errors */ 
+		if(res != CURLE_OK)
+			fprintf(stderr, "curl_easy_perform() failed: %s\n",
+							curl_easy_strerror(res));
+ 
+		/* always cleanup */ 
+		curl_easy_cleanup(curl);
+	}
+	return Value::nil();
+}
+
+//------------------------------------------------------------------------------
+// Entries
+//------------------------------------------------------------------------------
+Gurax_ModuleValidate()
+{
+	return Version::CheckCoreVersion(GURAX_VERSION, nullptr);
+}
+
+Gurax_ModulePrepare()
+{
+	// Assignment of function
+	Assign(Gurax_CreateFunction(Test));
+	return true;
+}
+
+Gurax_ModuleTerminate()
+{
+}
+
+Gurax_EndModule(curl)
---
+++ src/module-curl/module-curl.h
@@ -0,0 +1,14 @@
+//==============================================================================
+// module-curl.h
+//==============================================================================
+#ifndef GURAX_MODULE_CURL_H
+#define GURAX_MODULE_CURL_H
+#define CURL_STATICLIB
+#include <gurax.h>
+#include <curl/curl.h>
+#include "Util.h"
+
+Gurax_BeginModuleHeader(curl)
+Gurax_EndModuleHeader(curl)
+
+#endif
