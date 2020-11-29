af7f643a9a456b1e7c949f0ee1dac060df1cd146
ypsitau <ypsitau@nifty.com>
2020-11-29 12:28:39 +0900

    implementing tar module

--- src/module-tar/VType_StatEx.cpp
+++ src/module-tar/VType_StatEx.cpp
@@ -31,6 +31,110 @@ static const char* g_docHelp_en = u8R"**(
 //------------------------------------------------------------------------------
 // Implementation of property
 //------------------------------------------------------------------------------
+// tar.StatEx#chksum
+Gurax_DeclareProperty_R(StatEx, chksum)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(StatEx, chksum)
+{
+	const Header& hdr = GetValueThis(valueTarget).GetStatEx().GetHeader();
+	return new Value_Number(hdr.GetChksum());
+}
+
+// tar.StatEx#devmajor
+Gurax_DeclareProperty_R(StatEx, devmajor)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(StatEx, devmajor)
+{
+	const Header& hdr = GetValueThis(valueTarget).GetStatEx().GetHeader();
+	return new Value_Number(hdr.GetDevMajor());
+}
+
+// tar.StatEx#devminor
+Gurax_DeclareProperty_R(StatEx, devminor)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(StatEx, devminor)
+{
+	const Header& hdr = GetValueThis(valueTarget).GetStatEx().GetHeader();
+	return new Value_Number(hdr.GetDevMinor());
+}
+
+// tar.StatEx#mode
+Gurax_DeclareProperty_R(StatEx, mode)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(StatEx, mode)
+{
+	const Header& hdr = GetValueThis(valueTarget).GetStatEx().GetHeader();
+	return new Value_Number(hdr.GetMode());
+}
+
+// tar.StatEx#gname
+Gurax_DeclareProperty_R(StatEx, gname)
+{
+	Declare(VTYPE_String, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(StatEx, gname)
+{
+	const Header& hdr = GetValueThis(valueTarget).GetStatEx().GetHeader();
+	return new Value_String(hdr.GetGName());
+}
+
+// tar.StatEx#uname
+Gurax_DeclareProperty_R(StatEx, uname)
+{
+	Declare(VTYPE_String, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(StatEx, uname)
+{
+	const Header& hdr = GetValueThis(valueTarget).GetStatEx().GetHeader();
+	return new Value_String(hdr.GetUName());
+}
+
+// tar.StatEx#linkName
+Gurax_DeclareProperty_R(StatEx, linkName)
+{
+	Declare(VTYPE_String, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(StatEx, linkName)
+{
+	const Header& hdr = GetValueThis(valueTarget).GetStatEx().GetHeader();
+	return new Value_String(hdr.GetLinkName());
+}
 
 //------------------------------------------------------------------------------
 // VType_StatEx
@@ -45,6 +149,13 @@ void VType_StatEx::DoPrepare(Frame& frameOuter)
 	Declare(VTYPE_Stat, Flag::Immutable);
 	// Assignment of method
 	// Assignment of property
+	Assign(Gurax_CreateProperty(StatEx, chksum));
+	Assign(Gurax_CreateProperty(StatEx, devmajor));
+	Assign(Gurax_CreateProperty(StatEx, devminor));
+	Assign(Gurax_CreateProperty(StatEx, mode));
+	Assign(Gurax_CreateProperty(StatEx, gname));
+	Assign(Gurax_CreateProperty(StatEx, uname));
+	Assign(Gurax_CreateProperty(StatEx, linkName));
 }
 
 //------------------------------------------------------------------------------
