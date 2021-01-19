c8811e64df26d04b1d34c74adafc0ae5a58a9d58
ypsitau <ypsitau@nifty.com>
2021-01-19 21:30:05 +0900

    implementing cairo module

--- src/module-cairo/Util.cpp
+++ src/module-cairo/Util.cpp
@@ -5,6 +5,11 @@
 
 Gurax_BeginModuleScope(cairo)
 
+inline UInt8 DemultiplyAlpha(UInt8 elem, UInt8 alpha)
+{
+	return (alpha == 0)? 0 : static_cast<UInt8>(static_cast<UInt32>(elem) * 255 / alpha);
+}
+
 bool IsOK(cairo_status_t status)
 {
 	if (status == CAIRO_STATUS_SUCCESS) return true;
@@ -36,29 +41,186 @@ Image* CreateImageFromSurface(cairo_surface_t* surface, const Image::Format& for
 		case CAIRO_FORMAT_ARGB32: {
 			UInt8* pDst = pImage->GetPointerC();
 			for (int y = 0; y < height; y++, data += stride) {
-				unsigned char* pSrc = data;
+				UInt8* pSrc = data;
 				for (int x = 0; x < width; x++, pSrc += 4, pDst += 4) {
-					*(pDst + Image::offsetB) = *(pSrc + 0);
-					*(pDst + Image::offsetG) = *(pSrc + 1);
-					*(pDst + Image::offsetR) = *(pSrc + 2);
-					*(pDst + Image::offsetA) = *(pSrc + 3);
+					UInt8 alpha = *(pSrc + 3);
+					*(pDst + Image::offsetB) = DemultiplyAlpha(*(pSrc + 0), alpha);
+					*(pDst + Image::offsetG) = DemultiplyAlpha(*(pSrc + 1), alpha);
+					*(pDst + Image::offsetR) = DemultiplyAlpha(*(pSrc + 2), alpha);
+					*(pDst + Image::offsetA) = alpha;
 				}
 			}
 			break;
 		}
 		case CAIRO_FORMAT_RGB24: {
+			UInt8* pDst = pImage->GetPointerC();
+			for (int y = 0; y < height; y++, data += stride) {
+				UInt8* pSrc = data;
+				for (int x = 0; x < width; x++, pSrc += 4, pDst += 4) {
+					*(pDst + Image::offsetB) = *(pSrc + 0);
+					*(pDst + Image::offsetG) = *(pSrc + 1);
+					*(pDst + Image::offsetR) = *(pSrc + 2);
+					*(pDst + Image::offsetA) = 0xff;
+				}
+			}
 			break;
 		}
 		case CAIRO_FORMAT_A8: {
+			UInt8* pDst = pImage->GetPointerC();
+			for (int y = 0; y < height; y++, data += stride) {
+				UInt8* pSrc = data;
+				for (int x = 0; x < width; x++, pSrc++, pDst += 4) {
+					UInt8 alpha = *pSrc;
+					UInt8 ch = alpha? 0xff : 0x00;
+					*(pDst + Image::offsetB) = ch;
+					*(pDst + Image::offsetG) = ch;
+					*(pDst + Image::offsetR) = ch;
+					*(pDst + Image::offsetA) = alpha;
+				}
+			}
 			break;
 		}
 		case CAIRO_FORMAT_A1: {
+			UInt8* pDst = pImage->GetPointerC();
+			for (int y = 0; y < height; y++, data += stride) {
+				UInt32* pSrc = reinterpret_cast<UInt32*>(data);
+				UInt32 alphaPacked = 0;
+				size_t nBitsRest = 0;
+				for (int x = 0; x < width; x++, pDst += 4) {
+					if (nBitsRest == 0) {
+						alphaPacked = *pSrc++;
+						nBitsRest = 32;
+					}
+					UInt8 alpha = (alphaPacked & 1) * 0xff;
+					alphaPacked >>= 1;
+					nBitsRest--;
+					UInt8 ch = alpha? 0xff : 0x00;
+					*(pDst + Image::offsetB) = ch;
+					*(pDst + Image::offsetG) = ch;
+					*(pDst + Image::offsetR) = ch;
+					*(pDst + Image::offsetA) = alpha;
+				}
+			}
 			break;
 		}
 		case CAIRO_FORMAT_RGB16_565: {
+			UInt8* pDst = pImage->GetPointerC();
+			for (int y = 0; y < height; y++, data += stride) {
+				UInt16* pSrc = reinterpret_cast<UInt16*>(data);
+				for (int x = 0; x < width; x++, pSrc++, pDst += 4) {
+					UInt16 packed = *pSrc;
+					*(pDst + Image::offsetB) = static_cast<UInt8>((packed >> 0) & 0x1f);
+					*(pDst + Image::offsetG) = static_cast<UInt8>((packed >> 5) & 0x3f);
+					*(pDst + Image::offsetR) = static_cast<UInt8>((packed >> 11) & 0x1f);
+					*(pDst + Image::offsetA) = 0;
+				}
+			}
 			break;
 		}
 		case CAIRO_FORMAT_RGB30: {
+			UInt8* pDst = pImage->GetPointerC();
+			for (int y = 0; y < height; y++, data += stride) {
+				UInt32* pSrc = reinterpret_cast<UInt32*>(data);
+				for (int x = 0; x < width; x++, pSrc++, pDst += 4) {
+					UInt32 packed = *pSrc;
+					*(pDst + Image::offsetB) = static_cast<UInt8>((packed >> 2) & 0xff);
+					*(pDst + Image::offsetG) = static_cast<UInt8>((packed >> 12) & 0xff);
+					*(pDst + Image::offsetR) = static_cast<UInt8>((packed >> 22) & 0xff);
+					*(pDst + Image::offsetA) = 0;
+				}
+			}
+			break;
+		}
+		default:
+			return nullptr;
+		}
+	} else {
+		switch (format) {
+		case CAIRO_FORMAT_ARGB32: {
+			UInt8* pDst = pImage->GetPointerC();
+			for (int y = 0; y < height; y++, data += stride) {
+				UInt8* pSrc = data;
+				for (int x = 0; x < width; x++, pSrc += 4, pDst += 3) {
+					UInt8 alpha = *(pSrc + 3);
+					*(pDst + Image::offsetB) = DemultiplyAlpha(*(pSrc + 0), alpha);
+					*(pDst + Image::offsetG) = DemultiplyAlpha(*(pSrc + 1), alpha);
+					*(pDst + Image::offsetR) = DemultiplyAlpha(*(pSrc + 2), alpha);
+				}
+			}
+			break;
+		}
+		case CAIRO_FORMAT_RGB24: {
+			UInt8* pDst = pImage->GetPointerC();
+			for (int y = 0; y < height; y++, data += stride) {
+				UInt8* pSrc = data;
+				for (int x = 0; x < width; x++, pSrc += 4, pDst += 3) {
+					*(pDst + Image::offsetB) = *(pSrc + 0);
+					*(pDst + Image::offsetG) = *(pSrc + 1);
+					*(pDst + Image::offsetR) = *(pSrc + 2);
+				}
+			}
+			break;
+		}
+		case CAIRO_FORMAT_A8: {
+			UInt8* pDst = pImage->GetPointerC();
+			for (int y = 0; y < height; y++, data += stride) {
+				UInt8* pSrc = data;
+				for (int x = 0; x < width; x++, pSrc++, pDst += 3) {
+					UInt8 alpha = *pSrc;
+					UInt8 ch = alpha? 0xff : 0x00;
+					*(pDst + Image::offsetB) = ch;
+					*(pDst + Image::offsetG) = ch;
+					*(pDst + Image::offsetR) = ch;
+				}
+			}
+			break;
+		}
+		case CAIRO_FORMAT_A1: {
+			UInt8* pDst = pImage->GetPointerC();
+			for (int y = 0; y < height; y++, data += stride) {
+				UInt32* pSrc = reinterpret_cast<UInt32*>(data);
+				UInt32 alphaPacked = 0;
+				size_t nBitsRest = 0;
+				for (int x = 0; x < width; x++, pDst += 3) {
+					if (nBitsRest == 0) {
+						alphaPacked = *pSrc++;
+						nBitsRest = 32;
+					}
+					UInt8 alpha = (alphaPacked & 1) * 0xff;
+					alphaPacked >>= 1;
+					nBitsRest--;
+					UInt8 ch = alpha? 0xff : 0x00;
+					*(pDst + Image::offsetB) = ch;
+					*(pDst + Image::offsetG) = ch;
+					*(pDst + Image::offsetR) = ch;
+				}
+			}
+			break;
+		}
+		case CAIRO_FORMAT_RGB16_565: {
+			UInt8* pDst = pImage->GetPointerC();
+			for (int y = 0; y < height; y++, data += stride) {
+				UInt16* pSrc = reinterpret_cast<UInt16*>(data);
+				for (int x = 0; x < width; x++, pSrc++, pDst += 3) {
+					UInt16 packed = *pSrc;
+					*(pDst + Image::offsetB) = static_cast<UInt8>((packed >> 0) & 0x1f);
+					*(pDst + Image::offsetG) = static_cast<UInt8>((packed >> 5) & 0x3f);
+					*(pDst + Image::offsetR) = static_cast<UInt8>((packed >> 11) & 0x1f);
+				}
+			}
+			break;
+		}
+		case CAIRO_FORMAT_RGB30: {
+			UInt8* pDst = pImage->GetPointerC();
+			for (int y = 0; y < height; y++, data += stride) {
+				UInt32* pSrc = reinterpret_cast<UInt32*>(data);
+				for (int x = 0; x < width; x++, pSrc++, pDst += 4) {
+					UInt32 packed = *pSrc;
+					*(pDst + Image::offsetB) = static_cast<UInt8>((packed >> 2) & 0xff);
+					*(pDst + Image::offsetG) = static_cast<UInt8>((packed >> 12) & 0xff);
+					*(pDst + Image::offsetR) = static_cast<UInt8>((packed >> 22) & 0xff);
+				}
+			}
 			break;
 		}
 		default:
