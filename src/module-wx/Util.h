a79c4b7077ec0fb2d55e65be7cad4ab24fc34801
ypsitau <ypsitau@nifty.com>
2021-03-18 18:11:03 +0900

    added a directory for wx module

---
+++ src/module-wx/Util.h
@@ -0,0 +1,11 @@
+//==============================================================================
+// Util.h
+//==============================================================================
+#ifndef GURAX_MODULE_WX_UTIL_H
+#define GURAX_MODULE_WX_UTIL_H
+#include <gurax.h>
+
+Gurax_BeginModuleScope(wx)
+Gurax_EndModuleScope(wx)
+
+#endif
---
+++ src/module-wx/module-wx.h
@@ -0,0 +1,12 @@
+//==============================================================================
+// module-wx.h
+//==============================================================================
+#ifndef GURAX_MODULE_WX_H
+#define GURAX_MODULE_WX_H
+#include <gurax.h>
+#include "Util.h"
+
+Gurax_BeginModuleHeader(wx)
+Gurax_EndModuleHeader(wx)
+
+#endif
