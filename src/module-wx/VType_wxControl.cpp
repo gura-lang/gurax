2a922f72270a44d622b05a48def1089b51556d07
ypsitau <ypsitau@nifty.com>
2021-06-09 15:59:28 +0900

    implementing wx module

--- src/module-wx/VType_wxControl.cpp
+++ src/module-wx/VType_wxControl.cpp
@@ -27,17 +27,16 @@ static const char* g_docHelp_en = u8R"**(
 //------------------------------------------------------------------------------
 // Implementation of constructor
 //------------------------------------------------------------------------------
-// wx.Control(parent:nil as wx.Window, id as Number, title as String, pos as wx.Point, size as wx.Size, style as Number, name as String) {block?}
+// wx.Control(parent:nil as wx.Window, id as Number, pos? as wx.Point, size? as wx.Size, style? as Number, name? as String) {block?}
 Gurax_DeclareConstructorAlias(Control_gurax, "Control")
 {
 	Declare(VTYPE_wxControl, Flag::None);
 	DeclareArg("parent", VTYPE_wxWindow, ArgOccur::Once, ArgFlag::Nil);
 	DeclareArg("id", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
-	DeclareArg("title", VTYPE_String, ArgOccur::Once, ArgFlag::None);
-	DeclareArg("pos", VTYPE_wxPoint, ArgOccur::Once, ArgFlag::None);
-	DeclareArg("size", VTYPE_wxSize, ArgOccur::Once, ArgFlag::None);
-	DeclareArg("style", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
-	DeclareArg("name", VTYPE_String, ArgOccur::Once, ArgFlag::None);
+	DeclareArg("pos", VTYPE_wxPoint, ArgOccur::ZeroOrOnce, ArgFlag::None);
+	DeclareArg("size", VTYPE_wxSize, ArgOccur::ZeroOrOnce, ArgFlag::None);
+	DeclareArg("style", VTYPE_Number, ArgOccur::ZeroOrOnce, ArgFlag::None);
+	DeclareArg("name", VTYPE_String, ArgOccur::ZeroOrOnce, ArgFlag::None);
 	DeclareBlock(BlkOccur::ZeroOrOnce);
 	AddHelp(
 		Gurax_Symbol(en),
@@ -50,13 +49,12 @@ Gurax_ImplementConstructorEx(Control_gurax, processor_gurax, argument_gurax)
 	Gurax::ArgPicker args_gurax(argument_gurax);
 	wxWindow* parent = args_gurax.IsValid()? args_gurax.Pick<Value_wxWindow>().GetEntityPtr() : nullptr;
 	int id = args_gurax.PickNumber<int>();
-	const char* title = args_gurax.PickString();
-	wxPoint& pos = args_gurax.Pick<Value_wxPoint>().GetEntity();
-	wxSize& size = args_gurax.Pick<Value_wxSize>().GetEntity();
-	int style = args_gurax.PickNumber<int>();
-	const char* name = args_gurax.PickString();
+	const wxPoint& pos = args_gurax.IsValid()? args_gurax.Pick<Value_wxPoint>().GetEntity() : wxDefaultPosition;
+	const wxSize& size = args_gurax.IsValid()? args_gurax.Pick<Value_wxSize>().GetEntity() : wxDefaultSize;
+	int style = args_gurax.IsValid()? args_gurax.PickNumber<int>() : 0;
+	const char* name = args_gurax.IsValid()? args_gurax.PickString() : wxControlNameStr;
 	// Function body
-	auto pEntity_gurax = new Value_wxControl::EntityT(parent, id, title, pos, size, style, name);
+	auto pEntity_gurax = new Value_wxControl::EntityT(parent, id, pos, size, style, wxDefaultValidator, name);
 	RefPtr<Value_wxControl> pValue_gurax(new Value_wxControl(pEntity_gurax));
 	pEntity_gurax->core.SetInfo(processor_gurax.Reference(), *pValue_gurax);
 	return argument_gurax.ReturnValue(processor_gurax, pValue_gurax.release());
