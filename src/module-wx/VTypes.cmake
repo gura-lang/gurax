5d915f43237e4adffb39f14c39e89df42d193cee
ypsitau <ypsitau@nifty.com>
2021-06-30 21:15:58 +0900

    implementing wx module

--- src/module-wx/VTypes.cmake
+++ src/module-wx/VTypes.cmake
@@ -5,20 +5,13 @@
 set(SRCFILES_VTYPE
 	VType_wxApp.cpp
 	VType_wxEventType.cpp
-	VType_wxBrush.cpp
-	VType_wxCursor.cpp
-	VType_wxFont.cpp
-	VType_wxIcon.cpp
-	VType_wxIconBundle.cpp
-	VType_wxPalette.cpp
-	VType_wxPen.cpp
-	VType_wxRegion.cpp
 	VType_wxAboutDialogInfo.cpp
 	VType_wxActivateEvent.cpp
 	VType_wxAnyButton.cpp
 	VType_wxAppConsole.cpp
 	VType_wxBitmap.cpp
 	VType_wxBoxSizer.cpp
+	VType_wxBrush.cpp
 	VType_wxButton.cpp
 	VType_wxCheckBox.cpp
 	VType_wxChildFocusEvent.cpp
@@ -26,17 +19,21 @@ set(SRCFILES_VTYPE
 	VType_wxCommandEvent.cpp
 	VType_wxContextMenuEvent.cpp
 	VType_wxControl.cpp
+	VType_wxCursor.cpp
 	VType_wxDropFilesEvent.cpp
 	VType_wxEraseEvent.cpp
 	VType_wxEvent.cpp
 	VType_wxEvtHandler.cpp
 	VType_wxFlexGridSizer.cpp
 	VType_wxFocusEvent.cpp
+	VType_wxFont.cpp
 	VType_wxFrame.cpp
 	VType_wxGDIObject.cpp
 	VType_wxGridBagSizer.cpp
 	VType_wxGridSizer.cpp
 	VType_wxHelpEvent.cpp
+	VType_wxIcon.cpp
+	VType_wxIconBundle.cpp
 	VType_wxIdleEvent.cpp
 	VType_wxJoystickEvent.cpp
 	VType_wxKeyEvent.cpp
@@ -48,9 +45,12 @@ set(SRCFILES_VTYPE
 	VType_wxMouseEvent.cpp
 	VType_wxObject.cpp
 	VType_wxPaintEvent.cpp
+	VType_wxPalette.cpp
 	VType_wxPanel.cpp
+	VType_wxPen.cpp
 	VType_wxPoint.cpp
 	VType_wxPowerEvent.cpp
+	VType_wxRegion.cpp
 	VType_wxScrollWinEvent.cpp
 	VType_wxSetCursorEvent.cpp
 	VType_wxSize.cpp
