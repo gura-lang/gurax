b134948730dc51c2b75984b0a5961d5a25ad27b8
ypsitau <ypsitau@nifty.com>
2021-07-08 18:20:17 +0900

    implementing wx module

--- src/module-wx/VTypes.cmake
+++ src/module-wx/VTypes.cmake
@@ -4,38 +4,32 @@
 #===============================================================================
 set(SRCFILES_VTYPE
 	VType_wxEventType.cpp
-	VType_wxDateTime.cpp
-	VType_wxTextCtrl.cpp
-	VType_wxCalendarCtrl.cpp
-	VType_wxRadioButton.cpp
-	VType_wxSpinButton.cpp
-	VType_wxSpinCtrl.cpp
-	VType_wxSlider.cpp
-	VType_wxScrollBar.cpp
-	VType_wxStyledTextCtrl.cpp
-	VType_wxStaticLine.cpp
-	VType_wxStaticText.cpp
-	VType_wxStaticBox.cpp
-	VType_wxToolBar.cpp
 	VType_wxAboutDialogInfo.cpp
 	VType_wxActivateEvent.cpp
 	VType_wxAnyButton.cpp
 	VType_wxApp.cpp
 	VType_wxAppConsole.cpp
+	VType_wxAuiMDIClientWindow.cpp
+	VType_wxAuiMDIParentFrame.cpp
+	VType_wxAuiNotebook.cpp
 	VType_wxBitmap.cpp
 	VType_wxBitmapButton.cpp
+	VType_wxBookCtrlBase.cpp
 	VType_wxBoxSizer.cpp
 	VType_wxBrush.cpp
 	VType_wxButton.cpp
+	VType_wxCalendarCtrl.cpp
 	VType_wxCheckBox.cpp
 	VType_wxChildFocusEvent.cpp
 	VType_wxChoice.cpp
+	VType_wxChoicebook.cpp
 	VType_wxCmdLineParser.cpp
 	VType_wxComboBox.cpp
 	VType_wxCommandEvent.cpp
 	VType_wxContextMenuEvent.cpp
 	VType_wxControl.cpp
 	VType_wxCursor.cpp
+	VType_wxDateTime.cpp
 	VType_wxDropFilesEvent.cpp
 	VType_wxEraseEvent.cpp
 	VType_wxEvent.cpp
@@ -45,6 +39,7 @@ set(SRCFILES_VTYPE
 	VType_wxFocusEvent.cpp
 	VType_wxFont.cpp
 	VType_wxFrame.cpp
+	VType_wxGauge.cpp
 	VType_wxGDIObject.cpp
 	VType_wxGridBagSizer.cpp
 	VType_wxGridSizer.cpp
@@ -54,6 +49,7 @@ set(SRCFILES_VTYPE
 	VType_wxIdleEvent.cpp
 	VType_wxJoystickEvent.cpp
 	VType_wxKeyEvent.cpp
+	VType_wxListbook.cpp
 	VType_wxListBox.cpp
 	VType_wxListCtrl.cpp
 	VType_wxMenu.cpp
@@ -62,6 +58,7 @@ set(SRCFILES_VTYPE
 	VType_wxMouseCaptureChangedEvent.cpp
 	VType_wxMouseCaptureLostEvent.cpp
 	VType_wxMouseEvent.cpp
+	VType_wxNotebook.cpp
 	VType_wxObject.cpp
 	VType_wxPaintEvent.cpp
 	VType_wxPalette.cpp
@@ -70,21 +67,34 @@ set(SRCFILES_VTYPE
 	VType_wxPoint.cpp
 	VType_wxPowerEvent.cpp
 	VType_wxRadioBox.cpp
+	VType_wxRadioButton.cpp
 	VType_wxRect.cpp
 	VType_wxRegion.cpp
 	VType_wxSashWindow.cpp
+	VType_wxScrollBar.cpp
 	VType_wxScrollWinEvent.cpp
 	VType_wxSetCursorEvent.cpp
+	VType_wxSimplebook.cpp
 	VType_wxSize.cpp
 	VType_wxSizeEvent.cpp
 	VType_wxSizer.cpp
 	VType_wxSizerFlags.cpp
 	VType_wxSizerItem.cpp
+	VType_wxSlider.cpp
+	VType_wxSpinButton.cpp
+	VType_wxSpinCtrl.cpp
 	VType_wxStaticBox.cpp
 	VType_wxStaticBoxSizer.cpp
+	VType_wxStaticLine.cpp
+	VType_wxStaticText.cpp
 	VType_wxStatusBar.cpp
 	VType_wxStdDialogButtonSizer.cpp
+	VType_wxStyledTextCtrl.cpp
 	VType_wxSysColourChangedEvent.cpp
+	VType_wxTextCtrl.cpp
+	VType_wxToolBar.cpp
+	VType_wxToolbook.cpp
+	VType_wxTreebook.cpp
 	VType_wxValidator.cpp
 	VType_wxWindow.cpp
 	VType_wxWrapSizer.cpp
