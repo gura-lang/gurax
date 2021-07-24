1fa69da8838119d320f2053a9e83a97e4ca77903
ypsitau <ypsitau@nifty.com>
2021-07-24 22:07:21 +0900

    implementing wx module

--- src/module-wx/VType_wxSizeEvent.cpp
+++ src/module-wx/VType_wxSizeEvent.cpp
@@ -32,6 +32,95 @@ static const char* g_docHelp_en = u8R"**(
 //-----------------------------------------------------------------------------
 // Implementation of method
 //-----------------------------------------------------------------------------
+// wx.SizeEvent#GetSize()
+Gurax_DeclareMethodAlias(wxSizeEvent, GetSize_gurax, "GetSize")
+{
+	Declare(VTYPE_wxSize, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementMethodEx(wxSizeEvent, GetSize_gurax, processor_gurax, argument_gurax)
+{
+	// Target
+	auto& valueThis_gurax = GetValueThis(argument_gurax);
+	auto pEntity_gurax = valueThis_gurax.GetEntityPtr();
+	if (!pEntity_gurax) return Value::nil();
+	// Function body
+	return argument_gurax.ReturnValue(processor_gurax, new Value_wxSize(
+		pEntity_gurax->GetSize()));
+}
+
+// wx.SizeEvent#SetSize(size as wx.Size)
+Gurax_DeclareMethodAlias(wxSizeEvent, SetSize_gurax, "SetSize")
+{
+	Declare(VTYPE_Nil, Flag::None);
+	DeclareArg("size", VTYPE_wxSize, ArgOccur::Once, ArgFlag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementMethodEx(wxSizeEvent, SetSize_gurax, processor_gurax, argument_gurax)
+{
+	// Target
+	auto& valueThis_gurax = GetValueThis(argument_gurax);
+	auto pEntity_gurax = valueThis_gurax.GetEntityPtr();
+	if (!pEntity_gurax) return Value::nil();
+	// Arguments
+	Gurax::ArgPicker args_gurax(argument_gurax);
+	Value_wxSize& value_size = args_gurax.Pick<Value_wxSize>();
+	const wxSize& size = value_size.GetEntity();
+	// Function body
+	pEntity_gurax->SetSize(size);
+	return Gurax::Value::nil();
+}
+
+// wx.SizeEvent#GetRect()
+Gurax_DeclareMethodAlias(wxSizeEvent, GetRect_gurax, "GetRect")
+{
+	Declare(VTYPE_wxRect, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementMethodEx(wxSizeEvent, GetRect_gurax, processor_gurax, argument_gurax)
+{
+	// Target
+	auto& valueThis_gurax = GetValueThis(argument_gurax);
+	auto pEntity_gurax = valueThis_gurax.GetEntityPtr();
+	if (!pEntity_gurax) return Value::nil();
+	// Function body
+	return argument_gurax.ReturnValue(processor_gurax, new Value_wxRect(
+		pEntity_gurax->GetRect()));
+}
+
+// wx.SizeEvent#SetRect(rect as wx.Rect)
+Gurax_DeclareMethodAlias(wxSizeEvent, SetRect_gurax, "SetRect")
+{
+	Declare(VTYPE_Nil, Flag::None);
+	DeclareArg("rect", VTYPE_wxRect, ArgOccur::Once, ArgFlag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementMethodEx(wxSizeEvent, SetRect_gurax, processor_gurax, argument_gurax)
+{
+	// Target
+	auto& valueThis_gurax = GetValueThis(argument_gurax);
+	auto pEntity_gurax = valueThis_gurax.GetEntityPtr();
+	if (!pEntity_gurax) return Value::nil();
+	// Arguments
+	Gurax::ArgPicker args_gurax(argument_gurax);
+	Value_wxRect& value_rect = args_gurax.Pick<Value_wxRect>();
+	const wxRect& rect = value_rect.GetEntity();
+	// Function body
+	pEntity_gurax->SetRect(rect);
+	return Gurax::Value::nil();
+}
 
 //-----------------------------------------------------------------------------
 // Implementation of property
@@ -49,6 +138,10 @@ void VType_wxSizeEvent::DoPrepare(Frame& frameOuter)
 	// Declaration of VType
 	Declare(VTYPE_wxEvent, Flag::Mutable);
 	// Assignment of method
+	Assign(Gurax_CreateMethod(wxSizeEvent, GetSize_gurax));
+	Assign(Gurax_CreateMethod(wxSizeEvent, SetSize_gurax));
+	Assign(Gurax_CreateMethod(wxSizeEvent, GetRect_gurax));
+	Assign(Gurax_CreateMethod(wxSizeEvent, SetRect_gurax));
 }
 
 //------------------------------------------------------------------------------
