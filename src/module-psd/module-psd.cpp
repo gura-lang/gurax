abbf1cae8b93b51d78ef99f8677801c99308e682
ypsitau <ypsitau@nifty.com>
2021-10-23 21:43:09 +0900

    created skeleton for psd module

---
+++ src/module-psd/module-psd.cpp
@@ -0,0 +1,66 @@
+//==============================================================================
+// module-psd.cpp
+//==============================================================================
+#include "stdafx.h"
+
+Gurax_BeginModule(psd)
+
+struct FileHeader {
+	char Signature[4];
+	Gurax_PackedUInt16_BE(Version);
+	char Reserved1[6];
+	Gurax_PackedUInt16_BE(NumberOfChannels);
+	Gurax_PackedUInt32_BE(Height);
+	Gurax_PackedUInt32_BE(Width);
+	Gurax_PackedUInt16_BE(Depth);
+	Gurax_PackedUInt16_BE(ColorMode);
+}
+
+struct ColorModeDataSectionHeader {
+	Gurax_PackedUInt32_BE(Length);
+};
+
+//------------------------------------------------------------------------------
+// Implementation of function
+//------------------------------------------------------------------------------
+// psd.Test()
+Gurax_DeclareFunction(Test)
+{
+	Declare(VTYPE_Number, Flag::None);
+	DeclareArg("str", VTYPE_String, ArgOccur::Once, ArgFlag::None);
+	DeclareArg("num", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"Adds up the given two numbers and returns the result.");
+}
+
+Gurax_ImplementFunction(Test)
+{
+	// Arguments
+	ArgPicker args(argument);
+	const char* str = args.PickString();
+	Int num = args.PickNumber<Int>();
+	// Function body
+	return new Value_String(String::Repeat(str, num));
+}
+
+//------------------------------------------------------------------------------
+// Entries
+//------------------------------------------------------------------------------
+Gurax_ModuleValidate()
+{
+	return Version::CheckCoreVersion(GURAX_VERSION, nullptr);
+}
+
+Gurax_ModulePrepare()
+{
+	// Assignment of function
+	Assign(Gurax_CreateFunction(Test));
+	return true;
+}
+
+Gurax_ModuleTerminate()
+{
+}
+
+Gurax_EndModule(psd)
---
+++ src/module-psd/module-psd.h
@@ -0,0 +1,13 @@
+//==============================================================================
+// module-psd.h
+//==============================================================================
+#ifndef GURAX_MODULE_PSD_H
+#define GURAX_MODULE_PSD_H
+#include <gurax.h>
+#include <gurax/PackedNumber.h>
+#include "Util.h"
+
+Gurax_BeginModuleHeader(psd)
+Gurax_EndModuleHeader(psd)
+
+#endif
