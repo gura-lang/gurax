d4d73cd6eaeb1ec2ba63649a9771f017639c3a82
ypsitau <ypsitau@nifty.com>
2022-03-03 22:14:07 +0900

    implementing wx module

--- src/module-wx/VType_wxGridCellNumberEditor.cpp
+++ src/module-wx/VType_wxGridCellNumberEditor.cpp
@@ -28,10 +28,57 @@ static const char* g_docHelp_en = u8R"**(
 //------------------------------------------------------------------------------
 // Implementation of constructor
 //------------------------------------------------------------------------------
+// wx.GridCellNumberEditor(min? as Number, max? as Number) {block?} {block?}
+Gurax_DeclareConstructorAlias(GridCellNumberEditor_gurax, "GridCellNumberEditor")
+{
+	Declare(VTYPE_wxGridCellNumberEditor, Flag::None);
+	DeclareArg("min", VTYPE_Number, ArgOccur::ZeroOrOnce, ArgFlag::None);
+	DeclareArg("max", VTYPE_Number, ArgOccur::ZeroOrOnce, ArgFlag::None);
+	DeclareBlock(BlkOccur::ZeroOrOnce);
+	AddHelp(
+		Gurax_Symbol(en),
+		"Creates an instance of wx.GridCellNumberEditor.");
+}
+
+Gurax_ImplementConstructorEx(GridCellNumberEditor_gurax, processor_gurax, argument_gurax)
+{
+	// Arguments
+	Gurax::ArgPicker args_gurax(argument_gurax);
+	bool min_validFlag = args_gurax.IsValid();
+	int min = min_validFlag? args_gurax.PickNumber<int>() : -1;
+	bool max_validFlag = args_gurax.IsValid();
+	int max = max_validFlag? args_gurax.PickNumber<int>() : -1;
+	// Function body
+	return argument_gurax.ReturnValue(processor_gurax, new Value_wxGridCellNumberEditor(
+		wxGridCellNumberEditor(min, max)));
+}
 
 //-----------------------------------------------------------------------------
 // Implementation of method
 //-----------------------------------------------------------------------------
+// wx.GridCellNumberEditor#SetParameters(params as String)
+Gurax_DeclareMethodAlias(wxGridCellNumberEditor, SetParameters_gurax, "SetParameters")
+{
+	Declare(VTYPE_Nil, Flag::None);
+	DeclareArg("params", VTYPE_String, ArgOccur::Once, ArgFlag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementMethodEx(wxGridCellNumberEditor, SetParameters_gurax, processor_gurax, argument_gurax)
+{
+	// Target
+	auto& valueThis_gurax = GetValueThis(argument_gurax);
+	auto pEntity_gurax = valueThis_gurax.GetEntityPtr();
+	if (!pEntity_gurax) return Value::nil();
+	// Arguments
+	Gurax::ArgPicker args_gurax(argument_gurax);
+	const char* params = args_gurax.PickString();
+	// Function body
+	pEntity_gurax->SetParameters(params);
+	return Gurax::Value::nil();
+}
 
 //-----------------------------------------------------------------------------
 // Implementation of property
@@ -47,8 +94,9 @@ void VType_wxGridCellNumberEditor::DoPrepare(Frame& frameOuter)
 	// Add help
 	AddHelpTmpl(Gurax_Symbol(en), g_docHelp_en);
 	// Declaration of VType
-	Declare(VTYPE_wxGridCellTextEditor, Flag::Mutable);
+	Declare(VTYPE_wxGridCellTextEditor, Flag::Mutable, Gurax_CreateConstructor(GridCellNumberEditor_gurax));
 	// Assignment of method
+	Assign(Gurax_CreateMethod(wxGridCellNumberEditor, SetParameters_gurax));
 }
 
 //------------------------------------------------------------------------------
