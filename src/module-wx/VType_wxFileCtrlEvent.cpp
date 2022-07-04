f3d569e1e6daf46c15d7505d270935d203755a24
ypsitau <ypsitau@nifty.com>
2022-07-04 21:58:16 +0900

    implementi wx moduleQ

--- src/module-wx/VType_wxFileCtrlEvent.cpp
+++ src/module-wx/VType_wxFileCtrlEvent.cpp
@@ -32,6 +32,157 @@ static const char* g_docHelp_en = u8R"**(
 //-----------------------------------------------------------------------------
 // Implementation of method
 //-----------------------------------------------------------------------------
+// wx.FileCtrlEvent#GetDirectory()
+Gurax_DeclareMethodAlias(wxFileCtrlEvent, GetDirectory_gurax, "GetDirectory")
+{
+	Declare(VTYPE_String, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementMethodEx(wxFileCtrlEvent, GetDirectory_gurax, processor_gurax, argument_gurax)
+{
+	// Target
+	auto& valueThis_gurax = GetValueThis(argument_gurax);
+	auto pEntity_gurax = valueThis_gurax.GetEntityPtr();
+	if (!pEntity_gurax) return Value::nil();
+	// Function body
+	wxString rtn = pEntity_gurax->GetDirectory();
+	return new Gurax::Value_String(static_cast<const char*>(rtn.c_str()));
+}
+
+// wx.FileCtrlEvent#GetFile()
+Gurax_DeclareMethodAlias(wxFileCtrlEvent, GetFile_gurax, "GetFile")
+{
+	Declare(VTYPE_String, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementMethodEx(wxFileCtrlEvent, GetFile_gurax, processor_gurax, argument_gurax)
+{
+	// Target
+	auto& valueThis_gurax = GetValueThis(argument_gurax);
+	auto pEntity_gurax = valueThis_gurax.GetEntityPtr();
+	if (!pEntity_gurax) return Value::nil();
+	// Function body
+	wxString rtn = pEntity_gurax->GetFile();
+	return new Gurax::Value_String(static_cast<const char*>(rtn.c_str()));
+}
+
+// wx.FileCtrlEvent#GetFiles()
+Gurax_DeclareMethodAlias(wxFileCtrlEvent, GetFiles_gurax, "GetFiles")
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementMethodEx(wxFileCtrlEvent, GetFiles_gurax, processor_gurax, argument_gurax)
+{
+	// Target
+	auto& valueThis_gurax = GetValueThis(argument_gurax);
+	auto pEntity_gurax = valueThis_gurax.GetEntityPtr();
+	if (!pEntity_gurax) return Value::nil();
+	// Function body
+	wxArrayString rtn = pEntity_gurax->GetFiles();
+	return Util::CreateList(rtn);
+}
+
+// wx.FileCtrlEvent#GetFilterIndex()
+Gurax_DeclareMethodAlias(wxFileCtrlEvent, GetFilterIndex_gurax, "GetFilterIndex")
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementMethodEx(wxFileCtrlEvent, GetFilterIndex_gurax, processor_gurax, argument_gurax)
+{
+	// Target
+	auto& valueThis_gurax = GetValueThis(argument_gurax);
+	auto pEntity_gurax = valueThis_gurax.GetEntityPtr();
+	if (!pEntity_gurax) return Value::nil();
+	// Function body
+	int rtn = pEntity_gurax->GetFilterIndex();
+	return new Gurax::Value_Number(rtn);
+}
+
+// wx.FileCtrlEvent#SetFiles(files[] as String)
+Gurax_DeclareMethodAlias(wxFileCtrlEvent, SetFiles_gurax, "SetFiles")
+{
+	Declare(VTYPE_Nil, Flag::None);
+	DeclareArg("files", VTYPE_String, ArgOccur::Once, ArgFlag::ListVar);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementMethodEx(wxFileCtrlEvent, SetFiles_gurax, processor_gurax, argument_gurax)
+{
+	// Target
+	auto& valueThis_gurax = GetValueThis(argument_gurax);
+	auto pEntity_gurax = valueThis_gurax.GetEntityPtr();
+	if (!pEntity_gurax) return Value::nil();
+	// Arguments
+	Gurax::ArgPicker args_gurax(argument_gurax);
+	wxArrayString files = Util::CreateArrayString(args_gurax.PickList());
+	// Function body
+	pEntity_gurax->SetFiles(files);
+	return Gurax::Value::nil();
+}
+
+// wx.FileCtrlEvent#SetDirectory(directory as String)
+Gurax_DeclareMethodAlias(wxFileCtrlEvent, SetDirectory_gurax, "SetDirectory")
+{
+	Declare(VTYPE_Nil, Flag::None);
+	DeclareArg("directory", VTYPE_String, ArgOccur::Once, ArgFlag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementMethodEx(wxFileCtrlEvent, SetDirectory_gurax, processor_gurax, argument_gurax)
+{
+	// Target
+	auto& valueThis_gurax = GetValueThis(argument_gurax);
+	auto pEntity_gurax = valueThis_gurax.GetEntityPtr();
+	if (!pEntity_gurax) return Value::nil();
+	// Arguments
+	Gurax::ArgPicker args_gurax(argument_gurax);
+	const char* directory = args_gurax.PickString();
+	// Function body
+	pEntity_gurax->SetDirectory(directory);
+	return Gurax::Value::nil();
+}
+
+// wx.FileCtrlEvent#SetFilterIndex(index as Number)
+Gurax_DeclareMethodAlias(wxFileCtrlEvent, SetFilterIndex_gurax, "SetFilterIndex")
+{
+	Declare(VTYPE_Nil, Flag::None);
+	DeclareArg("index", VTYPE_Number, ArgOccur::Once, ArgFlag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementMethodEx(wxFileCtrlEvent, SetFilterIndex_gurax, processor_gurax, argument_gurax)
+{
+	// Target
+	auto& valueThis_gurax = GetValueThis(argument_gurax);
+	auto pEntity_gurax = valueThis_gurax.GetEntityPtr();
+	if (!pEntity_gurax) return Value::nil();
+	// Arguments
+	Gurax::ArgPicker args_gurax(argument_gurax);
+	int index = args_gurax.PickNumber<int>();
+	// Function body
+	pEntity_gurax->SetFilterIndex(index);
+	return Gurax::Value::nil();
+}
 
 //-----------------------------------------------------------------------------
 // Implementation of property
@@ -49,6 +200,13 @@ void VType_wxFileCtrlEvent::DoPrepare(Frame& frameOuter)
 	// Declaration of VType
 	Declare(VTYPE_wxCommandEvent, Flag::Mutable);
 	// Assignment of method
+	Assign(Gurax_CreateMethod(wxFileCtrlEvent, GetDirectory_gurax));
+	Assign(Gurax_CreateMethod(wxFileCtrlEvent, GetFile_gurax));
+	Assign(Gurax_CreateMethod(wxFileCtrlEvent, GetFiles_gurax));
+	Assign(Gurax_CreateMethod(wxFileCtrlEvent, GetFilterIndex_gurax));
+	Assign(Gurax_CreateMethod(wxFileCtrlEvent, SetFiles_gurax));
+	Assign(Gurax_CreateMethod(wxFileCtrlEvent, SetDirectory_gurax));
+	Assign(Gurax_CreateMethod(wxFileCtrlEvent, SetFilterIndex_gurax));
 }
 
 //------------------------------------------------------------------------------
