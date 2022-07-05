0cca8908fe627e85ef9327a5d23d0daa0c5e1f36
ypsitau <ypsitau@nifty.com>
2022-07-05 22:20:53 +0900

    implementing wx module

--- src/module-wx/VType_wxFontInfo.h
+++ src/module-wx/VType_wxFontInfo.h
@@ -6,6 +6,7 @@
 #define GURAX_MODULE_WX_VTYPE_WXFONTINFO_H
 #include <gurax.h>
 #include <wx/wx.h>
+#include <wx/font.h>
 #include "Util.h"
 
 Gurax_BeginModuleScope(wx)
--- src/module-wx/VType_wxFontMetrics.cpp
+++ src/module-wx/VType_wxFontMetrics.cpp
@@ -28,6 +28,22 @@ static const char* g_docHelp_en = u8R"**(
 //------------------------------------------------------------------------------
 // Implementation of constructor
 //------------------------------------------------------------------------------
+// wx.FontMetrics() {block?}
+Gurax_DeclareConstructorAlias(FontMetrics_gurax, "FontMetrics")
+{
+	Declare(VTYPE_wxFontMetrics, Flag::None);
+	DeclareBlock(BlkOccur::ZeroOrOnce);
+	AddHelp(
+		Gurax_Symbol(en),
+		"Creates an instance of wx.FontMetrics.");
+}
+
+Gurax_ImplementConstructorEx(FontMetrics_gurax, processor_gurax, argument_gurax)
+{
+	// Function body
+	return argument_gurax.ReturnValue(processor_gurax, new Value_wxFontMetrics(
+		wxFontMetrics()));
+}
 
 //-----------------------------------------------------------------------------
 // Implementation of method
@@ -36,6 +52,137 @@ static const char* g_docHelp_en = u8R"**(
 //-----------------------------------------------------------------------------
 // Implementation of property
 //-----------------------------------------------------------------------------
+Gurax_DeclareProperty_RW(wxFontMetrics, height)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(wxFontMetrics, height)
+{
+	auto& valueThis_gurax = GetValueThis(valueTarget);
+	return new Value_Number(valueThis_gurax.GetEntity().height);
+}
+
+Gurax_ImplementPropertySetter(wxFontMetrics, height)
+{
+	auto& valueThis_gurax = GetValueThis(valueTarget);
+	auto& value_gurax = value;
+	int height = Value_Number::GetNumber<int>(value_gurax);
+	valueThis_gurax.GetEntity().height = height;
+}
+
+Gurax_DeclareProperty_RW(wxFontMetrics, ascent)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(wxFontMetrics, ascent)
+{
+	auto& valueThis_gurax = GetValueThis(valueTarget);
+	return new Value_Number(valueThis_gurax.GetEntity().ascent);
+}
+
+Gurax_ImplementPropertySetter(wxFontMetrics, ascent)
+{
+	auto& valueThis_gurax = GetValueThis(valueTarget);
+	auto& value_gurax = value;
+	int ascent = Value_Number::GetNumber<int>(value_gurax);
+	valueThis_gurax.GetEntity().ascent = ascent;
+}
+
+Gurax_DeclareProperty_RW(wxFontMetrics, descent)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(wxFontMetrics, descent)
+{
+	auto& valueThis_gurax = GetValueThis(valueTarget);
+	return new Value_Number(valueThis_gurax.GetEntity().descent);
+}
+
+Gurax_ImplementPropertySetter(wxFontMetrics, descent)
+{
+	auto& valueThis_gurax = GetValueThis(valueTarget);
+	auto& value_gurax = value;
+	int descent = Value_Number::GetNumber<int>(value_gurax);
+	valueThis_gurax.GetEntity().descent = descent;
+}
+
+Gurax_DeclareProperty_RW(wxFontMetrics, internalLeading)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(wxFontMetrics, internalLeading)
+{
+	auto& valueThis_gurax = GetValueThis(valueTarget);
+	return new Value_Number(valueThis_gurax.GetEntity().internalLeading);
+}
+
+Gurax_ImplementPropertySetter(wxFontMetrics, internalLeading)
+{
+	auto& valueThis_gurax = GetValueThis(valueTarget);
+	auto& value_gurax = value;
+	int internalLeading = Value_Number::GetNumber<int>(value_gurax);
+	valueThis_gurax.GetEntity().internalLeading = internalLeading;
+}
+
+Gurax_DeclareProperty_RW(wxFontMetrics, externalLeading)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(wxFontMetrics, externalLeading)
+{
+	auto& valueThis_gurax = GetValueThis(valueTarget);
+	return new Value_Number(valueThis_gurax.GetEntity().externalLeading);
+}
+
+Gurax_ImplementPropertySetter(wxFontMetrics, externalLeading)
+{
+	auto& valueThis_gurax = GetValueThis(valueTarget);
+	auto& value_gurax = value;
+	int externalLeading = Value_Number::GetNumber<int>(value_gurax);
+	valueThis_gurax.GetEntity().externalLeading = externalLeading;
+}
+
+Gurax_DeclareProperty_RW(wxFontMetrics, averageWidth)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(
+		Gurax_Symbol(en),
+		"");
+}
+
+Gurax_ImplementPropertyGetter(wxFontMetrics, averageWidth)
+{
+	auto& valueThis_gurax = GetValueThis(valueTarget);
+	return new Value_Number(valueThis_gurax.GetEntity().averageWidth);
+}
+
+Gurax_ImplementPropertySetter(wxFontMetrics, averageWidth)
+{
+	auto& valueThis_gurax = GetValueThis(valueTarget);
+	auto& value_gurax = value;
+	int averageWidth = Value_Number::GetNumber<int>(value_gurax);
+	valueThis_gurax.GetEntity().averageWidth = averageWidth;
+}
 
 //------------------------------------------------------------------------------
 // VType_wxFontMetrics
@@ -47,8 +194,15 @@ void VType_wxFontMetrics::DoPrepare(Frame& frameOuter)
 	// Add help
 	AddHelpTmpl(Gurax_Symbol(en), g_docHelp_en);
 	// Declaration of VType
-	Declare(VTYPE_Object, Flag::Mutable);
+	Declare(VTYPE_Object, Flag::Mutable, Gurax_CreateConstructor(FontMetrics_gurax));
 	// Assignment of method
+	// Assignment of property
+	Assign(Gurax_CreateProperty(wxFontMetrics, height));
+	Assign(Gurax_CreateProperty(wxFontMetrics, ascent));
+	Assign(Gurax_CreateProperty(wxFontMetrics, descent));
+	Assign(Gurax_CreateProperty(wxFontMetrics, internalLeading));
+	Assign(Gurax_CreateProperty(wxFontMetrics, externalLeading));
+	Assign(Gurax_CreateProperty(wxFontMetrics, averageWidth));
 }
 
 //------------------------------------------------------------------------------
