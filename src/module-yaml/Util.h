3153bb6b4cfc6c24c5fcd8c5e766ea5c85c56027
ypsitau <ypsitau@nifty.com>
2022-09-18 18:16:51 +0900

    added yaml module

---
+++ src/module-yaml/Util.h
@@ -0,0 +1,11 @@
+//==============================================================================
+// Util.h
+//==============================================================================
+#ifndef GURAX_MODULE_YAML_UTIL_H
+#define GURAX_MODULE_YAML_UTIL_H
+#include <gurax.h>
+
+Gurax_BeginModuleScope(yaml)
+Gurax_EndModuleScope(yaml)
+
+#endif
---
+++ src/module-yaml/module-yaml.h
@@ -0,0 +1,12 @@
+//==============================================================================
+// module-yaml.h
+//==============================================================================
+#ifndef GURAX_MODULE_YAML_H
+#define GURAX_MODULE_YAML_H
+#include <gurax.h>
+#include "Util.h"
+
+Gurax_BeginModuleHeader(yaml)
+Gurax_EndModuleHeader(yaml)
+
+#endif
