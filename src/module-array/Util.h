5b3aaeb919d6fcbdfaf9fc7962ca3f110dad00c4
ypsitau <ypsitau@nifty.com>
2022-09-26 16:40:47 +0900

    implementing array module

---
+++ src/module-array/Util.h
@@ -0,0 +1,11 @@
+//==============================================================================
+// Util.h
+//==============================================================================
+#ifndef GURAX_MODULE_ARRAY_UTIL_H
+#define GURAX_MODULE_ARRAY_UTIL_H
+#include <gurax.h>
+
+Gurax_BeginModuleScope(array)
+Gurax_EndModuleScope(array)
+
+#endif
---
+++ src/module-array/module-array.h
@@ -0,0 +1,12 @@
+//==============================================================================
+// module-array.h
+//==============================================================================
+#ifndef GURAX_MODULE_ARRAY_H
+#define GURAX_MODULE_ARRAY_H
+#include <gurax.h>
+#include "Util.h"
+
+Gurax_BeginModuleHeader(array)
+Gurax_EndModuleHeader(array)
+
+#endif
