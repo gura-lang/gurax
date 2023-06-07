12feca91d98992d238622495cb166b5c0290a835
ypsitau <ypsitau@nifty.com>
2023-06-07 22:03:13 +0900

    implementing ImageToColumn

--- src/module-ml/Util.cpp
+++ src/module-ml/Util.cpp
@@ -7,6 +7,7 @@ Gurax_BeginModuleScope(ml)
 
 void ImageToColumn(RefPtr<Array> pArrayOut, const Array& arrayIn, size_t nRowsFilter, size_t nColsFilter, size_t strides, size_t padding)
 {
+	const Array::ElemTypeT& elemType = arrayIn.GetElemType();
 	const DimSizes& dimSizesIn = arrayIn.GetDimSizes();
 	size_t nSamples = dimSizesIn[0];
 	size_t nChannels = dimSizesIn[1];
@@ -14,21 +15,30 @@ void ImageToColumn(RefPtr<Array> pArrayOut, const Array& arrayIn, size_t nRowsFi
 	size_t nColsIn = dimSizesIn[3];
 	size_t nRowsOut = (nRowsIn + 2 * padding - nRowsFilter) / strides + 1;
 	size_t nColsOut = (nColsIn + 2 * padding - nColsFilter) / strides + 1;
-	size_t bytesStridesRowIn = nColsIn * arrayIn.GetElemType().bytes;
-	size_t bytesStridesChannel = bytesStridesRowIn * nRowsIn;
-	size_t bytesStridesSample = bytesStridesChannel * nChannels;
-	pArrayOut.reset(Array::Create2d(arrayIn.GetElemType(), nSamples * nRowsOut * nColsOut, nChannels * nRowsFilter * nColsFilter));
+	size_t bytesPerRow = nColsIn * elemType.bytes;
+	size_t bytesPerChannel = bytesPerRow * nRowsIn;
+	size_t bytesPerSample = bytesPerChannel * nChannels;
+	size_t bytesStridesCol = strides * elemType.bytes;
+	size_t bytesStridesRow = strides * bytesPerRow;
+	pArrayOut.reset(Array::Create2d(elemType, nSamples * nRowsOut * nColsOut, nChannels * nRowsFilter * nColsFilter));
 	UInt8* pElemOut = pArrayOut->GetPointerC<UInt8>();
-	size_t bytesToCopy = nColsFilter * arrayIn.GetElemType().bytes;
+	size_t bytesToCopy = nColsFilter * elemType.bytes;
 	const UInt8* pElemSample = arrayIn.GetPointerC<UInt8>();
-	for (size_t iSample = 0; iSample < nSamples; iSample++, pElemSample += bytesStridesSample) {
-		const UInt8* pElemChannel = pElemSample;
-		
-		for (size_t iChannel = 0; iChannel < nChannels; iChannel++, pElemChannel += bytesStridesChannel) {
-			const UInt8* pElemIn = pElemChannel;
-			for (size_t iRowFilter = 0; iRowFilter < nRowsFilter; iRowFilter++, pElemIn += bytesStridesRowIn) {
-				::memcpy(pElemOut, pElemIn, bytesToCopy);
-				pElemOut += bytesToCopy;
+	for (size_t iSample = 0; iSample < nSamples; iSample++, pElemSample += bytesPerSample) {
+		const UInt8* pElemKernelRow = pElemSample;
+		for (size_t iRowOut = 0; iRowOut < nRowsOut; iRowOut++, pElemKernelRow += bytesStridesRow) {
+			const UInt8* pElemKernelCol = pElemKernelRow;
+			for (size_t iColOut = 0; iColOut < nColsOut; iColOut++, pElemKernelCol += bytesStridesCol) {
+				const UInt8* pElemChannel = pElemKernelCol;
+				for (size_t iChannel = 0; iChannel < nChannels; iChannel++, pElemChannel += bytesPerChannel) {
+					const UInt8* pElemIn = pElemChannel;
+					for (size_t iRowFilter = 0; iRowFilter < nRowsFilter; iRowFilter++, pElemIn += bytesPerRow) {
+						pElemOut += elemType.FillZero(pElemOut, padding);
+						::memcpy(pElemOut, pElemIn, bytesToCopy);
+						pElemOut += bytesToCopy;
+						pElemOut += elemType.FillZero(pElemOut, padding);
+					}
+				}
 			}
 		}
 	}
