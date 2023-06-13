d80b8497a76345ff0ddd657a228f7c923e70ef5a
ypsitau <ypsitau@nifty.com>
2023-06-13 14:53:17 +0900

    implementing ColToImg2d

--- src/module-ml/Util.cpp
+++ src/module-ml/Util.cpp
@@ -14,7 +14,8 @@ void PrintCol(const void* pv, size_t bytes)
 	::printf("\n");
 }
 
-bool Img2dToCol(RefPtr<Array>& pArrayExp, const Array& arrayImg, size_t nRowsFilter, size_t nColsFilter, size_t stridesRow, size_t stridesCol, size_t paddingRow, size_t paddingCol)
+bool Img2dToCol(RefPtr<Array>& pArrayExp, const Array& arrayImg, size_t nRowsFilter, size_t nColsFilter,
+				size_t stridesRow, size_t stridesCol, size_t paddingRow, size_t paddingCol)
 {
 	const Array::ElemTypeT& elemType = arrayImg.GetElemType();
 	const DimSizes& dimSizesImg = arrayImg.GetDimSizes();
@@ -35,17 +36,17 @@ bool Img2dToCol(RefPtr<Array>& pArrayExp, const Array& arrayImg, size_t nRowsFil
 	size_t bytesStridesCol = stridesCol * bytesPerCol;
 	size_t bytesStridesRow = stridesRow * bytesPerRow;
 	pArrayExp.reset(Array::Create2d(elemType, nSamples * nRowsExp * nColsExp, nChannels * nRowsFilter * nColsFilter));
-	UInt8* pElemExp = pArrayExp->GetPointerC<UInt8>();
+	auto pElemExp = pArrayExp->GetPointerC<UInt8>();
 	size_t bytesFilterCol = nColsFilter * bytesPerCol;
-	const UInt8* pElemSample = arrayImg.GetPointerC<UInt8>();
+	auto pElemSample = arrayImg.GetPointerC<UInt8>();
 	for (size_t iSample = 0; iSample < nSamples; iSample++, pElemSample += bytesPerSample) {
-		const UInt8* pElemKernelRow = pElemSample;
+		auto pElemKernelRow = pElemSample;
 		size_t iRowImg = 0, iRowExp = 0;
 		for ( ; iRowExp < nRowsExp; iRowImg += stridesRow, iRowExp++) {
 			size_t bytesSkipRowPre = 0;
 			size_t bytesSkipRowPost = 0;
 			size_t nRowsFilterPart = 0;
-			const UInt8* pElemKernelColBase = pElemKernelRow;
+			auto pElemKernelColBase = pElemKernelRow;
 			if (iRowImg + nRowsFilter < paddingRow) {
 				// nothing to do
 			} else if (iRowImg < paddingRow) {
@@ -69,7 +70,7 @@ bool Img2dToCol(RefPtr<Array>& pArrayExp, const Array& arrayImg, size_t nRowsFil
 			do {
 				size_t iColImg = 0, iColExp = 0;
 				size_t bytesFilterColRowChannel = bytesFilterCol * nRowsFilterPart * nChannels;
-				const UInt8* pElemKernelCol = pElemKernelColBase;
+				auto pElemKernelCol = pElemKernelColBase;
 				if (nColsFilter < paddingCol) {
 					size_t n = paddingCol - nColsFilter;
 					iColImg += stridesCol * n;
@@ -78,12 +79,12 @@ bool Img2dToCol(RefPtr<Array>& pArrayExp, const Array& arrayImg, size_t nRowsFil
 				}
 				for ( ; iColExp < nColsExp && iColImg < paddingCol; iColImg += stridesCol, iColExp++) {
 					size_t nColsSkipPre = paddingCol - iColImg;
+					size_t nColsFilterPart = nColsFilter - nColsSkipPre;
 					size_t bytesSkipColPre = nColsSkipPre * bytesPerCol;
-					//size_t nColsFilterPart = nColsFilter - nColsSkipPre;
-					size_t bytesFilterColPart = bytesFilterCol - bytesSkipColPre;
-					const UInt8* pElemChannel = pElemKernelCol;
+					size_t bytesFilterColPart = nColsFilterPart * bytesPerCol;
+					auto pElemChannel = pElemKernelCol;
 					for (size_t iChannel = 0; iChannel < nChannels; iChannel++, pElemChannel += bytesPerChannel) {
-						const UInt8* pElemImg = pElemChannel;
+						auto pElemImg = pElemChannel;
 						pElemExp += bytesSkipRowPre;
 						for (size_t n = 0; n < nRowsFilterPart; n++, pElemImg += bytesPerRow) {
 							pElemExp += bytesSkipColPre;
@@ -95,9 +96,9 @@ bool Img2dToCol(RefPtr<Array>& pArrayExp, const Array& arrayImg, size_t nRowsFil
 				}
 				pElemKernelCol = pElemKernelColBase + (iColImg - paddingCol) * bytesPerCol;
 				for ( ; iColExp < nColsExp && iColImg + nColsFilter <= paddingCol + nColsImg; iColImg += stridesCol, iColExp++) {
-					const UInt8* pElemChannel = pElemKernelCol;
+					auto pElemChannel = pElemKernelCol;
 					for (size_t iChannel = 0; iChannel < nChannels; iChannel++, pElemChannel += bytesPerChannel) {
-						const UInt8* pElemImg = pElemChannel;
+						auto pElemImg = pElemChannel;
 						pElemExp += bytesSkipRowPre;
 						for (size_t n = 0; n < nRowsFilterPart; n++, pElemImg += bytesPerRow) {
 							::memcpy(pElemExp, pElemImg, bytesFilterCol);
@@ -111,9 +112,9 @@ bool Img2dToCol(RefPtr<Array>& pArrayExp, const Array& arrayImg, size_t nRowsFil
 					size_t nColsFilterPart = paddingCol + nColsImg - iColImg;
 					size_t bytesFilterColPart = nColsFilterPart * bytesPerCol;
 					size_t bytesSkipColPost = bytesFilterCol - bytesFilterColPart;
-					const UInt8* pElemChannel = pElemKernelCol;
+					auto pElemChannel = pElemKernelCol;
 					for (size_t iChannel = 0; iChannel < nChannels; iChannel++, pElemChannel += bytesPerChannel) {
-						const UInt8* pElemImg = pElemChannel;
+						auto pElemImg = pElemChannel;
 						pElemExp += bytesSkipRowPre;
 						for (size_t n = 0; n < nRowsFilterPart; n++, pElemImg += bytesPerRow) {
 							::memcpy(pElemExp, pElemImg, bytesFilterColPart);
@@ -130,11 +131,140 @@ bool Img2dToCol(RefPtr<Array>& pArrayExp, const Array& arrayImg, size_t nRowsFil
 				}
 			} while (0);
 		}
-
 	}
 	return true;
 }
 
+bool Img2dToCol(RefPtr<Array>& pArrayImg, const DimSizes& dimSizesImg, const Array& arrayExp, size_t nRowsFilter, size_t nColsFilter,
+				size_t stridesRow, size_t stridesCol, size_t paddingRow, size_t paddingCol)
+{
+	const Array::ElemTypeT& elemType = arrayExp.GetElemType();
+	const DimSizes& dimSizesExp = arrayExp.GetDimSizes();
+	if (dimSizesImg.size() != 4) {
+		Error::Issue(ErrorType::SizeError, "the array must have four dimensions");
+		return false;
+	}
+	size_t nSamples = dimSizesImg[0];
+	size_t nChannels = dimSizesImg[1];
+	size_t nRowsImg = dimSizesImg[2];
+	size_t nColsImg = dimSizesImg[3];
+	size_t nRowsExp = (nRowsImg + 2 * paddingRow - nRowsFilter) / stridesRow + 1;
+	size_t nColsExp = (nColsImg + 2 * paddingCol - nColsFilter) / stridesCol + 1;
+	if (!(dimSizesExp.size() == 2 && dimSizesExp[0] == nSamples * nRowsExp * nColsExp && dimSizesExp[1] == nChannels * nRowsFilter * nColsFilter)) {
+		Error::Issue(ErrorType::SizeError, "invalid size of Array");
+		return false;
+	}
+	auto funcAdd = Array::funcs.Add_ArrayArray[elemType.id][elemType.id];
+	size_t bytesPerCol = elemType.bytes;
+	size_t bytesPerRow = nColsImg * bytesPerCol;
+	size_t bytesPerChannel = bytesPerRow * nRowsImg;
+	size_t bytesPerSample = bytesPerChannel * nChannels;
+	size_t bytesStridesCol = stridesCol * bytesPerCol;
+	size_t bytesStridesRow = stridesRow * bytesPerRow;
+	pArrayImg.reset(Array::Create(elemType, dimSizesImg));
+	auto pElemExp = arrayExp.GetPointerC<UInt8>();
+	size_t bytesFilterCol = nColsFilter * bytesPerCol;
+	auto pElemSample = pArrayImg->GetPointerC<UInt8>();
+	for (size_t iSample = 0; iSample < nSamples; iSample++, pElemSample += bytesPerSample) {
+		auto pElemKernelRow = pElemSample;
+		size_t iRowImg = 0, iRowExp = 0;
+		for ( ; iRowExp < nRowsExp; iRowImg += stridesRow, iRowExp++) {
+			size_t bytesSkipRowPre = 0;
+			size_t bytesSkipRowPost = 0;
+			size_t nRowsFilterPart = 0;
+			auto pElemKernelColBase = pElemKernelRow;
+			if (iRowImg + nRowsFilter < paddingRow) {
+				// nothing to do
+			} else if (iRowImg < paddingRow) {
+				size_t nRowsSkipPre = paddingRow - iRowImg;
+				bytesSkipRowPre = bytesFilterCol * nRowsSkipPre;
+				nRowsFilterPart = nRowsFilter - nRowsSkipPre;
+				if (iRowImg + stridesRow >= paddingRow) {
+					pElemKernelRow = pElemSample + (iRowImg + stridesRow - paddingRow) * bytesPerRow;
+				}
+			} else if (iRowImg + nRowsFilter <= paddingRow + nRowsImg) {
+				nRowsFilterPart = nRowsFilter;
+				pElemKernelRow += bytesStridesRow;
+			} else if (iRowImg < paddingRow + nRowsImg) {
+				nRowsFilterPart = paddingRow + nRowsImg - iRowImg;
+				size_t nRowsSkipPost = nRowsFilter - nRowsFilterPart;
+				bytesSkipRowPost = bytesFilterCol * nRowsSkipPost;
+				pElemKernelRow += bytesStridesRow;
+			} else {
+				// nothing to do
+			}
+			do {
+				size_t iColImg = 0, iColExp = 0;
+				size_t bytesFilterColRowChannel = bytesFilterCol * nRowsFilterPart * nChannels;
+				auto pElemKernelCol = pElemKernelColBase;
+				if (nColsFilter < paddingCol) {
+					size_t n = paddingCol - nColsFilter;
+					iColImg += stridesCol * n;
+					iColExp += n;
+					pElemExp += bytesFilterColRowChannel * n;
+				}
+				for ( ; iColExp < nColsExp && iColImg < paddingCol; iColImg += stridesCol, iColExp++) {
+					size_t nColsSkipPre = paddingCol - iColImg;
+					size_t nColsFilterPart = nColsFilter - nColsSkipPre;
+					size_t bytesSkipColPre = nColsSkipPre * bytesPerCol;
+					size_t bytesFilterColPart = nColsFilterPart * bytesPerCol;
+					auto pElemChannel = pElemKernelCol;
+					for (size_t iChannel = 0; iChannel < nChannels; iChannel++, pElemChannel += bytesPerChannel) {
+						auto pElemImg = pElemChannel;
+						pElemExp += bytesSkipRowPre;
+						for (size_t n = 0; n < nRowsFilterPart; n++, pElemImg += bytesPerRow) {
+							pElemExp += bytesSkipColPre;
+							//::memcpy(pElemExp, pElemImg, bytesFilterColPart);
+							funcAdd(pElemImg, pElemImg, pElemExp, nColsFilterPart);
+							pElemExp += bytesFilterColPart;
+						}
+						pElemExp += bytesSkipRowPost;
+					}
+				}
+				pElemKernelCol = pElemKernelColBase + (iColImg - paddingCol) * bytesPerCol;
+				for ( ; iColExp < nColsExp && iColImg + nColsFilter <= paddingCol + nColsImg; iColImg += stridesCol, iColExp++) {
+					auto pElemChannel = pElemKernelCol;
+					for (size_t iChannel = 0; iChannel < nChannels; iChannel++, pElemChannel += bytesPerChannel) {
+						auto pElemImg = pElemChannel;
+						pElemExp += bytesSkipRowPre;
+						for (size_t n = 0; n < nRowsFilterPart; n++, pElemImg += bytesPerRow) {
+							//::memcpy(pElemExp, pElemImg, bytesFilterCol);
+							funcAdd(pElemImg, pElemImg, pElemExp, nColsFilter);
+							pElemExp += bytesFilterCol;
+						}
+						pElemExp += bytesSkipRowPost;
+					}
+					pElemKernelCol += bytesStridesCol;
+				}
+				for ( ; iColExp < nColsExp && iColImg < paddingCol + nColsImg; iColImg += stridesCol, iColExp++) {
+					size_t nColsFilterPart = paddingCol + nColsImg - iColImg;
+					size_t bytesFilterColPart = nColsFilterPart * bytesPerCol;
+					size_t bytesSkipColPost = bytesFilterCol - bytesFilterColPart;
+					auto pElemChannel = pElemKernelCol;
+					for (size_t iChannel = 0; iChannel < nChannels; iChannel++, pElemChannel += bytesPerChannel) {
+						auto pElemImg = pElemChannel;
+						pElemExp += bytesSkipRowPre;
+						for (size_t n = 0; n < nRowsFilterPart; n++, pElemImg += bytesPerRow) {
+							//::memcpy(pElemExp, pElemImg, bytesFilterColPart);
+							funcAdd(pElemImg, pElemImg, pElemExp, nColsFilterPart);
+							pElemExp += bytesFilterColPart;
+							pElemExp += bytesSkipColPost;
+						}
+						pElemExp += bytesSkipRowPost;
+					}
+					pElemKernelCol += bytesStridesCol;
+				}
+				if (iColExp < nColsExp) {
+					size_t n = nColsExp - iColExp;
+					pElemExp += bytesFilterColRowChannel * n;
+				}
+			} while (0);
+		}
+	}
+	return true;
+}
+
+#if 0
 bool ColToImg2d(RefPtr<Array>& pArrayImg, const DimSizes& dimSizesImg, const Array& arrayExp, size_t nRowsFilter, size_t nColsFilter, size_t stridesRow, size_t stridesCol, size_t paddingRow, size_t paddingCol)
 {
 	const Array::ElemTypeT& elemType = arrayExp.GetElemType();
@@ -260,5 +390,6 @@ bool ColToImg2d(RefPtr<Array>& pArrayImg, const DimSizes& dimSizesImg, const Arr
 	}
 	return true;
 }
+#endif
 
 Gurax_EndModuleScope(ml)
