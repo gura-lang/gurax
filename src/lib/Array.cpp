72d56c9d79cfb8189cc56f50f7e1171ad19468e4
ypsitau <ypsitau@nifty.com>
2023-06-22 16:22:50 +0900

    implementing Array

--- src/lib/Array.cpp
+++ src/lib/Array.cpp
@@ -2194,11 +2194,15 @@ bool Array::Dot(RefPtr<Array>& pArrayRtn, const Array& arrayL, const Array& arra
 	if (dimSizesL.size() >= 2 && dimSizesR.size() == 2) {
 		auto pBegin = dimSizesL.begin(), pEnd = dimSizesL.begin() + dimSizesL.size() - 2;
 		dimSizesRtn.insert(dimSizesRtn.begin(), pBegin, pEnd);
-		nFwdL = DimSizes::CalcLength(pBegin, pEnd) * dimSizesL.GetColSize() * dimSizesL.GetRowSize();
+		nRtns = DimSizes::CalcLength(pBegin, pEnd);
+		nFwdRtn = dimSizesL.GetRowSize() * dimSizesR.GetColSize();
+		nFwdL = dimSizesL.GetColSize() * dimSizesL.GetRowSize();
 	} else if (dimSizesL.size() == 2 && dimSizesR.size() >= 2) {
 		auto pBegin = dimSizesR.begin(), pEnd = dimSizesR.begin() + dimSizesR.size() - 2;
 		dimSizesRtn.insert(dimSizesRtn.begin(), pBegin, pEnd);
-		nFwdR = DimSizes::CalcLength(pBegin, pEnd) * dimSizesR.GetColSize() * dimSizesR.GetRowSize();
+		nRtns = DimSizes::CalcLength(pBegin, pEnd);
+		nFwdRtn = dimSizesL.GetRowSize() * dimSizesR.GetColSize();
+		nFwdR = dimSizesR.GetColSize() * dimSizesR.GetRowSize();
 	} else {
 		Error::Issue(ErrorType::SizeError, "unmatched array size: %s |.| %s",
 				arrayL.ToString(StringStyle::BriefCram).c_str(), arrayR.ToString(StringStyle::BriefCram).c_str());
