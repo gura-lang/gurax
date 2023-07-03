e4014a0fbddff97385071b5563d1fe1762c44fb4
ypsitau <ypsitau@nifty.com>
2023-07-03 16:01:28 +0900

    reimplementing bzip2

--- src/module-bzip2/module-bzip2.cpp
+++ src/module-bzip2/module-bzip2.cpp
@@ -8,33 +8,31 @@ Gurax_BeginModule(bzip2)
 //------------------------------------------------------------------------------
 // Implementation of function
 //------------------------------------------------------------------------------
-// bzip2.Reader(stream:Stream:r) {block?}
-Gurax_DeclareFunction(Reader)
+// Stream#Reader@bzip2() {block?}
+Gurax_DeclareMethodAlias(Stream, Reader_at_bzip2, "Reader@bzip2")
 {
 	Declare(VTYPE_Stream, Flag::None);
-	DeclareArg("stream", VTYPE_Stream, ArgOccur::Once, ArgFlag::StreamR);
 	DeclareBlock(BlkOccur::ZeroOrOnce);
 	AddHelp(Gurax_Symbol(en), u8R"""(
 Creates a `Stream` instance that reads and uncompresses bzip2-encoded data stream.
 )""");
 }
 
-Gurax_ImplementFunction(Reader)
+Gurax_ImplementMethod(Stream, Reader_at_bzip2)
 {
-	// Arguments
-	ArgPicker args(argument);
-	Stream& stream = args.PickStream();
+	// Target
+	auto& valueThis = GetValueThis(argument);
+	Stream& stream = valueThis.GetStream();
 	// Function body
 	RefPtr<Value> pValue(GenerateReader(stream.Reference()));
 	if (!pValue) return Value::nil();
 	return argument.ReturnValue(processor, pValue.release());
 }
 
-// bzip2.Writer(stream:Stream:w, blockSize100k?:Number) {block?}
-Gurax_DeclareFunction(Writer)
+// Stream#Writer@bzip2(blockSize100k? as Number) {block?}
+Gurax_DeclareMethodAlias(Stream, Writer_at_bzip2, "Writer@bzip2")
 {
 	Declare(VTYPE_Stream, Flag::None);
-	DeclareArg("stream", VTYPE_Stream, ArgOccur::Once, ArgFlag::StreamW);
 	DeclareArg("blockSize100k", VTYPE_Number, ArgOccur::ZeroOrOnce);
 	DeclareBlock(BlkOccur::ZeroOrOnce);
 	AddHelp(Gurax_Symbol(en), u8R"""(
@@ -46,11 +44,13 @@ Nine gives the best compression but takes most memory.
 )""");
 }
 
-Gurax_ImplementFunction(Writer)
+Gurax_ImplementMethod(Stream, Writer_at_bzip2)
 {
+	// Target
+	auto& valueThis = GetValueThis(argument);
+	Stream& stream = valueThis.GetStream();
 	// Arguments
 	ArgPicker args(argument);
-	Stream& stream = args.PickStream();
 	int blockSize100k = args.IsValid()? args.PickNumberRanged<int>(1, 9) : 9;
 	if (Error::IsIssued()) return Value::nil();
 	// Function body
@@ -69,9 +69,9 @@ Gurax_ModuleValidate()
 
 Gurax_ModulePrepare()
 {
-	// Assignment of function
-	Assign(Gurax_CreateFunction(Reader));
-	Assign(Gurax_CreateFunction(Writer));
+	// Assignment of method
+	VTYPE_Stream.Assign(Gurax_CreateMethod(Stream, Reader_at_bzip2));
+	VTYPE_Stream.Assign(Gurax_CreateMethod(Stream, Writer_at_bzip2));
 	return true;
 }
 
