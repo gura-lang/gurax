df603566f79b961ed15d28d6247822a5af763f91
ypsitau <ypsitau@nifty.com>
2023-07-03 15:30:56 +0900

    implementing gzip

--- src/module-gzip/module-gzip.cpp
+++ src/module-gzip/module-gzip.cpp
@@ -8,22 +8,21 @@ Gurax_BeginModule(gzip)
 //------------------------------------------------------------------------------
 // Implementation of function
 //------------------------------------------------------------------------------
-// gzip.Reader(stream:r as Stream) {block?}
-Gurax_DeclareFunction(Reader)
+// Stream#Reader@gzip() {block?}
+Gurax_DeclareMethodAlias(Stream, Reader_at_gzip, "Reader@gzip")
 {
 	Declare(VTYPE_Stream, Flag::None);
-	DeclareArg("stream", VTYPE_Stream, ArgOccur::Once, ArgFlag::StreamR);
 	DeclareBlock(BlkOccur::ZeroOrOnce);
 	AddHelp(Gurax_Symbol(en), u8R"""(
 Creates a `Stream` instance that reads and uncompresses gzip-encoded data stream.
 )""");
 }
 
-Gurax_ImplementFunction(Reader)
+Gurax_ImplementMethod(Stream, Reader_at_gzip)
 {
-	// Arguments
-	ArgPicker args(argument);
-	Stream& stream = args.PickStream();
+	// Target
+	auto& valueThis = GetValueThis(argument);
+	Stream& stream = valueThis.GetStream();
 	// Function body
 	const int windowBits = 31;
 	RefPtr<Value> pValue(GenerateReader(stream.Reference(), windowBits));
@@ -31,11 +30,10 @@ Gurax_ImplementFunction(Reader)
 	return argument.ReturnValue(processor, pValue.release());
 }
 
-// gzip.Writer(stream:w as Stream, level? as Number) {block?}
-Gurax_DeclareFunction(Writer)
+// Stream#Writer@gzip(level? as Number) {block?}
+Gurax_DeclareMethodAlias(Stream, Writer_at_gzip, "Writer@gzip")
 {
 	Declare(VTYPE_Stream, Flag::None);
-	DeclareArg("stream", VTYPE_Stream, ArgOccur::Once, ArgFlag::StreamW);
 	DeclareArg("level", VTYPE_Number, ArgOccur::ZeroOrOnce);
 	DeclareBlock(BlkOccur::ZeroOrOnce);
 	AddHelp(Gurax_Symbol(en), u8R"""(
@@ -43,11 +41,13 @@ Creates a `Stream` instance that compresses data into a gzip-encoded data stream
 )""");
 }
 
-Gurax_ImplementFunction(Writer)
+Gurax_ImplementMethod(Stream, Writer_at_gzip)
 {
+	// Target
+	auto& valueThis = GetValueThis(argument);
+	Stream& stream = valueThis.GetStream();
 	// Arguments
 	ArgPicker args(argument);
-	Stream& stream = args.PickStream();
 	int level = args.IsValid()? args.PickNumber<int>() : Z_DEFAULT_COMPRESSION;
 	// Function body
 	const int windowBits = 31;
@@ -66,9 +66,9 @@ Gurax_ModuleValidate()
 
 Gurax_ModulePrepare()
 {
-	// Assignment of function
-	Assign(Gurax_CreateFunction(Reader));
-	Assign(Gurax_CreateFunction(Writer));
+	// Assignment of method
+	VTYPE_Stream.Assign(Gurax_CreateMethod(Stream, Reader_at_gzip));
+	VTYPE_Stream.Assign(Gurax_CreateMethod(Stream, Writer_at_gzip));
 	return true;
 }
 
