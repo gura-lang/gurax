548c707054c98ce4c3b6040c287b8bb5bc201fe0
ypsitau <ypsitau@nifty.com>
2023-12-18 18:16:29 +0900

    implementing sdl module

---
+++ src/module-sdl/VType_SDL_HapticDirection.cpp
@@ -0,0 +1,70 @@
+﻿//==============================================================================
+// VType_SDL_HapticDirection.cpp
+//==============================================================================
+#include "stdafx.h"
+
+Gurax_BeginModuleScope(sdl)
+
+//------------------------------------------------------------------------------
+// Help
+//------------------------------------------------------------------------------
+static const char* g_docHelp_en = u8R"""(
+# Overview
+
+# Predefined Variable
+
+${help.ComposePropertyHelp(sdl.SDL_HapticDirection, `en)}
+
+# Operator
+
+# Cast Operation
+
+${help.ComposeConstructorHelp(sdl.SDL_HapticDirection, `en)}
+
+${help.ComposeMethodHelp(sdl.SDL_HapticDirection, `en)}
+)""";
+
+//-----------------------------------------------------------------------------
+// Implementation of property
+//-----------------------------------------------------------------------------
+// sdl.SDL_HapticDirection#type
+Gurax_DeclareProperty_R(SDL_HapticDirection, type)
+{
+	Declare(VTYPE_Number, Flag::None);
+	AddHelp(Gurax_Symbol(en), u8R"""(
+
+)""");
+}
+
+Gurax_ImplementPropertyGetter(SDL_HapticDirection, type)
+{
+	auto& valueThis = GetValueThis(valueTarget);
+	return new Value_Number(valueThis.GetEntity().type);
+}
+
+//------------------------------------------------------------------------------
+// VType_SDL_HapticDirection
+//------------------------------------------------------------------------------
+VType_SDL_HapticDirection VTYPE_SDL_HapticDirection("SDL_HapticDirection");
+
+void VType_SDL_HapticDirection::DoPrepare(Frame& frameOuter)
+{
+	// Add help
+	AddHelp(Gurax_Symbol(en), g_docHelp_en);
+	// Declaration of VType
+	Declare(VTYPE_Object, Flag::Mutable);
+	// Assignment of property
+	Assign(Gurax_CreateProperty(SDL_HapticDirection, type));
+}
+
+//------------------------------------------------------------------------------
+// Value_SDL_HapticDirection
+//------------------------------------------------------------------------------
+VType& Value_SDL_HapticDirection::vtype = VTYPE_SDL_HapticDirection;
+
+String Value_SDL_HapticDirection::ToString(const StringStyle& ss) const
+{
+	return ToStringGeneric(ss, "sdl.SDL_HapticDirection");
+}
+
+Gurax_EndModuleScope(sdl)
---
+++ src/module-sdl/VType_SDL_HapticDirection.h
@@ -0,0 +1,87 @@
+﻿//==============================================================================
+// VType_SDL_HapticDirection.h
+//==============================================================================
+#ifndef GURAX_MODULE_SDL_VTYPE_SDL_HAPTICDIRECTION_H
+#define GURAX_MODULE_SDL_VTYPE_SDL_HAPTICDIRECTION_H
+#include "module-sdl.h"
+#include "VType_SDL_HapticEffect.h"
+
+Gurax_BeginModuleScope(sdl)
+
+//------------------------------------------------------------------------------
+// VType_SDL_HapticDirection
+//------------------------------------------------------------------------------
+class GURAX_DLLDECLARE VType_SDL_HapticDirection : public VType {
+public:
+	using VType::VType;
+	virtual void DoPrepare(Frame& frameOuter) override;
+};
+
+extern GURAX_DLLDECLARE VType_SDL_HapticDirection VTYPE_SDL_HapticDirection;
+
+//------------------------------------------------------------------------------
+// Value_SDL_HapticDirection
+//------------------------------------------------------------------------------
+class GURAX_DLLDECLARE Value_SDL_HapticDirection : public Value_Object {
+public:
+	// Referable declaration
+	Gurax_DeclareReferable(Value_SDL_HapticDirection);
+	// Uses MemoryPool allocator
+	Gurax_MemoryPoolAllocator("Value_SDL_HapticDirection");
+protected:
+	RefPtr<Value_SDL_HapticEffect> _pValue_HapticEffect;
+public:
+	static VType& vtype;
+public:
+	// Constructor
+	explicit Value_SDL_HapticDirection(VType& vtype = VTYPE_SDL_HapticDirection) : Value_Object(vtype) {}
+	explicit Value_SDL_HapticDirection(Value_SDL_HapticEffect* pValue_HapticEffect, VType& vtype = VTYPE_SDL_HapticDirection) :
+		Value_Object(vtype), _pValue_HapticEffect(pValue_HapticEffect) {}
+	// Copy constructor/operator
+	Value_SDL_HapticDirection(const Value_SDL_HapticDirection& src) = delete;
+	Value_SDL_HapticDirection& operator=(const Value_SDL_HapticDirection& src) = delete;
+	// Move constructor/operator
+	Value_SDL_HapticDirection(Value_SDL_HapticDirection&& src) noexcept = delete;
+	Value_SDL_HapticDirection& operator=(Value_SDL_HapticDirection&& src) noexcept = delete;
+protected:
+	// Destructor
+	~Value_SDL_HapticDirection() = default;
+public:
+	SDL_HapticDirection& GetEntity() { return _pValue_HapticEffect->GetEntity().constant.direction; }
+	const SDL_HapticDirection& GetEntity() const { return _pValue_HapticEffect->GetEntity().constant.direction; }
+	SDL_HapticDirection* GetEntityPtr() { return &_pValue_HapticEffect->GetEntity().constant.direction; }
+	const SDL_HapticDirection* GetEntityPtr() const { return &_pValue_HapticEffect->GetEntity().constant.direction; }
+public:
+	static SDL_HapticDirection& GetEntity(Value& value) {
+		return dynamic_cast<Value_SDL_HapticDirection&>(value).GetEntity();
+	}
+	static const SDL_HapticDirection& GetEntity(const Value& value) {
+		return dynamic_cast<const Value_SDL_HapticDirection&>(value).GetEntity();
+	}
+	static SDL_HapticDirection* GetEntityPtr(Value& value) {
+		return dynamic_cast<Value_SDL_HapticDirection&>(value).GetEntityPtr();
+	}
+	static const SDL_HapticDirection* GetEntityPtr(const Value& value) {
+		return dynamic_cast<const Value_SDL_HapticDirection&>(value).GetEntityPtr();
+	}
+public:
+	// Virtual functions of Value
+	virtual Value* Clone() const override { return Reference(); }
+	virtual size_t DoCalcHash() const override {
+		return reinterpret_cast<size_t>(GetEntityPtr());
+	}
+	virtual bool IsEqualTo(const Value& value) const override {
+		return IsSameType(value) &&
+			GetEntityPtr() == Value_SDL_HapticDirection::GetEntityPtr(value);
+	}
+	virtual bool IsLessThan(const Value& value) const override {
+		return IsSameType(value)?
+			GetEntityPtr() < Value_SDL_HapticDirection::GetEntityPtr(value) :
+			GetVTypeCustom().IsLessThan(value.GetVTypeCustom());
+	}
+	virtual String ToString(const StringStyle& ss) const override;
+};
+
+Gurax_EndModuleScope(sdl)
+
+#endif
